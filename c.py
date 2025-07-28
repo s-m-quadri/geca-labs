311f7a8d5d44ab0e68ddba1cf2b6a05b84c0743d
Arpit Patil <arpitpatil9307@gmail.com>
2025-07-28 09:29:54 +0000

    c.py code chages

--- c.py
+++ c.py
@@ -1,5 +1,11 @@
-# Ask the user for their name and age using `input()`.
-# Then print: "Hello <name>, you are <age> years old."
+// Ask the user for their name and age using `input()`.
+// Then print: "Hello <name>, you are <age> years old."
 
-# 💡 TIP:
-# `input()` always returns a string, so use `int()` to convert age.
+// 💡 TIP:
+// `input()` always returns a string, so use `int()` to convert age.
+
+ let name = prompt("Enter Your Name");
+    let age = prompt("Enter Your age");
+    let is_std = prompt(" is_std");
+
+    console.log("name", name, "age", age, "is_Std", is_std);
