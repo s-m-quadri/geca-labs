646c128d29307def64e14a776c3b22c242435889
Arpit Patil <arpitpatil9307@gmail.com>
2025-07-28 09:47:47 +0000

    commited d

--- d.py
+++ d.py
@@ -1,11 +1,38 @@
-# Write a program that takes two numbers from the user.
-# Print their:
-#  - sum
-#  - difference
-#  - product
-#  - quotient
-#  - remainder
-#  - power (x^y)
+// # Write a program that takes two numbers from the user.
+// # Print their:
+// #  - sum
+// #  - difference
+// #  - product
+// #  - quotient
+// #  - remainder
+// #  - power (x^y)
 
-# 💡 TIP:
-# Use `+`, `-`, `*`, `/`, `%`, and `**`.
+// # 💡 TIP:
+// # Use `+`, `-`, `*`, `/`, `%`, and `**`.
+
+function sum(a,b){
+    return a+b
+}
+function diff(a,b){
+    return a-b
+}
+function product(a,b){
+    return a*b
+}
+function suquotientm(a,b){
+    return a/b
+}
+function remainder(a,b){
+    return a%b
+}
+function power(a,b){
+    return Math.pow(a,b)
+}
+
+let sum = sum(3,5)
+let diff = diff(6,5)
+let product = product(3,5)
+let suquotientm = suquotientm(3,5)
+let remainder = remainder(3,5)
+let remainder = remainder(3,5)
+let power = power(3,5)
