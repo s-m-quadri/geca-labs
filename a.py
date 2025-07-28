3756966607c5dc8f8a43b5e340611c0ae2bce873
Saiesh Vieshnow Mohandas Sharma <161768278+SaieshSharma@users.noreply.github.com>
2025-07-28 09:09:51 +0000

    Completed my First DAA Lab by solving the first four programs a,b,c,d

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 # Write a program that prints:
 #  - "Hello, Python!"
 #  - "This is Lab A"
-# This is your first Python program.
 
-# 💡 TIP:
-# Use the `print()` function.
+print("Hello World")
+print("This is Lab A")
+
+
