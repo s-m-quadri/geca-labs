52c97529d4f1948af282df43d380da715d8833f8
bunnypranay <149605837+bunnypranay@users.noreply.github.com>
2025-08-03 18:14:27 +0000

    Complete g.py
    I've completed g.py lab by solving on GitHub codespace

--- g.py
+++ g.py
@@ -4,4 +4,14 @@
 
 # 💡 TIP:
 # `range(1, 11)` gives 1 through 10.
-# Use `while` with a counter.
+# Use `while` with a x.
+
+# for loop
+for x in range(1, 11):
+    print(x)
+
+# while loop
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
