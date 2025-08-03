50bed2f212d876598cd0df1b3eed4dcf8a5295fb
prathamesh0416 <166020481+prathamesh0416@users.noreply.github.com>
2025-08-03 18:14:53 +0000

    Complete h.py
    I've completed h.py lab by solving on GitHub codespace

--- h.py
+++ h.py
@@ -4,3 +4,13 @@
 
 # 💡 TIP:
 # Use indexing like list[0], list[-1], and list[2] to access or change values.
+
+list = [10, 20, 30, 40, 50]
+
+print("First:", list[0])
+print("Middle:", list[len(list) // 2])
+print("Last:", list[-1])
+
+list[2] = 100
+
+print("New list:", list)
