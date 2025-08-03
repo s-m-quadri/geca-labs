56f6e515888906e8136cd42b6a884ec8106e3fae
prathamesh0416 <166020481+prathamesh0416@users.noreply.github.com>
2025-08-03 18:15:06 +0000

    Complete i.py
    I've completed i.py lab by solving on GitHub codespace

--- i.py
+++ i.py
@@ -3,3 +3,11 @@
 
 # 💡 TIP:
 # Use `for item in list` and `enumerate(list)` for both item and index.
+
+list = ["a", "b", "c", "d", "e"]
+
+for item in list:
+    print(item)
+
+for index, value in enumerate(list):
+    print(f"Index {index}: {value}")
