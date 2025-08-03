7aeef90f1bda3fb940ea6df56fb70dcc36d9610a
prathamesh0416 <166020481+prathamesh0416@users.noreply.github.com>
2025-08-03 18:27:40 +0000

    Complete s.py
    I've completed s.py lab by solving on GitHub codespace

--- s.py
+++ s.py
@@ -4,3 +4,13 @@
 
 # 💡 TIP:
 # Use `except ZeroDivisionError:` and `except ValueError:` blocks.
+
+try:
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    div = a / b
+    print("Division:", div)
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero.")
+except ValueError:
+    print("Error: Please enter valid integers.")
