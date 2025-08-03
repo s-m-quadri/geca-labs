dbff67ebcbed3071c846a0449b483fd95e86ff09
tanvi28pilawan <166020773+tanvi28pilawan@users.noreply.github.com>
2025-08-03 17:06:14 +0000

    lab g to z+++

--- p.py
+++ p.py
@@ -5,3 +5,19 @@
 
 # 💡 TIP:
 # All Python functions return something, even if it's just `None`.
+
+
+def void_function():
+    print("Running")
+
+
+def add(a, b):
+    return a + b
+
+
+result1 = void_function()
+print("Return value of void_function():", result1)
+
+
+result2 = add(10, 5)
+print("Return value of add(10, 5):", result2)
