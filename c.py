73f98e7d6bf23e9bf833ee18cda495e90faf673b
Gunjan Tembhare <157410431+GunjanTembhare@users.noreply.github.com>
2025-08-07 11:01:40 +0000

    Completed c.py

--- c.py
+++ c.py
@@ -3,3 +3,9 @@
 
 # 💡 TIP:
 # `input()` always returns a string, so use `int()` to convert age.
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print(f"Hello {name}, you are {age} years old.")
+
+
