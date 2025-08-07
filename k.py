6c3dad1cfc08e37d2d2eeb643550efbe801105b1
Gayatriii05 <165500049+Gayatriii05@users.noreply.github.com>
2025-08-07 14:37:31 +0000

    Completed k.py
    I have competed k.py on GitHub codesoaces.

--- k.py
+++ k.py
@@ -4,3 +4,15 @@
 
 # 💡 TIP:
 # Tuples are like lists, but immutable (they cannot be changed).
+# Create an empty list
+# Create a tuple with 3 items
+my_tuple = ("cat", "dog", "rabbit")
+
+
+print("Items in the tuple:")
+for item in my_tuple:
+    print(item)
+
+
+my_list = []
+print("\nEmpty list created:", my_list)
