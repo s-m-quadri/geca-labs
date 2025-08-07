08e932f5164a51c2babe46182a7780be53901b28
BhartiKarpe <bhartikarpe05@gmail.com>
2025-08-07 10:44:56 +0000

    Completed b.py
    I have completed b.py using Github Codespaces

--- b.py
+++ b.py
@@ -5,5 +5,11 @@
 #  - is_student (boolean)
 # Then print them in one line using commas.
 
+name = "Bharti"
+age = 20
+is_student = True
+print(name,age,is_student)
+
+
 # 💡 TIP:
 # Use `=`, `True/False`, and `print()` with commas.
