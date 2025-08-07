4f59b053f41539d58f1255e949d02218315eb841
Amruta madhav kotalwar <madhavkotalwar@gmail.com>
2025-08-07 11:33:35 +0000

    completed j.py

--- j.py
+++ j.py
@@ -4,3 +4,20 @@
 
 # 💡 TIP:
 # Use .append(), .sort(), and .reverse()
+
+# Create an empty list
+my_list = []
+
+# Append 3 strings
+my_list.append("orange")
+my_list.append("apple")
+my_list.append("banana")
+
+# Sort the list
+my_list.sort()
+
+# Reverse the list
+my_list.reverse()
+
+# Print the final list
+print(my_list)
