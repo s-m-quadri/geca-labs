5941daf958d4dfc45ae4572063534d50ebd3e8a8
BhartiKarpe <bhartikarpe05@gmail.com>
2025-08-07 11:01:49 +0000

    Completed f.py

--- f.py
+++ f.py
@@ -3,6 +3,14 @@
 #  - "Positive" if it's > 0
 #  - "Negative" if it's < 0
 #  - "Zero" if it's 0
+num = int(input("Enter a Number : "))
 
+if num>0:
+    print("Positive")
+elif num<0:
+    print("Negative")
+else : 
+    print("Zero")
 # 💡 TIP:
+
 # Use `if`, `elif`, and `else`.
