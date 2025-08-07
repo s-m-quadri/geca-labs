1b2d2b956c4834b02d67da3740198924aa349ead
Vishal Bhutekar <127893332+vishal-bhutekar21@users.noreply.github.com>
2025-08-07 10:18:27 +0000

    added new a file

--- a.py
+++ a.py
@@ -1,7 +1,4 @@
-# Write a program that prints:
-#  - "Hello, Python!"
-#  - "This is Lab A"
-# This is your first Python program.
+print("Hello,Python")
+print("This is Lab A")
+print("This is your first Python progarm")
 
-# 💡 TIP:
-# Use the `print()` function.
