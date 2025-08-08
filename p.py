2bd6467ff6fc49978739fd5c53ed64f6a04c23d8
riyagobade <166010013+riyagobade@users.noreply.github.com>
2025-08-08 13:37:53 +0000

    Complete p.py file changes

--- p.py
+++ p.py
@@ -5,3 +5,10 @@
 
 # 💡 TIP:
 # All Python functions return something, even if it's just `None`.
+
+def void_function():
+    print("Running")
+def add(a, b):
+    return a + b
+print(void_function())
+print(add(1, 2))
