50aa698f8607f9b1924279c3d7f187e70cba596f
riyagobade <166010013+riyagobade@users.noreply.github.com>
2025-08-08 14:22:30 +0000

    Complete z.py file changes.

--- z.py
+++ z.py
@@ -4,3 +4,25 @@
 
 # 💡 TIP:
 # Combine `if`, `math`, `random`, and type conversion.
+
+import math
+import random
+quotes = [
+    "Believe in yourself!",
+    "Keep going, you’re doing great!",
+    "Every day is a second chance.",
+    "You are stronger than you think.",
+    "Success is no accident."
+]
+try:
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print(f"{num} is Even.")
+    else:
+        print(f"{num} is Odd.")
+
+    print("Square root:", math.sqrt(num))
+    if num > 10:
+        print("Motivational quote:", random.choice(quotes))
+except ValueError:
+    print("Invalid input")
