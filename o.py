3c15092edeaa3c6cfa75a6235b07ad396067bbdc
riyagobade <166010013+riyagobade@users.noreply.github.com>
2025-08-08 13:33:49 +0000

    Complete o.py file changes

--- o.py
+++ o.py
@@ -4,3 +4,9 @@
 
 # 💡 TIP:
 #  Use `def greet(name, msg="Hello"):` to set a default value.
+
+def greet(name, msg = "Hello"):
+    print(msg, name)
+greet("Riya")
+greet("Sakshi", "Hey")
+greet(name = "Leela", msg = "Hi")
