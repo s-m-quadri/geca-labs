610a67c555e5f6226b2e34611cff753375ca2970
riyagobade <166010013+riyagobade@users.noreply.github.com>
2025-08-08 14:04:20 +0000

    Complete t.py file changes.

--- t.py
+++ t.py
@@ -3,3 +3,11 @@
 
 # 💡 TIP:
 # Use `with open(...) as f:` to auto-close files.
+
+with open("output.txt", "w") as f:
+    f.write("Hi\n")
+    f.write("My name is Riya\n")
+    f.write("Thank You!")
+with open("output.txt", "r") as f:
+    content = f.read()
+print(content)
