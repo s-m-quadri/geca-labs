2313bffa485090ea69a0933f6aca0b6ef52a29a2
komal-kharatmol <kharatmolkomal250@gmail.com>
2025-08-11 16:05:07 +0000

    Completed d.py

--- d.py
+++ d.py
@@ -9,3 +9,14 @@
 
 # 💡 TIP:
 # Use `+`, `-`, `*`, `/`, `%`, and `**`.
+
+x = float(input("Enter the first number : "))
+y = float(input("Enter the second number : "))
+
+print("Sum:", x + y)
+print("Difference:", x - y)
+print("Product:", x * y)
+print("Quotient:", x / y)
+print("Remainder:", x % y)
+print("Power (x^y):", x ** y)
+
