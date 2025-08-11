9892e006afba2f460c14c0e15501630f54f20692
Jgargee <gargeejoshi536@gmail.com>
2025-08-11 09:19:52 +0000

    complete z.py

--- z.py
+++ z.py
@@ -4,3 +4,22 @@
 
 # 💡 TIP:
 # Combine `if`, `math`, `random`, and type conversion.
+import math
+import random
+
+quotes = [
+    "Keep going, you're doing great!",
+    "Believe in yourself!",
+    "Every step counts!",
+    "Success starts with the first step.",
+    "You have got this!"
+]
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+        print(f"{num} is even.")
+print("Square root:", math.sqrt(num))
+
+if num > 10:
+        print("Motivation:", random.choice(quotes))
