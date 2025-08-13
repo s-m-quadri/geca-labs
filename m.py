2bbc8a7dfc185285bd59b5345e55ebbc72800ec2
Rutik Ratnkar Ghose <rutikghose901@gmail.com>
2025-08-13 15:44:38 +0000

    i have completed lab 0 m.py

--- m.py
+++ m.py
@@ -4,3 +4,8 @@
 
 # 💡 TIP:
 # Sets remove duplicates automatically. Use `.add()` to insert.
+
+numbers = {1, 2, 3, 2, 4, 1, 5}
+print("Unique numbers:", numbers)
+numbers.add(6)
+print("Updated set:", numbers)
