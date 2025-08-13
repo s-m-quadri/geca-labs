d61d46f3015cd8f3aa5c9819f73bd6df2fc289d2
Rutik Ratnkar Ghose <rutikghose901@gmail.com>
2025-08-13 15:41:40 +0000

    i have completed lab 0 l.py

--- l.py
+++ l.py
@@ -4,3 +4,14 @@
 
 # 💡 TIP:
 # Use dict[key] to access values, and `.items()` to loop.
+
+directory={
+    "one":1,
+    "two":2,
+    "three":3
+}
+
+print(directory["one"])
+for key,value in directory.items():
+    print(key,value)
+
