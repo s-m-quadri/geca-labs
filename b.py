b892236aa7761abd653a8010d390d00ca8308aae
tejs11 <tejaswinibankewar2311@gmail.com>
2025-08-13 17:20:32 +0000

    Completed b.py

--- b.py
+++ b.py
@@ -5,6 +5,8 @@
 #  - is_student (boolean)
 # Then print them in one line using commas.
 
-
+print("Tejaswini")
+print("20")
+print("Yes")
 # 💡 TIP:
 # Use `=`, `True/False`, and `print()` with commas.
