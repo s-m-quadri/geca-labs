ac96281c8c006234168bbec2227a4dcd9a3e6248
Rutik Ratnkar Ghose <rutikghose901@gmail.com>
2025-08-13 15:32:29 +0000

    i have completed lab 0 i.py

--- i.py
+++ i.py
@@ -3,3 +3,13 @@
 
 # 💡 TIP:
 # Use `for item in list` and `enumerate(list)` for both item and index.
+
+list=["apple","banana","orange"]
+
+for i in list:
+    print(i)
+
+
+
+for index,value in enumerate(list):
+    print(index,value)  
