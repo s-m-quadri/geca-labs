fe2f05c5119c6ad4842d300929bde73a968c6b98
Vaishnavi <vaishnavibasvande2005@gmail.com>
2025-08-14 11:02:30 +0530

    i completed d.py

--- d.py
+++ d.py
@@ -9,3 +9,17 @@
 
 # 💡 TIP:
 # Use `+`, `-`, `*`, `/`, `%`, and `**`.
+a = int(input("first num:"))
+b = int(input("second num:"))
+c = a+b
+d = a-b
+m = a*b
+s = a/b
+r = a%b
+p = a^b
+print(c)
+print(d)
+print(m)
+print(s)
+print(r)
+print(p)
