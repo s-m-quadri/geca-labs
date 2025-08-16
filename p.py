44449dfa2fecd11fb67d88f3609b7e3a681986d3
komal-kharatmol <kharatmolkomal250@gmail.com>
2025-08-16 07:16:25 +0000

    Completed p.py

--- p.py
+++ p.py
@@ -5,3 +5,13 @@
 
 # 💡 TIP:
 # All Python functions return something, even if it's just `None`.
+
+def void_function():
+    print("Running")
+
+def add(a, b):
+    return a + b
+
+# Calling functions and printing their return values
+print("Return value of void_function():", void_function())
+print("Return value of add(5, 3):", add(5, 3))
