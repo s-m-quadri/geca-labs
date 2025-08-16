d12973bd451163c82fc79ec7cea2ec464a9846f8
komal-kharatmol <kharatmolkomal250@gmail.com>
2025-08-16 07:16:57 +0000

    Completed q.py

--- q.py
+++ q.py
@@ -4,3 +4,13 @@
 
 # 💡 TIP:
 # Outer loop → rows; Inner loop → columns.
+
+rows = int(input("Enter number of rows: "))
+cols = int(input("Enter number of columns: "))
+
+# Print rectangle
+for i in range(rows):
+    for j in range(cols):
+        print("*", end=" ")
+    print() 
+
