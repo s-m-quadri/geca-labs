d151faa115e8891f8ab97a916b5d2a2a8a1931f3
Bhagyashree200527 <bhagyashreekere2005@gmail.com>
2025-08-19 17:54:23 +0000

    completed l.py

--- l.py
+++ l.py
@@ -4,3 +4,16 @@
 
 # 💡 TIP:
 # Use dict[key] to access values, and `.items()` to loop.
+# Create a dictionary with 3 Indian student names and their marks
+student_marks = {
+    "Aarav": 85,
+    "Isha": 92,
+    "Rahul": 78
+}
+
+# Print a specific student's marks
+print("Isha's marks:", student_marks["Isha"])
+
+# Loop through all students and print their names and scores
+for name, marks in student_marks.items():
+    print(f"{name}: {marks}")
