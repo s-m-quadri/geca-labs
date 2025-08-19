b7d2d20772e8b31f9078a2203a41ba3e99715421
Bhagyashree200527 <bhagyashreekere2005@gmail.com>
2025-08-19 18:16:20 +0000

    completed t.py

--- t.py
+++ t.py
@@ -3,3 +3,11 @@
 
 # 💡 TIP:
 # Use `with open(...) as f:` to auto-close files.
+with open("output.txt", "w") as f:
+    f.write("Line 1: Hello\n")
+    f.write("Line 2: Python is fun\n")
+    f.write("Line 3: File handling\n")
+
+with open("output.txt", "r") as f:
+    content = f.read()
+    print(content)
