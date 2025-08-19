56424a577072e26072bfa1c40099aa568f8e3399
Bhagyashree200527 <bhagyashreekere2005@gmail.com>
2025-08-19 10:54:30 +0000

    completed d.py

--- d.py
+++ d.py
@@ -9,3 +9,18 @@
 
 # 💡 TIP:
 # Use `+`, `-`, `*`, `/`, `%`, and `**`.
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number : "))
+
+print("Sum : ", x+y)
+print("Difference : ",x-y)
+print("Product : ",x*y)
+
+if y!= 0:
+    print("Quotient : ",x/y)
+    print("Remainder : ",x%y)
+else: 
+    print("Quotinent: Undefined (division by zero)")    
+    print("Remainder: Undefined (division by zero)") 
+
+print("Power (x^y):", x**y)
