ca3b9a596b792aeba8ed344322a1a00985c0a1f2
Rushikesh Gaikwad <166025882+Rushikesh1821@users.noreply.github.com>
2025-08-21 17:54:20 +0000

    completed z++.py

--- z+.py
+++ z+.py
@@ -2,3 +2,11 @@
 
 # 💡 TIP:
 # Use `input()` for numbers and operator, then `if-elif` to select operation.
+a = float(input("First number: "))
+op = input("Operator (+ - * /): ")
+b = float(input("Second number: "))
+if op == '+': print(a + b)
+elif op == '-': print(a - b)
+elif op == '*': print(a * b)
+elif op == '/': print(a / b)
+else: print("Invalid operator")
