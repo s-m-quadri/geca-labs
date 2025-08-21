cb250c8aeddb2207824796a3bd2b9da255790e3f
ShivamAtram <shivamatram2002@gmail.com>
2025-08-21 11:47:58 +0000

    completed h.py

--- h.py
+++ h.py
@@ -4,3 +4,10 @@
 
 # 💡 TIP:
 # Use indexing like list[0], list[-1], and list[2] to access or change values.
+my_list = [10, 20, 30, 40, 50]
+print("First:", my_list[0])
+print("Middle:", my_list[len(my_list) // 2])
+print("Last:", my_list[-1])
+
+my_list[2] = 100
+print("Modified List:", my_list)
