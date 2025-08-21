988890ef87c981a72c0e78c2525a6bfb17d7c8a8
NandiniTange <116943213+NandiniTange@users.noreply.github.com>
2025-08-21 09:27:37 +0000

    Completed lab 1

--- task_reverse_digits.py
+++ task_reverse_digits.py
@@ -14,3 +14,14 @@
 # - Recursive version can peel off last digit using n % 10
 # - You may pass an extra parameter (e.g., accumulator) if needed
 # - Iterative version: use while-loop and integer math
+
+def reverse_digits_recursive(num,sum=0):
+    if num<=0:
+        return sum
+
+    rem = num%10
+    sum= sum*10+rem
+    return reverse_digits_recursive(num//10,sum)
+
+num = 1234
+print(reverse_digits_recursive(num))
