e4b8c87c541ed37785ab253c1fd834a74c22123a
Rushikesh Gaikwad <166025882+Rushikesh1821@users.noreply.github.com>
2025-08-21 17:34:31 +0000

    completed j.py

--- j.py
+++ j.py
@@ -4,3 +4,13 @@
 
 # 💡 TIP:
 # Use .append(), .sort(), and .reverse()
+words = []
+words.append("zebra")
+words.append("apple")
+words.append("monkey")
+
+words.sort()
+print("Sorted:", words)
+
+words.reverse()
+print("Reversed:", words)
