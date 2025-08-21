cacb952c2b7e18584d73568adc99cd58fe9a2e1d
kevalgawali <kevalgawali20@gmail.com>
2025-08-21 20:35:25 +0000

    c and d done

--- c.py
+++ c.py
@@ -3,3 +3,6 @@
 
 # 💡 TIP:
 # `input()` always returns a string, so use `int()` to convert age.
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+print(f"Hello {name}, you are {age} years old")
