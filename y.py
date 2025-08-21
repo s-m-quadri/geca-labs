f9cc638f3e3d0da43d7776949088393efe14ce3e
Vaishnavi <vaishnavibasvande2005@gmail.com>
2025-08-21 13:36:40 +0530

    complete x.py

--- y.py
+++ y.py
@@ -3,3 +3,11 @@
 
 # 💡 TIP:
 # Use `.isdigit()` or handle exceptions using try-except.
+def take_number():
+    user_input = input("Enter a number: ")
+    if user_input.isdigit():
+        print("You entered:", int(user_input))
+    else:
+        print("Invalid input")
+
+take_number()
