41c599f7bb9e22fab1a60f998474d3d8e91b2165
bot-s-m-quadri <access.smq@gmail.com>
2025-08-24 07:58:44 +0000

    Organize submission for BT23F05F044 (merge into sub-lab-daa-02)

--- task_sort_student_scores.py
+++ task_sort_student_scores.py
@@ -13,38 +13,3 @@
 # - Do not use Python's built-in sort().
 
 # Write your solution here
-def merge_sort_students(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = merge_sort_students(arr[:mid])
-    right = merge_sort_students(arr[mid:])
-    return merge(left, right)
-
-def merge(left, right):
-    merged = []
-    i = j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i][1] > right[j][1]:
-            merged.append(left[i])
-            i += 1
-        elif left[i][1] < right[j][1]:
-            merged.append(right[j])
-            j += 1
-        else:  
-            if left[i][0] < right[j][0]:
-                merged.append(left[i])
-                i += 1
-            else:
-                merged.append(right[j])
-                j += 1
-
-    merged.extend(left[i:])
-    merged.extend(right[j:])
-    return merged
-
-students = [("Alice", 85), ("Bob", 92), ("Charlie", 78), ("David", 92)]
-sorted_students = merge_sort_students(students)
-print(sorted_students)
