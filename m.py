a839673a98dc86a6b39f4937c2f7f05e8622d1db
adityasarode001 <adityasarode001@gmail.com>
2025-08-25 09:06:38 +0000

    completed lab0

--- m.py
+++ m.py
@@ -4,3 +4,10 @@
 
 # 💡 TIP:
 # Sets remove duplicates automatically. Use `.add()` to insert.
+
+
+nums = {1, 2, 2, 3, 4, 4, 5}
+print("Unique set:", nums)
+
+nums.add(6)
+print("After adding 6:", nums)
--- z.py
+++ z.py
@@ -4,3 +4,13 @@
 
 # 💡 TIP:
 # Combine `if`, `math`, `random`, and type conversion.
+
+
+import math, random
+quotes = ["Keep going!", "You got this!", "Never stop learning."]
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+print("Square root:", math.sqrt(num))
+if num > 10:
+    print(random.choice(quotes))
