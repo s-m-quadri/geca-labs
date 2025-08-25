4662c9fb0d1e326d7bb7904184b974d74e7a398d
bot-s-m-quadri <access.smq@gmail.com>
2025-08-25 15:42:58 +0000

    Organize submission for BT23F05F049 (merge into sub-lab-daa-01)

--- labs-design-analysis-algorithms/lab-01/BT23F05F049/task_reverse_digits.py
+++ labs-design-analysis-algorithms/lab-01/BT23F05F049/task_reverse_digits.py
@@ -14,3 +14,24 @@
 # - Recursive version can peel off last digit using n % 10
 # - You may pass an extra parameter (e.g., accumulator) if needed
 # - Iterative version: use while-loop and integer math
+
+# Recursive version
+def reverse_digits_recursive(n, ref=0):
+    if n == 0:  
+        return ref
+    last_digit = n % 10
+    return reverse_digits_recursive(n // 10, ref * 10 + last_digit)
+
+
+# Function to reverse digits using iteration
+def reverse_digits_iterative(n):
+    ref = 0
+    while n > 0:
+        last_digit = n % 10
+        ref = ref * 10 + last_digit
+        n //= 10
+    return ref
+
+num = 1234
+print("Recursive Output:", reverse_digits_recursive(num))
+print("Iterative Output:", reverse_digits_iterative(num))
--- labs-design-analysis-algorithms/lab-01/BT23F05F049/task_sum_even_numbers.py
+++ labs-design-analysis-algorithms/lab-01/BT23F05F049/task_sum_even_numbers.py
@@ -14,3 +14,20 @@
 # - Base case for recursion: if n <= 1, return 0
 # - Recursive step: if n is even, add n and recurse on n-2
 # - Use `range()` with a step of 2 for the iterative version
+
+# Recursive version
+def sum_even_recursive(n):
+    if n <= 1:
+        return 0
+    if n%2==0:
+        return n + sum_even_recursive(n-2)
+    
+# Iterative version
+def sum_even_iterative(n):
+    total = 0
+    for i in range(2, n + 1, 2): 
+        total += i
+    return total    
+n=10
+print("Recursive sum:", sum_even_recursive(n))
+print("Iterative sum:", sum_even_iterative(n))
