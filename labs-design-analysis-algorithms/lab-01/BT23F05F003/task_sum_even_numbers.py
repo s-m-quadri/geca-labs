aac509212663895731d31e974d5fb0ba69090e6c
bot-s-m-quadri <access.smq@gmail.com>
2025-08-25 15:34:51 +0000

    Organize submission for BT23F05F003 (merge into sub-lab-daa-01)

--- labs-design-analysis-algorithms/lab-01/BT23F05F003/task_sum_even_numbers.py
+++ labs-design-analysis-algorithms/lab-01/BT23F05F003/task_sum_even_numbers.py
@@ -14,3 +14,23 @@
 # - Base case for recursion: if n <= 1, return 0
 # - Recursive step: if n is even, add n and recurse on n-2
 # - Use `range()` with a step of 2 for the iterative version
+
+n = 10
+
+def sum_even_recursive(n, sum=0):
+    if n<=1:
+        return 0
+    if n%2==0:
+        return n + sum_even_recursive(n-2)
+    else:
+        return sum_even_recursive(n-1)
+    
+
+def sum_even_iterative(n):
+    sum=0
+    for i in range(2, n+1, 2):
+        sum = sum + i
+    return sum
+
+print("Recursive output : ", sum_even_recursive(n))
+print("Iterative output : ", sum_even_iterative(n))
