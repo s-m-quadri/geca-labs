65f7307f6825721581243758be7fab77b9f9bb66
122raunak <166008527+122raunak@users.noreply.github.com>
2025-08-25 09:55:51 +0000

    completed the task

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,3 +9,18 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
+
+
+def find_peak(arr):
+    n = len(arr)
+    low, high = 0, n - 1
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] < arr[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+    return arr[low]
+
+print(find_peak([1, 3, 7, 12, 9, 5, 2]))
+print(find_peak([0, 2, 4, 6, 3, 1]))
