52c6fb8293959217074ee103cbd6eed285bca7bb
tanvi28pilawan <166020773+tanvi28pilawan@users.noreply.github.com>
2025-08-25 09:54:02 +0000

    lab_03

--- binary_search.py
+++ binary_search.py
@@ -3,10 +3,20 @@
 # Follow along to see the classic binary search in action!
 
 def binary_search_recursive(arr, low, high, x):
-    """Recursive binary search"""
-    # TODO
+   # Recursive binary search
+def binary_search_recursive(arr, low, high, x):
+    if low > high:
+        return -1
+    mid = (low + high) // 2
+    if arr[mid] == x:
+        return mid
+    elif x < arr[mid]:
+        return binary_search_recursive(arr, low, mid - 1, x)
+    else:
+        return binary_search_recursive(arr, mid + 1, high, x)
+ 
 
-# Try it out
+
 arr = [1, 3, 5, 7, 9, 11]
 x = 7
 print("Recursive:", binary_search_recursive(arr, 0, len(arr)-1, x))
