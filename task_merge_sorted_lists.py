ce6676bf13d96390e465ca04a1490b105de7d57f
bot-s-m-quadri <access.smq@gmail.com>
2025-08-25 16:00:49 +0000

    Organize submission for BT23F05F046 (merge into sub-lab-daa-02)

--- task_merge_sorted_lists.py
+++ task_merge_sorted_lists.py
@@ -14,33 +14,3 @@
 # - No recursion required.
 
 # Write your solution here
-def merge_sorted_lists(list1, list2):
-    merged_list = []
-    i, j = 0, 0
-
-    # Traverse both lists and append the smaller element to merged_list
-    while i < len(list1) and j < len(list2):
-        if list1[i] < list2[j]:
-            merged_list.append(list1[i])
-            i += 1
-        else:
-            merged_list.append(list2[j])
-            j += 1
-
-    # If there are remaining elements in list1, append them
-    while i < len(list1):
-        merged_list.append(list1[i])
-        i += 1
-
-    # If there are remaining elements in list2, append them
-    while j < len(list2):
-        merged_list.append(list2[j])
-        j += 1
-
-    return merged_list
-# Example usage
-list1 = [1, 3, 5]
-list2 = [2, 4, 6]           
-
-result = merge_sorted_lists(list1, list2)
-print(result)  # Output: [1, 2, 3, 4, 5, 6]
