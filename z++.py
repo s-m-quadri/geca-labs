6a761c6dc8eeebbfdad6785f8e66036d976e71c9
bot-s-m-quadri <access.smq@gmail.com>
2025-08-25 15:55:32 +0000

    Organize submission for BT24S05F007 (merge into sub-lab-daa-00)

--- z++.py
+++ z++.py
@@ -4,9 +4,4 @@
 #  - Only initials (e.g., A. B. C.)
 
 # 💡 TIP:
-# Use `.split()` and `.upper()` / `.lower()`name = input("Enter full name: ")
-parts = name.split()
-initials = '. '.join(p[0].upper() for p in parts) + '.'
-print("Lower:", name.lower())
-print("Upper:", name.upper())
-print("Initials:", initials)
+# Use `.split()` and `.upper()` / `.lower()`
