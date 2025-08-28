90cd55cb7cda6cd181a653c33f7515e60c712269
Tanisha-Deshmukh <tanishadeshmukh38@gmail.com>
2025-08-28 06:44:47 +0000

    submission by BT23F05F016 This reverts commit 4d407a964a5200df3d2f5216a5cc7a8126053de7.

--- task_count_inversions.py
+++ task_count_inversions.py
@@ -12,3 +12,48 @@
 # - Do not use brute force O(n^2) method.
 
 # Write your solution here
+public class InversionCount {
+
+    static int mergeAndCount(int[] arr, int l, int m, int r) {
+        int[] left = new int[m - l + 1];
+        int[] right = new int[r - m];
+        
+        for (int i = 0; i < left.length; i++)
+            left[i] = arr[l + i];
+        for (int j = 0; j < right.length; j++)
+            right[j] = arr[m + 1 + j];
+        
+        int i = 0, j = 0, k = l, swaps = 0;
+        
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+                swaps += (left.length - i); // all remaining left elements are greater
+            }
+        }
+        
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+        
+        return swaps;
+    }
+
+    static int mergeSortAndCount(int[] arr, int l, int r) {
+        int count = 0;
+        if (l < r) {
+            int m = (l + r) / 2;
+            count += mergeSortAndCount(arr, l, m);
+            count += mergeSortAndCount(arr, m + 1, r);
+            count += mergeAndCount(arr, l, m, r);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        int result = mergeSortAndCount(arr, 0, arr.length - 1);
+        System.out.println("Number of inversions: " + result);
+    }
+}
