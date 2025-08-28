74ee9b238aa92e30650f549f724351a2b1f1fa78
Tanvi_Wankhede <117929623+Tanviwankhede@users.noreply.github.com>
2025-08-28 11:50:57 +0000

    completed

--- task_count_inversions.py
+++ task_count_inversions.py
@@ -14,7 +14,7 @@
 # Write your solution here
 def merge(left, right):
     """Merge two sorted lists into one sorted list."""
-    result = 0
+    result = 0 
     i = j = 0
     while i < len(left) and j < len(right):
         if left[i] > right[j] and i<j:
