3fa38ebf48fe61f92aa341143e6cca59a2cd15f3
BhushanHage <166019971+BhushanHage@users.noreply.github.com>
2025-08-29 14:44:52 +0000

    v.py completed

--- v.py
+++ v.py
@@ -5,3 +5,8 @@
 
 # 💡 TIP:
 # Use `import math`, and remember degrees must be converted to radians.
+import math     
+print("Square root of 144:", math.sqrt(144))
+print("Value of π:", math.pi)   
+print("sin(90 degrees):", math.sin(math.radians(90)))
+    
