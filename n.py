82a6b3645425f146edbc612e2e308418fca6a4a0
BhushanHage <166019971+BhushanHage@users.noreply.github.com>
2025-08-29 14:37:35 +0000

    n.py completed

--- n.py
+++ n.py
@@ -3,3 +3,9 @@
 
 # 💡 TIP:
 # Use `def`, `return`, and call it like `f(2, 3)`.
+def add(x, y):
+    return x + y        
+result = add(5, 7)
+print("Sum:", result)           
+
+                            
