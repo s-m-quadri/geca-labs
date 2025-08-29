b81ebfd1c291bf57d6e4e48c01bc2ac11b5546c4
Vednarayan Hiralkar <168326157+Ved3205@users.noreply.github.com>
2025-08-29 05:47:55 +0000

    complete h.py

--- h.py
+++ h.py
@@ -4,3 +4,9 @@
 
 # 💡 TIP:
 # Use indexing like list[0], list[-1], and list[2] to access or change values.
+my_list=[11,12,13,14,15,16]
+print("First: ",my_list[0])
+print("Middle:", my_list[len(my_list) // 2])
+my_list[2]=100
+print("Last:",my_list[len(my_list)-1])
+print(my_list)
