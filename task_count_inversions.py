f45b0df65fdccac84734f833eb2533384033e018
bot-s-m-quadri <access.smq@gmail.com>
2025-08-31 05:59:32 +0000

    Organize submission for BT24S05F001 (merge into sub-lab-daa-02)

--- task_count_inversions.py
+++ task_count_inversions.py
@@ -12,32 +12,3 @@
 # - Do not use brute force O(n^2) method.
 
 # Write your solution here
-def merge_sort_count(arr):
-    if len(arr) <= 1:
-        return arr, 0
-    mid = len(arr) // 2
-    left, inv_left = merge_sort_count(arr[:mid])
-    right, inv_right = merge_sort_count(arr[mid:])
-    merged, inv_split = merge_count(left, right)
-    return merged, inv_left + inv_right + inv_split
-
-def merge_count(left, right):
-    i = j = inv_count = 0
-    merged = []
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged.append(left[i])
-            i += 1
-        else:
-            merged.append(right[j])
-            inv_count += len(left) - i
-            j += 1
-    merged.extend(left[i:])
-    merged.extend(right[j:])
-    return merged, inv_count
-
-def count_inversions(arr):
-    _, count = merge_sort_count(arr)
-    return count
-
-print(count_inversions([2, 4, 1, 3, 5]))
