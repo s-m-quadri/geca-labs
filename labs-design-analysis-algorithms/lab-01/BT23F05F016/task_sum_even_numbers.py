871739ee31e5d85f435cbe94602603a9a0a0e3e8
bot-s-m-quadri <access.smq@gmail.com>
2025-08-31 05:45:22 +0000

    Organize submission for BT23F05F016 (merge into sub-lab-daa-01)

--- labs-design-analysis-algorithms/lab-01/BT23F05F016/task_sum_even_numbers.py
+++ labs-design-analysis-algorithms/lab-01/BT23F05F016/task_sum_even_numbers.py
@@ -14,3 +14,19 @@
 # - Base case for recursion: if n <= 1, return 0
 # - Recursive step: if n is even, add n and recurse on n-2
 # - Use `range()` with a step of 2 for the iterative version
+import java.util.Scanner;
+
+public class SumEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 2; i <= n; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("Sum of Even Numbers up to " + n + " = " + sum);
+    }
+}
