b8cafadff98ba155e771dc39575af3e3291e221f
bot-s-m-quadri <access.smq@gmail.com>
2025-08-31 05:18:37 +0000

    Organize submission for BT23F05F030 (merge into sub-lab-daa-01)

--- task_print_array_backward.py
+++ task_print_array_backward.py
@@ -14,32 +14,3 @@
 # - For recursion, start from last index: len(arr) - 1
 # - Reduce index by 1 each call
 # - For iteration, use a loop from end to start
-
-#To do
-
-
-# -------------------------------------------
-# TASK: Print Elements of Array in Reverse
-# -------------------------------------------
-
-# Recursive approach
-def print_reverse_recursive(arr, index=None):
-    if index is None:  
-        index = len(arr) - 1
-    if index < 0:
-        return
-    print(arr[index], end=" ")
-    print_reverse_recursive(arr, index - 1)
-
-# Iterative approach
-def print_reverse_iterative(arr):
-    for i in range(len(arr) - 1, -1, -1):
-        print(arr[i], end=" ")
-
-
-arr = [1, 2, 3, 4]
-
-print("Recursive Output:")
-print_reverse_recursive(arr)   
-print("\nIterative Output:")
-print_reverse_iterative(arr)  
