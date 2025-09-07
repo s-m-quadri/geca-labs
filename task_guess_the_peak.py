f7261d6c7b0dc5ed7b255238d019fc85b40065e7
potdarapurva432 <166023784+potdarapurva432@users.noreply.github.com>
2025-09-07 17:06:07 +0000

    Completed lab 03

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,3 +9,19 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
+
+def peak_index_in_mountain(arr):
+    low, high = 0, len(arr) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] < arr[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+    return low
+
+def peak_value_in_mountain(arr):
+    return arr[peak_index_in_mountain(arr)]
+
+print(peak_value_in_mountain([1, 3, 7, 12, 9, 5, 2]))
+print(peak_value_in_mountain([0, 2, 4, 6, 3, 1]))
