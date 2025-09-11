47b729c4f3651121a8b0f910b785c65faf09f966
Gangaprasad Urekar <166024180+gangaprasad29@users.noreply.github.com>
2025-09-11 11:12:05 +0000

    Complete binary_search.py
    I've Completed my  lab,
    by solving binary_search.py on GitHub codespace.

--- binary_search.py
+++ binary_search.py
@@ -2,6 +2,7 @@
 # Basic binary search implementations using recursion
 # Follow along to see the classic binary search in action!
 
+
 def binary_search_recursive(arr, low, high, x):
     """Recursive binary search"""
     # TODO
