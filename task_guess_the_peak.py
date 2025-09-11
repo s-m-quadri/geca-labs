4444e35c326b3250ad8b4ba32bcdd3c8b5823e3a
Gangaprasad Urekar <166024180+gangaprasad29@users.noreply.github.com>
2025-09-11 11:17:16 +0000

    Complete task_guess_the_peak.py
    I've Completed my  lab,
    by solving task_guess_the_peak.py on GitHub codespace.

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,3 +9,20 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
+
+
+def find_peak_mountain(arr):
+    low, high = 0, len(arr) - 1
+
+    while low < high:
+        mid = low + (high - low) // 2
+        if arr[mid] < arr[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return arr[low]
+
+# Example usage
+print(find_peak_mountain([1, 3, 7, 12, 9, 5, 2])) 
+print(find_peak_mountain([0, 2, 4, 6, 3, 1]))      
