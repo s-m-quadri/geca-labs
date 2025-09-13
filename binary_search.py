51994adbe9bb27405b84cb1bdae404af76a7100f
bot-s-m-quadri <access.smq@gmail.com>
2025-09-13 04:45:14 +0000

    Organize submission for BT23F05F025 (merge into sub-lab-daa-03)

--- binary_search.py
+++ binary_search.py
@@ -3,16 +3,10 @@
 # Follow along to see the classic binary search in action!
 
 def binary_search_recursive(arr, low, high, x):
-    if high >= low:
-        mid = (low + high) // 2
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] > x:
-            return binary_search_recursive(arr, low, mid - 1, x)
-        else:
-            return binary_search_recursive(arr, mid + 1, high, x)
-    else:
-        return -1
+    """Recursive binary search"""
+    # TODO
+
+# Try it out
 arr = [1, 3, 5, 7, 9, 11]
 x = 7
 print("Recursive:", binary_search_recursive(arr, 0, len(arr)-1, x))
--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,18 +9,3 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
-def smallest_missing_positive(arr):
-    low, high = 0, len(arr) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == mid + 1:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return low + 1
-
-print(smallest_missing_positive([1, 2, 3, 5, 6]))
-print(smallest_missing_positive([2, 3, 4, 5]))
-print(smallest_missing_positive([1, 2, 3, 4, 5]))
-print(smallest_missing_positive([1]))
-
