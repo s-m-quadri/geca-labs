1982a930e3e2a033b18c17a989cdc58601a5904a
Saiesh Vieshnow Mohandas Sharma <161768278+SaieshSharma@users.noreply.github.com>
2025-09-15 08:31:02 +0000

    Lab Programs completed

--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,3 +9,17 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
+
+
+def find_smallest_missing(arr):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == mid + 1:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low + 1
+
+print(find_smallest_missing([1, 2, 3, 5, 6])) 
+print(find_smallest_missing([2, 3, 4, 5])) 
