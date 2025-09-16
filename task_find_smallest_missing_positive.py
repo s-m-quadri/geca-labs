adffc3b50c9257bb26564dc55ff8392f9232c657
komal-kharatmol <kharatmolkomal250@gmail.com>
2025-09-16 05:28:27 +0000

    Completed Lab-3 .

--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,3 +9,25 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
+
+def find_smallest_missing(arr):
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        if arr[mid] == mid + 1:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return left + 1
+
+# Example test cases:
+
+print(find_smallest_missing([1, 2, 3, 5, 6]))  # Output: 4
+print(find_smallest_missing([2, 3, 4, 5]))     # Output: 1
+print(find_smallest_missing([]))               # Output: 1
+
+
