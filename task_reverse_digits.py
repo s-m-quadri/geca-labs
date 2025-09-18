d33d6ac4604d824e8feca99da789ce373c6e4e9e
bot-s-m-quadri <access.smq@gmail.com>
2025-09-18 08:38:19 +0000

    Organize submission for BT23F05F019 (merge into sub-lab-daa-01)

--- task_reverse_digits.py
+++ task_reverse_digits.py
@@ -14,34 +14,3 @@
 # - Recursive version can peel off last digit using n % 10
 # - You may pass an extra parameter (e.g., accumulator) if needed
 # - Iterative version: use while-loop and integer math
-
-# Recursive version
-def reverse_digits_recursive(n, reversed_num=0):
-    if n == 0:
-        return reversed_num
-    
-    last_digit = n % 10
-    reversed_num = reversed_num * 10 + last_digit
-    return reverse_digits_recursive(n // 10, reversed_num)
-
-# Iterative version
-def reverse_digits_iterative(n):
-    reversed_num = 0
-    while n > 0:
-        last_digit = n % 10
-        reversed_num = reversed_num * 10 + last_digit
-        n = n // 10
-    return reversed_num
-
-# Test both functions
-test_number = 1234
-print("Original number:", test_number)
-
-print("Recursive reverse:", reverse_digits_recursive(test_number))
-print("Iterative reverse:", reverse_digits_iterative(test_number))
-
-# Test with another number
-test_number2 = 56789
-print("\nOriginal number:", test_number2)
-print("Recursive reverse:", reverse_digits_recursive(test_number2))
-print("Iterative reverse:", reverse_digits_iterative(test_number2))
