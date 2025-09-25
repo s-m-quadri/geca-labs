010526ce113dd0b74d7a82e42662367bb97d9b17
bot-s-m-quadri <access.smq@gmail.com>
2025-09-25 15:40:13 +0000

    Organize submission for BT24S05F003 (merge into sub-lab-daa-05)

--- labs-design-analysis-algorithms/lab-05/BT24S05F003/task_5.1.py
+++ labs-design-analysis-algorithms/lab-05/BT24S05F003/task_5.1.py
@@ -19,3 +19,19 @@ V = 4
 # parent = [-1, -1, -1, -1]
 # mstSet = [False, False, False, False]
 """
+def initialize_prim(V):
+    # Use float('inf') for infinity
+    key = [float('inf')] * V
+    parent = [-1] * V
+    mstSet = [False] * V
+    return key, parent, mstSet
+
+
+# Test case
+V = 4
+key, parent, mstSet = initialize_prim(V)
+
+print("key    =", key)
+print("parent =", parent)
+print("mstSet =", mstSet)
+
