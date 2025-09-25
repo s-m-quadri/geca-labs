326341b6a917382e845fc229b409c8f6ceb2175d
Vednarayan Hiralkar <168326157+Ved3205@users.noreply.github.com>
2025-09-25 15:09:31 +0000

    Submission of Lab 02 by BT23F05F028

--- task_merge_sorted_lists.py
+++ task_merge_sorted_lists.py
@@ -18,7 +18,7 @@ def merge_sorted_lists(list1, list2):
     merged = []
     i, j = 0, 0
 
-    # Compare elements from both lists and add the smallest to merged
+    # Merge the two lists while both have elements
     while i < len(list1) and j < len(list2):
         if list1[i] <= list2[j]:
             merged.append(list1[i])
@@ -27,19 +27,19 @@ def merge_sorted_lists(list1, list2):
             merged.append(list2[j])
             j += 1
 
-    # If elements are left in list1, extend merged with them
+    # Append any remaining elements from list1
     while i < len(list1):
         merged.append(list1[i])
         i += 1
 
-    # If elements are left in list2, extend merged with them
+    # Append any remaining elements from list2
     while j < len(list2):
         merged.append(list2[j])
         j += 1
 
     return merged
 
-# Example usage:
+
 list1 = [1, 3, 5]
 list2 = [2, 4, 6]
 print(merge_sorted_lists(list1, list2))  # Output: [1, 2, 3, 4, 5, 6]
