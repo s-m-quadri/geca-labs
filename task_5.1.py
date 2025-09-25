d2a174ed8a3aaec3485138cb135ba7477c2aff4c
tejs11 <tejaswinibankewar2311@gmail.com>
2025-09-25 17:19:28 +0000

    complted lab 05

--- task_5.1.py
+++ task_5.1.py
@@ -19,3 +19,18 @@ V = 4
 # parent = [-1, -1, -1, -1]
 # mstSet = [False, False, False, False]
 """
+
+def initialize_arrays(V):
+    
+    key = [float('inf')] * V
+    parent = [-1] * V
+    mstSet = [False] * V
+    return key, parent, mstSet
+
+
+V = 4
+key, parent, mstSet = initialize_arrays(V)
+
+print("key    =", key)
+print("parent =", parent)
+print("mstSet =", mstSet)
