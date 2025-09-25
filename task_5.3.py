5affa9cd92a3e2269382da9af3ee46045ab0a4a5
bot-s-m-quadri <access.smq@gmail.com>
2025-09-25 15:41:00 +0000

    Organize submission for BT23F05F039 (merge into sub-lab-daa-05)

--- task_5.3.py
+++ task_5.3.py
@@ -26,30 +26,3 @@ parent = [-1, -1, -1]
 # key = [0, 2, ∞]
 # parent = [-1, 0, -1]
 """
-import math
-
-def update_keys(graph, u, key, parent, mstSet):
-    V = len(graph)
-    for v in range(V):
-        if graph[u][v] != 0 and not mstSet[v] and graph[u][v] < key[v]:
-            key[v] = graph[u][v]
-            parent[v] = u
-
-
-graph = [
-    [0, 2, 0],
-    [2, 0, 3],
-    [0, 3, 0]
-]
-u = 0
-key = [0, float('inf'), float('inf')]
-mstSet = [True, False, False]
-parent = [-1, -1, -1]
-
-update_keys(graph, u, key, parent, mstSet)
-
-def display_list(lst):
-    return ["∞" if (isinstance(x, float) and math.isinf(x)) else x for x in lst]
-
-print("key    =", display_list(key))
-print("parent =", parent)
