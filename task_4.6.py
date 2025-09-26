e237e94d314948aed2a8522a7762131cb49d824d
riyagobade <166010013+riyagobade@users.noreply.github.com>
2025-09-26 06:16:10 +0000

    Complete task 6 of lab 04

--- task_4.6.py
+++ task_4.6.py
@@ -1,22 +1,14 @@
-# Task 4.6: Challenge – Activity Selection Problem
-# ----------------------------------------------
-# This is a classic greedy scheduling problem.
-#
-# Problem:
-# You are given n activities with start and finish times.
-# Select the maximum number of activities that can be performed by one person,
-# assuming that a person can only work on a single activity at a time.
-#
-# Input:
-# - List of activities with (start_time, finish_time)
-#
-# Output:
-# - Maximum number of non-overlapping activities that can be performed.
-#
-# Example:
-# Activities = [(1,2), (3,4), (0,6), (5,7), (8,9), (5,9)]
-# Output: 4 activities (choose: (1,2), (3,4), (5,7), (8,9))
-#
-# Hint: Sort activities by finish time, then pick the next compatible activity.
-#
-# This is your challenge task for the nerds!
+def activity_selection(activities):
+    activities.sort(key=lambda x: x[1])
+    selected = []
+    last_finish = -1
+    for start, finish in activities:
+        if start >= last_finish:
+            selected.append((start, finish))
+            last_finish = finish
+    return selected
+
+activities = [(1,2), (3,4), (0,6), (5,7), (8,9), (5,9)]
+selected = activity_selection(activities)
+print("Maximum number of activities:", len(selected))
+print("Selected activities:", selected)
