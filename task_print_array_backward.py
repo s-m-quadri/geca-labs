98de96c0cc90b8e5113b4bdb784e510a205997ed
GAYATRI SANTOSH DABARE <gayatridabare@gmail.com>
2025-09-26 07:24:53 +0000

    Complete labs

--- task_print_array_backward.py
+++ task_print_array_backward.py
@@ -14,3 +14,19 @@
 # - For recursion, start from last index: len(arr) - 1
 # - Reduce index by 1 each call
 # - For iteration, use a loop from end to start
+def print_reverse_recursive(arr,i=None):
+    if i is None:
+        i= len(arr)-1
+    if i <0:
+        return 
+
+    print(arr[i],end =" ")
+    print_reverse_recursive(arr,i-1 )
+
+Test_array = [1, 2, 3, 4, 5, ]
+print ("array:",Test_array)
+
+print("Recursive reverse :" ,end=" ")
+print_reverse_recursive(Test_array)
+print 
+ 
