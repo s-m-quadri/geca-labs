00e80d0f1cbc8f4bb6174e09e102dcd8fa54e53d
potdarapurva432 <166023784+potdarapurva432@users.noreply.github.com>
2025-09-29 10:01:45 +0000

    completed task_5.1.py

--- task_5.1.py
+++ task_5.1.py
@@ -26,3 +26,10 @@ def initialize_prim_arrays(V):
     parent = [-1] * V
     mstSet = [False] * V
     return key, parent, mstSet
+
+V = 4
+key, parent, mstSet = initialize_prim_arrays(V)
+print("key    =", key)
+print("parent =", parent)
+print("mstSet =", mstSet)
+
