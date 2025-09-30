331c07e586c80ea2a85c37ae158005bb69b92703
ShivamAtram <shivamatram2002@gmail.com>
2025-09-30 10:19:54 +0000

    Completed task_4.1.py

--- task_4.1.py
+++ task_4.1.py
@@ -13,3 +13,18 @@
 #
 # Hint: Start with the mathematical definition:
 # factorial(n) = 1 if n == 0 else n * factorial(n-1)
+
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    return  n * factorial_recursive(n - 1)
+
+print(factorial_recursive(5))
+
+def factorial_iterative(n):
+    fact = 1;
+    for i in range(1,n+1):
+        fact *= i
+    return fact
+
+print(factorial_iterative(5))
