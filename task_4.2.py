31bb7cbd588737f488a57854cd5e981c5a918150
Bhagyashree200527 <bhagyashreekere2005@gmail.com>
2025-10-08 08:49:51 +0000

    completed

--- task_4.2.py
+++ task_4.2.py
@@ -12,3 +12,31 @@
 # Output: 8
 #
 # Bonus: Try printing the whole Fibonacci sequence up to n instead of just nth number.
+def fib_recursive(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+def fib_iterative(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+def fib_sequence(n):
+    sequence = []
+    for i in range(n + 1):
+        sequence.append(fib_iterative(i))
+    return sequence
+
+n = int(input())
+print(fib_recursive(n))
+print(fib_iterative(n))
+print(fib_sequence(n))
