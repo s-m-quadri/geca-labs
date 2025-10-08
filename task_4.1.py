9b870a4413f85d41d4fb71d5b486dd3b95281b9e
bot-s-m-quadri <access.smq@gmail.com>
2025-10-08 17:23:11 +0000

    Organize submission for BT23F05F004 (merge into sub-lab-daa-04)

--- task_4.1.py
+++ task_4.1.py
@@ -1,4 +1,4 @@
-# Task 4.1: Factorial (Recursive vs Iterative)//
+# Task 4.1: Factorial (Recursive vs Iterative)
 # -----------------------------------------
 # Write two functions:
 # 1. factorial_recursive(n): Uses recursion to compute factorial of n.
@@ -13,23 +13,3 @@
 #
 # Hint: Start with the mathematical definition:
 # factorial(n) = 1 if n == 0 else n * factorial(n-1)
-
-
-
-def factorial_recursive(n):
-    if n < 0:
-        raise ValueError("Input must be a non-negative integer.")
-    if n == 0:
-        return 1
-    return n * factorial_recursive(n - 1)
-
-def factorial_iterative(n):
-    if n < 0:
-        raise ValueError("Input must be a non-negative integer.")
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-print("Recursive:", factorial_recursive(5))  
-print("Iterative:", factorial_iterative(5)) 
--- task_4.4.py
+++ task_4.4.py
@@ -20,36 +20,3 @@
 # Output: 240.0
 #
 # Hint: Use sorting and simple loops.
-
-# Task 4.4: Fractional Knapsack
-# -----------------------------
-
-def fractional_knapsack(W, items):
-    
-    items = sorted(items, key=lambda x: x[0] / x[1], reverse=True)
-
-    total_value = 0.0  
-    remaining_capacity = W
-
-    for value, weight in items:
-        if remaining_capacity == 0:
-            break
-
-        if weight <= remaining_capacity:
-            
-            total_value += value
-            remaining_capacity -= weight
-        else:
-            
-            fraction = remaining_capacity / weight
-            total_value += value * fraction
-            remaining_capacity = 0  
-    return total_value
-
-
-if __name__ == "__main__":
-    capacity = 50
-    items = [(60, 10), (100, 20), (120, 30)]
-
-    max_value = fractional_knapsack(capacity, items)
-    print("Maximum achievable value:", max_value)  
