393c077c7019f1051844b65fed433ae88fd1b97f
Bhagyashree200527 <bhagyashreekere2005@gmail.com>
2025-10-08 08:51:29 +0000

    completed

--- task_4.3.py
+++ task_4.3.py
@@ -12,3 +12,17 @@
 # Output: "olleh"
 #
 # Bonus: Try solving without using Python slicing [::-1].
+def reverse_recursive(s):
+    if s == "":
+        return ""
+    return reverse_recursive(s[1:]) + s[0]
+
+def reverse_iterative(s):
+    result = ""
+    for char in s:
+        result = char + result
+    return result
+
+s = input()
+print(reverse_recursive(s))
+print(reverse_iterative(s))
