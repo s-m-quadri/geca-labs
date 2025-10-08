2fe033ddb6fc26e02f3fbc1e7b56ec18f59ea7e7
Prajakta Dutonde <prajaktadutonde9@gmail.com>
2025-10-08 11:31:42 +0000

    Completed

--- task_5.1.py
+++ task_5.1.py
@@ -20,4 +20,24 @@ V = 4
 # mstSet = [False, False, False, False]
 """
 #edit 
+def initialize_prim(V):
+    
+    key = [float('inf')] * V
+    
+    
+    parent = [-1] * V
+    
+   
+    mstSet = [False] * V
+    
+    return key, parent, mstSet
+
+
+
+V = 4
+key, parent, mstSet = initialize_prim(V)
+
+print("key =", key)
+print("parent =", parent)
+print("mstSet =", mstSet)
 
