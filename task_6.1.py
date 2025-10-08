32620c8d29bf8257027d4c76538195f2bcf64d66
Sarkaar <bawaskarsiddhesh@gmail.com>
2025-10-08 19:45:37 +0000

    Submission of Lab 06 by BT23F05F005

--- task_6.1.py
+++ task_6.1.py
@@ -10,3 +10,16 @@
 
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
+
+#solution
+def create_edge_list(edges):
+    edge_list = []
+    for u, v, w in edges:
+        edge_list.append((u, v, w))
+    return edge_list
+
+# Example (very important)
+edges = [(6, 1, 2), (4, 2, 8), (5, 3, 1)]
+result = create_edge_list(edges)
+print("Edge List:", result)  
+
