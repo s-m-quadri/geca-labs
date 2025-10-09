db2935ee013d394ab6d0394724110cdb2c66561f
bot-s-m-quadri <access.smq@gmail.com>
2025-10-09 16:50:36 +0000

    Organize submission for BT23F05F005 (merge into sub-lab-daa-03)

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,22 +9,3 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
-
-#solution:
-def find_peak(arr):
-	left, right = 0, len(arr) - 1
-	while left < right:
-		mid = (left + right) // 2
-		if arr[mid] < arr[mid + 1]:
-			left = mid + 1
-		else:
-			right = mid
-	return arr[left]
-
-# Example (much needed):
-if __name__ == "__main__":
-	arr1 = [1, 4, 5, 7, 34, 54, 12, 30]
-	print(find_peak(arr1))  #i'd expect 54 as output
-
-	arr2 = [1, 2, 3, 4, 5, 3, 1]
-	print(find_peak(arr2))  #and here 5 as output
