35015512a373b56b3eac2064a0ec449fb90e3626
bot-s-m-quadri <access.smq@gmail.com>
2025-10-09 17:35:57 +0000

    Organize submission for BT23F05F018 (merge into sub-lab-daa-05)

--- labs-design-analysis-algorithms/lab-05/BT23F05F018/task_5.3.py
+++ labs-design-analysis-algorithms/lab-05/BT23F05F018/task_5.3.py
@@ -26,3 +26,26 @@ parent = [-1, -1, -1]
 # key = [0, 2, ∞]
 # parent = [-1, 0, -1]
 """
+def update_keys(graph, u, key, parent, mstSet):
+    V = len(graph)
+    for v in range(V):
+        if graph[u][v] != 0 and not mstSet[v] and graph[u][v] < key[v]:
+            key[v] = graph[u][v]
+            parent[v] = u
+
+
+# Test case
+graph = [
+    [0, 2, 0],
+    [2, 0, 3],
+    [0, 3, 0]
+]
+u = 0
+key = [0, float('inf'), float('inf')]
+mstSet = [True, False, False]
+parent = [-1, -1, -1]
+
+update_keys(graph, u, key, parent, mstSet)
+
+print("key =", key)
+print("parent =", parent)
