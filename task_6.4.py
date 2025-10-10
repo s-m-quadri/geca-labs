89d789ab121df473b4023fff179b65d837d7b4c3
bot-s-m-quadri <access.smq@gmail.com>
2025-10-10 16:52:23 +0000

    Organize submission for BT24S05F005 (merge into sub-lab-daa-06)

--- task_6.4.py
+++ task_6.4.py
@@ -11,33 +11,3 @@
 
 # Hint: Use recursion for find().
 # Tip: Try multiple unions, like (0,1), (1,2).
-def initialize_parent(n):
-    return [i for i in range(n)]
-
-def find(parent, x):
-    """
-    Finds the root parent of x using recursion.
-    
-    Parameters:
-    parent (list): The parent array.
-    x (int): The element to find the root of.
-    
-    Returns:
-    int: Root parent of x.
-    """
-    if parent[x] == x:
-        return x
-    return find(parent, parent[x])
-
-def union(parent, x, y):
-    """
-    Merges the sets that contain x and y.
-
-    Parameters:
-    parent (list): The parent array.
-    x (int), y (int): Elements to union.
-    """
-    root_x = find(parent, x)
-    root_y = find(parent, y)
-    if root_x != root_y:
-        parent[root_y] = root_x  # Attach y's root to x's root
