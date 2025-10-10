e182d20321ff4abdde30e36f44db64ecc7b31ddc
bot-s-m-quadri <access.smq@gmail.com>
2025-10-10 16:55:46 +0000

    Organize submission for BT23F05F007 (merge into sub-lab-daa-05)

--- labs-design-analysis-algorithms/lab-05/BT23F05F007/task_5.2.py
+++ labs-design-analysis-algorithms/lab-05/BT23F05F007/task_5.2.py
@@ -16,4 +16,18 @@ Test case:
 key = [0, 2, 3]
 mstSet = [True, False, False]
 # Expected output: 1
-"""
+"""def min_key_vertex(key, mstSet):
+    min_val = float('inf')
+    min_index = -1
+    for v in range(len(key)):
+        if not mstSet[v] and key[v] < min_val:
+            min_val = key[v]
+            min_index = v
+    return min_index
+
+# Test case
+key = [0, 2, 3]
+mstSet = [True, False, False]
+
+print(min_key_vertex(key, mstSet))
+
