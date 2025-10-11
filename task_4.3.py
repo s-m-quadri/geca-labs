70df99dd4386c49829d9faded016507f4670aafd
Amruta <amrutabedwal05@gmail.com>
2025-10-11 11:41:52 +0000

    I've completed my 4th Lab.

--- task_4.3.py
+++ task_4.3.py
@@ -12,3 +12,25 @@
 # Output: "olleh"
 #
 # Bonus: Try solving without using Python slicing [::-1].
+
+
+# Recursive approach
+def reverse_recursive(s):
+    if len(s) <= 1:
+        return s
+    else:
+        # Take the last char + reverse of the rest
+        return reverse_recursive(s[1:]) + s[0]
+
+
+# Iterative approach
+def reverse_iterative(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str  # prepend each character
+    return reversed_str
+
+
+
+print(reverse_recursive("hello"))   
+print(reverse_iterative("hello"))  
--- task_4.5.py
+++ task_4.5.py
@@ -17,3 +17,27 @@
 #
 # Note: Greedy works with canonical coin systems like Indian/US coins,
 # but may fail with arbitrary denominations. That’s the fun part to test!
+
+
+def coin_change_greedy(denominations, amount):
+    
+    denominations.sort(reverse=True)
+    
+    coins_used = []  
+    remaining = amount
+    
+    for coin in denominations:
+        while remaining >= coin:
+            remaining -= coin
+            coins_used.append(coin)
+    
+    return len(coins_used), coins_used
+
+
+
+denominations = [1, 2, 5, 10, 20, 50, 100]
+amount = 93
+
+count, coins = coin_change_greedy(denominations, amount)
+print(f"Minimum coins needed: {count}")
+print("Coins used:", coins)
