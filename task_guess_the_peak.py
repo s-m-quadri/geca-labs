8cc70b9c9bbba043f14ad5d09be0f6dd1b6c2d62
Tanvi_Wankhede <117929623+Tanviwankhede@users.noreply.github.com>
2025-10-12 09:55:25 +0000

    Completed Lab 03

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,3 +9,24 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
+
+
+def find_peak_mountain(arr):
+    low, high = 0, len(arr) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] < arr[mid + 1]:
+            # Peak is to the right
+            low = mid + 1
+        else:
+          
+            high = mid
+    
+    return arr[low]
+
+arr1 = [1, 3, 7, 12, 9, 5, 2]
+print(find_peak_mountain(arr1)) 
+
+arr2 = [0, 2, 4, 6, 3, 1]
+print(find_peak_mountain(arr2))  
