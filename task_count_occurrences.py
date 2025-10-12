7f2a30df875d9109da8af2e002b0645b17ae5f0c
ArjunDasalkar <arjun.dasalkar2244@gmail.com>
2025-10-12 16:03:03 +0530

    Complete lab 03

--- task_count_occurrences.py
+++ task_count_occurrences.py
@@ -9,3 +9,48 @@
 #
 # Input: arr = [5, 5, 5, 5, 5], target = 5
 # Output: 5
+
+def find_first(arr, target):
+	left, right = 0, len(arr) - 1
+	result = -1
+	while left <= right:
+		mid = (left + right) // 2
+		if arr[mid] == target:
+			result = mid
+			right = mid - 1
+		elif arr[mid] < target:
+			left = mid + 1
+		else:
+			right = mid - 1
+	return result
+
+def find_last(arr, target):
+	left, right = 0, len(arr) - 1
+	result = -1
+	while left <= right:
+		mid = (left + right) // 2
+		if arr[mid] == target:
+			result = mid
+			left = mid + 1
+		elif arr[mid] < target:
+			left = mid + 1
+		else:
+			right = mid - 1
+	return result
+
+def count_occurrences(arr, target):
+	first = find_first(arr, target)
+	if first == -1:
+		return 0
+	last = find_last(arr, target)
+	return last - first + 1
+
+# Examples (much needed):
+if __name__ == "__main__":
+	arr1 = [1, 2, 3, 3, 3, 4, 4, 5,]
+	target1 = 3
+	print(count_occurrences(arr1, target1))  
+
+	arr2 = [3, 4, 4, 4, 5, 5, 5, 5, 5]
+	target2 = 4
+	print(count_occurrences(arr2, target2))  
