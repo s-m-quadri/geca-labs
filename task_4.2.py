6e191ab564e3dad7b7689362c488258580479b87
bot-s-m-quadri <access.smq@gmail.com>
2025-10-12 09:34:52 +0000

    Organize submission for BT23F05F006 (merge into sub-lab-daa-04)

--- task_4.2.py
+++ task_4.2.py
@@ -12,29 +12,3 @@
 # Output: 8
 #
 # Bonus: Try printing the whole Fibonacci sequence up to n instead of just nth number.
-
-# Recursive approach
-def fib_recursive(n):
-    if n < 0:
-        raise ValueError("Fibonacci is not defined for negative numbers.")
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib_recursive(n - 1) + fib_recursive(n - 2)
-
-
-# Iterative approach
-def fib_iterative(n):
-    if n < 0:
-        raise ValueError("Fibonacci is not defined for negative numbers.")
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a + b
-    return a
-
-
-# Example usage:
-print(fib_recursive(6))  
-print(fib_iterative(6))  
