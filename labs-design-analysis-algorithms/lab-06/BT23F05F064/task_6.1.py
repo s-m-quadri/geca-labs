2ecc9daacab456e85b226ac6d7274644257e4ca2
bot-s-m-quadri <access.smq@gmail.com>
2025-10-12 09:33:27 +0000

    Organize submission for BT23F05F064 (merge into sub-lab-daa-06)

--- labs-design-analysis-algorithms/lab-06/BT23F05F064/task_6.1.py
+++ labs-design-analysis-algorithms/lab-06/BT23F05F064/task_6.1.py
@@ -10,3 +10,18 @@
 
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
+
+# Task 6.1: Representing Graph Edges
+# -----------------------------------
+
+def represent_graph_edges(edges):
+    edge_list = []
+    for edge in edges:
+        edge_list.append(edge)
+    return edge_list
+
+if __name__ == "__main__":
+    edges = [(0, 1, 4), (0, 2, 3), (1, 2, 1)]
+    stored_edges = represent_graph_edges(edges)
+    print("Stored edges:", stored_edges)
+
