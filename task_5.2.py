0c9b91dcdf06da64633e4b790f8548eca58fe47e
BhartiKarpe <bhartikarpe05@gmail.com>
2025-10-13 13:42:46 +0000

    Completed lab 5 by BT24S05F008

--- task_5.2.py
+++ task_5.2.py
@@ -17,3 +17,16 @@ key = [0, 2, 3]
 mstSet = [True, False, False]
 # Expected output: 1
 """
+
+def min_key_vertex(key, mstSet):
+    min_val = float('inf')
+    min_index = -1
+    for i in range(len(key)):
+        if not mstSet[i] and key[i] < min_val:
+            min_val = key[i]
+            min_index = i
+    return min_index
+
+key = [0, 2, 3]
+mstSet = [True, False, False]
+print(min_key_vertex(key, mstSet))
