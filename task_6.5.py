a5bbdc548827dbd4399b8f01a2da048fbb21792d
bot-s-m-quadri <access.smq@gmail.com>
2025-10-13 03:39:03 +0000

    Organize submission for BT23F05F014 (merge into sub-lab-daa-06)

--- task_6.5.py
+++ task_6.5.py
@@ -10,38 +10,3 @@
 
 # Hint: Use union-find to check cycle.
 # Tip: Keep track of total weight and chosen edges.
-
-# Solution:
-
-def find(parent, x):
-	if parent[x] != x:
-		return find(parent, parent[x])
-	return x
-
-def union(parent, x, y):
-	root_x = find(parent, x)
-	root_y = find(parent, y)
-	if root_x != root_y:
-		parent[root_y] = root_x
-
-def kruskal_mst(num_vertices, edges):
-	"""
-	Builds MST using Kruskal's algorithm.
-	Returns MST edges and total weight.
-	"""
-	parent = [i for i in range(num_vertices)]
-	sorted_edges = sorted(edges, key=lambda x: x[2])
-	mst_edges = []
-	total_weight = 0
-	for u, v, w in sorted_edges:
-		if find(parent, u) != find(parent, v):
-			union(parent, u, v)
-			mst_edges.append((u, v, w))
-			total_weight += w
-	print("MST edges:", mst_edges)
-	print("MST total weight:", total_weight)
-	return mst_edges, total_weight
-
-if __name__ == "__main__":
-	edges = [(0, 1, 10), (0, 2, 6), (0, 3, 5), (1, 3, 15), (2, 3, 4)]
-	kruskal_mst(4, edges)
