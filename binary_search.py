86101dafc995174a06e7c79af73a866979cf36e1
bot-s-m-quadri <access.smq@gmail.com>
2025-10-13 03:43:28 +0000

    Organize submission for BT24S05F010 (merge into sub-lab-daa-03)

--- binary_search.py
+++ binary_search.py
@@ -3,19 +3,10 @@
 # Follow along to see the classic binary search in action!
 
 def binary_search_recursive(arr, low, high, x):
-    if low > high:
-        return -1  
-    
-    mid = (low + high) // 2
-    
-    if arr[mid] == x:
-        return mid
-    elif arr[mid] < x:
-        return binary_search_recursive(arr, mid + 1, high, x)
-    else:
-        return binary_search_recursive(arr, low, mid - 1, x)
+    """Recursive binary search"""
+    # TODO
 
 # Try it out
 arr = [1, 3, 5, 7, 9, 11]
 x = 7
-print("Recursive:", binary_search_recursive(arr, 0, len(arr)-1, x))  # Output: 3
+print("Recursive:", binary_search_recursive(arr, 0, len(arr)-1, x))
--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,28 +9,3 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
-
-
-def smallest_missing_positive(arr):
-    if not arr or arr[0] != 1:
-        return 1
-    
-    low, high = 0, len(arr) - 1
-    
-    while low <= high:
-        mid = (low + high) // 2
-     
-        if arr[mid] != mid + 1:
-            high = mid - 1
-        else:
-            low = mid + 1
-    
-  
-    return low + 1
-
-
-arr1 = [1, 2, 3, 5, 6]
-print(smallest_missing_positive(arr1)) 
-
-arr2 = [2, 3, 4, 5]
-print(smallest_missing_positive(arr2))  
