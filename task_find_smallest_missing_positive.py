eadaf5f7b5e4dd136817ce2a14b39f8d2b105f26
prathamesh0416 <166020481+prathamesh0416@users.noreply.github.com>
2025-10-13 14:25:29 +0000

    Submission of lab 03 by BT23F05F045

--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,3 +9,16 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
+
+def smallest_missing_positive(arr):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == mid + 1:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low + 1
+
+print(smallest_missing_positive([1, 2, 3, 5, 6]))
+print(smallest_missing_positive([2, 3, 4, 5]))
