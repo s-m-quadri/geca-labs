e98ed678eb7a58708ade368db23623cfe1efd210
pranay-sm <149605837+pranay-sm@users.noreply.github.com>
2025-10-13 14:33:26 +0000

    Completed lab daa 04

--- task_4.6.py
+++ task_4.6.py
@@ -20,3 +20,24 @@
 # Hint: Sort activities by finish time, then pick the next compatible activity.
 #
 # This is your challenge task for the nerds!
+
+
+def activity_selection(activities):
+    activities.sort(key=lambda x: x[1])
+    selected = []
+    last_finish = -1
+    
+    for start, finish in activities:
+        if start >= last_finish:
+            selected.append((start, finish))
+            last_finish = finish
+    return selected
+
+
+# Example usage
+
+activities = [(1,2), (3,4), (0,6), (5,7), (8,9), (5,9)]
+selected_activities = activity_selection(activities)
+print("Selected activities:", selected_activities)
+print("Total activities:", len(selected_activities))
+
