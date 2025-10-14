4b07f32aec5d1d9b9e1547feacf04b1a857937aa
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:47:34 +0000

    Organize submission for BT23F05F044 (merge into sub-lab-daa-06)

--- labs-design-analysis-algorithms/lab-06/BT23F05F044/task_6.3.py
+++ labs-design-analysis-algorithms/lab-06/BT23F05F044/task_6.3.py
@@ -9,3 +9,11 @@
 
 # Hint: Use list comprehension.
 # Tip: No path compression or union by rank yet.
+def create_parent_array(n):
+    parent = [i for i in range(n)]
+    return parent
+
+
+num_vertices = 5
+parent = create_parent_array(num_vertices)
+print(parent)
