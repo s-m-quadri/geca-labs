3a8c188a8f71c80adaccaf08c28e907ac919a625
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:33:20 +0000

    Organize submission for BT23F05F045 (merge into sub-lab-daa-06)

--- labs-design-analysis-algorithms/lab-06/BT23F05F045/task_6.2.py
+++ labs-design-analysis-algorithms/lab-06/BT23F05F045/task_6.2.py
@@ -9,3 +9,10 @@
 
 # Hint: Sort using key = lambda x: x[2]
 # Tip: Test with 5-6 edges to check order.
+
+def sort_edges(edges):
+    return sorted(edges, key=lambda x: x[2])
+
+edges = [(0,1,4), (0,2,3), (1,2,1)]
+sorted_edges = sort_edges(edges)
+print(sorted_edges)
--- task_6.5.py
+++ task_6.5.py
@@ -10,34 +10,3 @@
 
 # Hint: Use union-find to check cycle.
 # Tip: Keep track of total weight and chosen edges.
-
-def find(parent, x):
-    if parent[x] != x:
-        parent[x] = find(parent, parent[x])
-    return parent[x]
-
-def union(parent, x, y):
-    x_root = find(parent, x)
-    y_root = find(parent, y)
-    if x_root != y_root:
-        parent[y_root] = x_root
-
-def kruskal_mst(edges, V):
-    edges = sorted(edges, key=lambda x: x[2])
-    parent = [i for i in range(V)]
-    mst_edges = []
-    mst_weight = 0
-
-    for u, v, w in edges:
-        if find(parent, u) != find(parent, v):
-            union(parent, u, v)
-            mst_edges.append((u, v, w))
-            mst_weight += w
-
-    return mst_edges, mst_weight
-
-edges = [(0,1,10), (0,2,6), (0,3,5), (1,3,15), (2,3,4)]
-V = 4
-mst_edges, mst_weight = kruskal_mst(edges, V)
-print("MST edges:", mst_edges)
-print("MST weight:", mst_weight)
