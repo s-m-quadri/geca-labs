2175316255d10ccae1fc6bb63c36668f1532cdda
Jgargee <gargeejoshi536@gmail.com>
2025-10-14 22:36:45 +0530

    Update task_6.1.py

--- task_6.1.py
+++ task_6.1.py
@@ -10,4 +10,9 @@
 
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
-#to do
+def store_edges(edges):
+    return edges
+
+edges = [(0,1,4), (0,2,3), (1,2,1)]
+stored_edges = store_edges(edges)
+print(stored_edges)
