29c6c1c0d831e9da55e1eef63e9bae66efb13aa0
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 10:26:46 +0000

    Organize submission for BT24S05F004 (merge into sub-lab-daa-06)

--- task_6.1.py
+++ task_6.1.py
@@ -10,30 +10,3 @@
 
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
-# Task 6.1: Representing Graph Edges
-# -----------------------------------
-
-def create_edge_list(edges):
-    """
-    Accepts edges as tuples (u, v, w)
-    and stores them in a list of tuples.
-    
-    Parameters:
-        edges (list of tuples): Each tuple represents (u, v, weight)
-    
-    Returns:
-        list: The same list of edges
-    """
-    edge_list = []
-    
-    # Store all edges in the list
-    for edge in edges:
-        edge_list.append(edge)
-    
-    return edge_list
-
-
-# Example test
-edges = [(0, 1, 4), (0, 2, 3), (1, 2, 1)]
-result = create_edge_list(edges)
-print("Stored Edges:", result)
--- task_6.2.py
+++ task_6.2.py
@@ -9,16 +9,3 @@
 
 # Hint: Sort using key = lambda x: x[2]
 # Tip: Test with 5-6 edges to check order.
-# Task 6.2: Sorting Edges
-# ------------------------
-
-def sort_edges_by_weight(edges):
-    # Sort using weight (3rd element of tuple)
-    sorted_edges = sorted(edges, key=lambda x: x[2])
-    return sorted_edges
-
-
-# Example test
-edges = [(0, 1, 4), (0, 2, 3), (1, 2, 1), (2, 3, 5), (3, 4, 2)]
-result = sort_edges_by_weight(edges)
-print("Sorted Edges:", result)
