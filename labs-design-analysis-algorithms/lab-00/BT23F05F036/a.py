7c5d0ab12d1b8e81d81225148db6f9e7a4987396
s-m-quadri <88645248+s-m-quadri@users.noreply.github.com>
2025-10-14 13:52:06 +0530

    Cleanup

--- labs-design-analysis-algorithms/lab-00/BT23F05F036/a.py
+++
@@ -1,9 +0,0 @@
-# Write a program that prints:
-#  - "Hello, Python!"
-#  - "This is Lab A"
-# This is your first Python program.
-
-# 💡 TIP:
-# Use the `print()` function.
-print("Hello, Python!")
-print("This is lab A")
