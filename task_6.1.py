d30c63dfd0a6b6de6763636ebf7b6d843f34ffdc
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:48:22 +0000

    Organize submission for BT23F05F067 (merge into sub-lab-daa-06)

--- task_6.1.py
+++ task_6.1.py
@@ -10,20 +10,3 @@
 
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
-
-def store_graph_edges(edges):
-    edge_list = []
-    for u, v, w in edges:
-        edge_list.append((u, v, w))
-    return edge_list
-
-n = int(input("Enter number of edges: "))
-edges = []
-for i in range(n):
-    u = int(input(f"Enter starting vertex of edge {i+1}: "))
-    v = int(input(f"Enter ending vertex of edge {i+1}: "))
-    w = int(input(f"Enter weight of edge {i+1}: "))
-    edges.append((u, v, w))
-
-edge_list = store_graph_edges(edges)
-print("Stored edges:", edge_list)
--- task_6.5.py
+++ task_6.5.py
@@ -10,34 +10,3 @@
 
 # Hint: Use union-find to check cycle.
 # Tip: Keep track of total weight and chosen edges.
-
-
-def find(parent, x):
-    if parent[x] != x:
-        return find(parent, parent[x])
-    return x
-
-def union(parent, x, y):
-    xroot = find(parent, x)
-    yroot = find(parent, y)
-    if xroot != yroot:
-        parent[yroot] = xroot
-
-def kruskal_mst(edges, n):
-    edges = sorted(edges, key=lambda x: x[2])
-    parent = [i for i in range(n)]
-    mst = []
-    total_weight = 0
-
-    for u, v, w in edges:
-        if find(parent, u) != find(parent, v):
-            union(parent, u, v)
-            mst.append((u, v, w))
-            total_weight += w
-    return mst, total_weight
-
-edges = [(0,1,10), (0,2,6), (0,3,5), (1,3,15), (2,3,4)]
-n = 4
-mst, total_weight = kruskal_mst(edges, n)
-print("MST edges:", mst)
-print("Total weight:", total_weight)
