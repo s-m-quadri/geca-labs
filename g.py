ccc832c17d778554dc9eb734fdd27d475351de14
Longshlur <suronglongshlur22@gmail.com>
2025-10-14 06:10:59 +0000

    completed g.py

--- g.py
+++ g.py
@@ -1,7 +1,10 @@
-# Print the numbers from 1 to 10 using:
-#  - a for loop
-#  - a while loop
 
-# 💡 TIP:
-# `range(1, 11)` gives 1 through 10.
-# Use `while` with a counter.
+# Print 1 to 10 using a for loop
+for i in range(1, 11):
+	print(i)
+
+# Print 1 to 10 using a while loop
+counter = 1
+while counter <= 10:
+	print(counter)
+	counter += 1
