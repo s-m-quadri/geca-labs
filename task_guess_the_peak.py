0d3f6bd718068990e84dfb0170076f84489b8ecf
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:26:12 +0000

    Organize submission for BT23F05F050 (merge into sub-lab-daa-03)

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,20 +9,3 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
-def find_peak(arr):
-    if not arr:
-        return None
-    low, high = 0, len(arr) - 1
-    while low < high:
-        mid = (low + high) // 2
-        if arr[mid] > arr[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-    return arr[low]
-
-# Examples
-print(find_peak([1, 3, 7, 12, 9, 5, 2]))  # 12
-print(find_peak([0, 2, 4, 6, 3, 1]))      # 6
-
-
