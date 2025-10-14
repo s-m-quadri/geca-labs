df3ac8fb2d520435e1ceb10492bccd55b0771276
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:34:11 +0000

    Organize submission for BT23F05F042 (merge into sub-lab-daa-06)

--- task_6.4.py
+++ task_6.4.py
@@ -11,28 +11,3 @@
 
 # Hint: Use recursion for find().
 # Tip: Try multiple unions, like (0,1), (1,2).
-
-
-def find(parent, x):
-    if parent[x] == x:
-        return x
-    return find(parent, parent[x])
-
-def union(parent, x, y):
-    x_root = find(parent, x)
-    y_root = find(parent, y)
-    if x_root != y_root:
-        parent[y_root] = x_root
-
-
-# Example usage
-
-parent = [0, 1, 2, 3]
-
-union(parent, 0, 1)
-print("Parent after union(0,1):", parent)  # e.g., [0,0,2,3]
-print("Find(1):", find(parent, 1))         # Output: 0
-
-union(parent, 1, 2)
-print("Parent after union(1,2):", parent)  # e.g., [0,0,0,3]
-print("Find(2):", find(parent, 2))         # Output: 0
