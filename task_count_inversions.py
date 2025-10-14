5c4facae9453528d879744fdd0f3c575022f81c5
Aditya Gujarkar <165485333+Aditya-Gujarkar@users.noreply.github.com>
2025-10-14 03:14:17 +0000

    BT23F05F026 lab 2 completed

--- task_count_inversions.py
+++ task_count_inversions.py
@@ -12,3 +12,41 @@
 # - Do not use brute force O(n^2) method.
 
 # Write your solution here
+def count_inversions(arr):
+    def merge_sort(nums):
+        if len(nums) <= 1:
+            return nums, 0
+
+        mid = len(nums) // 2
+        left, left_inv = merge_sort(nums[:mid])
+        right, right_inv = merge_sort(nums[mid:])
+
+        merged, merge_inv = merge(left, right)
+        total_inv = left_inv + right_inv + merge_inv
+
+        return merged, total_inv
+
+    def merge(left, right):
+        result = []
+        i = j = count = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+                count += len(left) - i  
+
+        result.extend(left[i:])
+        result.extend(right[j:])
+
+        return result, count
+
+    _, total_inversions = merge_sort(arr)
+    return total_inversions
+
+
+arr = [2, 4, 1, 3, 5]
+print("Number of inversions:", count_inversions(arr))
--- task_merge_sorted_lists.py
+++ task_merge_sorted_lists.py
@@ -14,3 +14,21 @@
 # - No recursion required.
 
 # Write your solution here
+def merge_sorted_lists(list1, list2):
+    i = j = 0
+    merged = []
+    # here lists are being merged
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+    return merged
+
+list1 = [1, 3, 5]
+list2 = [2, 4, 6]
+print(merge_sorted_lists(list1, list2))
