f33077460074dcb40c2fd628b2b93afba732d2bd
Longshlur <suronglongshlur22@gmail.com>
2025-10-14 07:34:08 +0000

    Completed m.py

--- m.py
+++ m.py
@@ -4,3 +4,9 @@
 
 # 💡 TIP:
 # Sets remove duplicates automatically. Use `.add()` to insert.
+
+numbers = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4}
+print(f"Set with duplicates removed: {numbers}")
+
+numbers.add(5)
+print(f"After adding 5: {numbers}")
