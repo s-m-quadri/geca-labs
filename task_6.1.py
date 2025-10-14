aa858b8abd2a09a391196f7ac93a339fe734ebe3
Aditya Gujarkar <165485333+Aditya-Gujarkar@users.noreply.github.com>
2025-10-14 04:41:21 +0000

    lab 06 completed

--- task_6.1.py
+++ task_6.1.py
@@ -11,3 +11,11 @@
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
 # hi
+def store_edges(edges):
+    edge_list = []
+    for u, v, w in edges:
+        edge_list.append((u, v, w))
+    return edge_list
+
+edges = [(0, 1, 4), (0, 2, 3), (1, 2, 1)]
+print(store_edges(edges))
