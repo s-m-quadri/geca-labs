b981fdbf5d3c3f316f98caa85e42e07392b1c786
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 07:00:23 +0000

    Organize submission for BT24S05F006 (merge into sub-lab-daa-05)

--- labs-design-analysis-algorithms/lab-05/BT24S05F006/task_5.2.py
+++ labs-design-analysis-algorithms/lab-05/BT24S05F006/task_5.2.py
@@ -17,3 +17,19 @@ key = [0, 2, 3]
 mstSet = [True, False, False]
 # Expected output: 1
 """
+def minKey(key, mstSet):
+    min_val = float('inf')
+    min_index = -1
+
+    for v in range(len(key)):
+        if mstSet[v] == False and key[v] < min_val:
+            min_val = key[v]
+            min_index = v
+
+    return min_index
+
+
+# Test case
+key = [0, 2, 3]
+mstSet = [True, False, False]
+print(minKey(key, mstSet))  
