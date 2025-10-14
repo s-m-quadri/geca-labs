4d8fc85c99280188f097074011607c1574e51c33
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:56:00 +0000

    Organize submission for BT23F05F036 (merge into sub-lab-daa-06)

--- labs-design-analysis-algorithms/lab-06/BT23F05F036/task_6.3.py
+++ labs-design-analysis-algorithms/lab-06/BT23F05F036/task_6.3.py
@@ -9,3 +9,10 @@
 
 # Hint: Use list comprehension.
 # Tip: No path compression or union by rank yet.
+def initialize_union_find(n):
+    parent = [i for i in range(n)]
+    return parent
+
+n = 5
+parent = initialize_union_find(n)
+print(parent)
