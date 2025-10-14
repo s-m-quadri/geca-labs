d88e3f0f0571e6bd81c4c19a1bf8a71f4ab90131
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:49:16 +0000

    Organize submission for BT24S05F008 (merge into sub-lab-daa-04)

--- labs-design-analysis-algorithms/lab-04/BT24S05F008/task_4.5.py
+++ labs-design-analysis-algorithms/lab-04/BT24S05F008/task_4.5.py
@@ -17,3 +17,20 @@
 #
 # Note: Greedy works with canonical coin systems like Indian/US coins,
 # but may fail with arbitrary denominations. That’s the fun part to test!
+
+
+def coin_change_greedy(denominations, amount):
+    denominations.sort(reverse=True)
+    coins_used = []
+    remaining = amount
+    for coin in denominations:
+        while remaining >= coin:
+            remaining -= coin
+            coins_used.append(coin)
+    return len(coins_used), coins_used
+
+denominations = [1, 2, 5, 10, 20, 50, 100]
+amount = 93
+min_coins, coins = coin_change_greedy(denominations, amount)
+print(min_coins)
+print(coins)
