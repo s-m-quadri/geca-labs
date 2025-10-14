f1477f58c3d5baf45164bc758b8c310e96847e53
t_b_desale <desaletushar512a@gmail.com>
2025-10-14 15:57:49 +0000

    COMPLETED LAB-02 BY BT23F05F015.

--- task_merge_sorted_lists.py
+++ task_merge_sorted_lists.py
@@ -14,3 +14,33 @@
 # - No recursion required.
 
 # Write your solution here
+
+# -------------------------------------------
+# TASK: Merge Two Sorted Lists (Ascending Order)
+# -------------------------------------------
+
+def merge_sorted_lists(list1, list2):
+    merged = []
+    i = j = 0
+
+    # Compare elements from both lists one by one
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+
+    # Add remaining elements (if any)
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+
+    return merged
+
+
+# Example test
+list1 = [1, 3, 5]
+list2 = [2, 4, 6]
+print("Merged List:", merge_sorted_lists(list1, list2))
+
