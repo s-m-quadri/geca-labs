a28e20317c09970f170c3e165876d0dcf8fd8b44
Longshlur <suronglongshlur22@gmail.com>
2025-10-14 06:10:59 +0000

    completed h.py

--- h.py
+++ h.py
@@ -1,6 +1,5 @@
-# Create a list of 5 integers.
-# Print the first, middle, and last elements.
-# Modify the third element to be 100, then print the entire list.
 
-# 💡 TIP:
-# Use indexing like list[0], list[-1], and list[2] to access or change values.
+nums = [10, 20, 30, 40, 50]
+print(nums[0], nums[2], nums[-1])
+nums[2] = 100
+print(nums)
