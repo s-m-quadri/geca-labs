e090a9d2cdb523549bfb47eb126ba7ddeca31df5
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:32:28 +0000

    Organize submission for BT23F05F021 (merge into sub-lab-daa-05)

--- task_5.2.py
+++ task_5.2.py
@@ -17,18 +17,3 @@ key = [0, 2, 3]
 mstSet = [True, False, False]
 # Expected output: 1
 """
-
-def min_key_vertex(key, mstSet):
-    min_val = float('inf')
-    min_index = -1
-    for i in range(len(key)):
-        if not mstSet[i] and key[i] < min_val:
-            min_val = key[i]
-            min_index = i
-    return min_index
-
-# Example usage
-if __name__ == "__main__":
-    key = [0, 2, 3]
-    mstSet = [True, False, False]
-    print(min_key_vertex(key, mstSet))  # Expected output: 1
