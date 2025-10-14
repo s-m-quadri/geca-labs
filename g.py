1390fa9b3d8f25f30c1a9e9f31ad5ab662780f84
AniketKulkarni1234 <156163167+AniketKulkarni1234@users.noreply.github.com>
2025-10-14 11:40:56 +0530

    completed g.py

--- g.py
+++ g.py
@@ -5,3 +5,12 @@
 # 💡 TIP:
 # `range(1, 11)` gives 1 through 10.
 # Use `while` with a counter.
+# Using for loop
+for i in range(1, 11):
+    print(i)
+
+# Using while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
