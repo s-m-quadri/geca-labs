b27e56f23c2e63d9d20cccbf76239d07898178fd
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:48:02 +0000

    Organize submission for BT24S05F008 (merge into sub-lab-daa-06)

--- labs-design-analysis-algorithms/lab-06/BT24S05F008/task_6.1.py
+++ labs-design-analysis-algorithms/lab-06/BT24S05F008/task_6.1.py
@@ -10,3 +10,11 @@
 
 # Hint: Just create and return the list.
 # Tip: Print the list to verify edges.
+
+
+def store_edges(edges):
+    return edges
+
+edges_input = [(0,1,4), (0,2,3), (1,2,1)]
+edges_list = store_edges(edges_input)
+print(edges_list)
