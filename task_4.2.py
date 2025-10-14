c466b3c5bb41610b66a280e8eb14809e81be7542
bot-s-m-quadri <access.smq@gmail.com>
2025-10-14 03:34:58 +0000

    Organize submission for BT23F05F045 (merge into sub-lab-daa-04)

--- task_4.2.py
+++ task_4.2.py
@@ -12,32 +12,3 @@
 # Output: 8
 #
 # Bonus: Try printing the whole Fibonacci sequence up to n instead of just nth number.
-
-def fib_recursive(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib_recursive(n - 1) + fib_recursive(n - 2)
-
-def fib_iterative(n):
-    if n == 0:
-        return 0
-    a, b = 0, 1
-    for _ in range(1, n):
-        a, b = b, a + b
-    return b
-
-n = 6
-print(fib_recursive(n))
-print(fib_iterative(n))
-
-def fib_sequence(n):
-    seq = []
-    a, b = 0, 1
-    for i in range(n + 1):
-        seq.append(a)
-        a, b = b, a + b
-    return seq
-
-print(fib_sequence(n))
