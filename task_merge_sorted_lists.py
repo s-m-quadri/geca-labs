5898de48e13047d189b26a7c4e8fdea483637670
Mayur303 <mgccomputer2311@gmail.com>
2025-10-15 13:46:06 +0530

    Update task_merge_sorted_lists.py

--- task_merge_sorted_lists.py
+++ task_merge_sorted_lists.py
@@ -1,16 +1,17 @@
-# You are given two sorted lists `list1` and `list2`.
-# Merge them into a single sorted list (ascending order) and return the result.
+def merge_sorted_lists(list1, list2):
+    merged = []
+    i = j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+    return merged
 
-# Example:
-# Input:
-# list1 = [1, 3, 5]
-# list2 = [2, 4, 6]
-# Output:
-# [1, 2, 3, 4, 5, 6]
-
-# INSTRUCTIONS:
-# - Do NOT use Python's built-in sorted().
-# - Implement your own merge function (similar to merge sort's merge step).
-# - No recursion required.
-
-# Write your solution here
+list1 = [1, 3, 5]
+list2 = [2, 4, 6]
+print(merge_sorted_lists(list1, list2))
