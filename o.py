c70a75e52eda5f10222552f8716d95a9f58a6f22
Mayur303 <mgccomputer2311@gmail.com>
2025-10-15 13:28:57 +0530

    Update o.py

--- o.py
+++ o.py
@@ -1,6 +1,2 @@
-# Create a function `greet` that takes a name and an optional message.
-#  - If no message is provided, it should default to "Hello".
-#  - Call the function in three different ways using default and named arguments.
-
-# 💡 TIP:
-#  Use `def greet(name, msg="Hello"):` to set a default value.
+def greet(name, msg="Hello"):
+    print(f"{msg}, {name}!")
