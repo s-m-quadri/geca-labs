52747b4bfc75d5a044ae2ddb8566671b7aa2ad19
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 03:14:37 +0000

    Organize submission for BT23F05F030 (merge into sub-lab-daa-04)

--- labs-design-analysis-algorithms/lab-04/BT23F05F030/task_4.3.py
+++ labs-design-analysis-algorithms/lab-04/BT23F05F030/task_4.3.py
@@ -12,3 +12,17 @@
 # Output: "olleh"
 #
 # Bonus: Try solving without using Python slicing [::-1].
+
+def reverse_rec(s):
+    if len(s) <= 1:
+        return s
+    else:
+        return reverse_rec(s[1:]) + s[0]
+
+
+
+def reverse_iterative(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str  # prepend each character
+    return reversed_str
