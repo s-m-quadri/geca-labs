3a41c5edc5b07dfee36f6196a1a7704da28afc4f
Mayur303 <mgccomputer2311@gmail.com>
2025-10-15 13:31:16 +0530

    Update v.py

--- v.py
+++ v.py
@@ -1,7 +1,4 @@
-# Import the `math` module and use it to calculate:
-#  - The square root of 144
-#  - The value of π
-#  - sin(90 degrees) converted to radians
-
-# 💡 TIP:
-# Use `import math`, and remember degrees must be converted to radians.
+import math
+print(math.sqrt(144))
+print(math.pi)
+print(math.sin(math.radians(90)))
