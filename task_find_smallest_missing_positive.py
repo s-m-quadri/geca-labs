41c8e4ce150004b1ff099a1164a066d9a7fb8c62
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 03:18:44 +0000

    Organize submission for BT23F05F043 (merge into sub-lab-daa-03)

--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,20 +9,3 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
-def smallest_missing_positive(arr):
-    left, right = 0, len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == mid + 1:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    # left is the index where the first missing positive occurs
-    return left + 1
-
-# Example usage
-print(smallest_missing_positive([1, 2, 3, 5, 6]))  # Output: 4
-print(smallest_missing_positive([2, 3, 4, 5]))     # Output: 1
-print(smallest_missing_positive([1, 2, 3, 4]))     # Output: 5
