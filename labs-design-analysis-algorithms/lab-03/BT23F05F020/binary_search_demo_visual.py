b4a82d682fb05738aabd035dbe8232831e9df5e2
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 07:40:34 +0000

    Organize submission for BT23F05F020 (merge into sub-lab-daa-03)

--- labs-design-analysis-algorithms/lab-03/BT23F05F020/binary_search_demo_visual.py
+++ labs-design-analysis-algorithms/lab-03/BT23F05F020/binary_search_demo_visual.py
@@ -17,7 +17,7 @@ def binary_search_visual(arr, target):
     print(f"{target} not found")
     return -1
 
-# Example usage
+# to do
 arr = [1, 2, 3, 4, 5, 6, 7]
 target = 4
 binary_search_visual(arr, target)
