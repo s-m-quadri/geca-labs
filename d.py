60c468548b7b4a282e2d0309a664f8f6172d8356
Mayur303 <mgccomputer2311@gmail.com>
2025-10-15 13:23:39 +0530

    Update d.py

--- d.py
+++ d.py
@@ -1,11 +1,8 @@
-# Write a program that takes two numbers from the user.
-# Print their:
-#  - sum
-#  - difference
-#  - product
-#  - quotient
-#  - remainder
-#  - power (x^y)
-
-# 💡 TIP:
-# Use `+`, `-`, `*`, `/`, `%`, and `**`.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+print("Diff:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
