2aeb659510445c9a9b1eaf17452cd82017f61767
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 03:21:24 +0000

    Organize submission for BT23F05F043 (merge into sub-lab-daa-05)

--- task_5.4.py
+++ task_5.4.py
@@ -25,29 +25,3 @@ parent = [-1, -1, -1]
 # key = [0, 1, 2]
 # parent = [-1, 0, 1]
 """
-def prim_iteration(graph, key, parent, mstSet):
-    u = -1
-    min_val = float('inf')
-    for i in range(len(key)):
-        if not mstSet[i] and key[i] < min_val:
-            min_val = key[i]
-            u = i
-    if u != -1:
-        mstSet[u] = True
-        for v in range(len(graph)):
-            if graph[u][v] > 0 and not mstSet[v] and graph[u][v] < key[v]:
-                key[v] = graph[u][v]
-                parent[v] = u
-
-graph = [
-    [0, 1, 4],
-    [1, 0, 2],
-    [4, 2, 0]
-]
-key = [0, float('inf'), float('inf')]
-mstSet = [True, False, False]
-parent = [-1, -1, -1]
-
-prim_iteration(graph, key, parent, mstSet)
-print("key =", key)
-print("parent =", parent)
