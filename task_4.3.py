bdd18e42854474340b9fdd1da565c10f1d7fa5be
Isha S <9405isha@gmail.com>
2025-10-15 21:22:31 +0000

    Added lab 04

--- task_4.3.py
+++ task_4.3.py
@@ -12,3 +12,17 @@
 # Output: "olleh"
 #
 # Bonus: Try solving without using Python slicing [::-1].
+def reverse_recursive(s):
+    if len(s) == 0:
+        return s
+    return reverse_recursive(s[1:]) + s[0]
+
+def reverse_iterative(s):
+    rev = ""
+    for ch in s:
+        rev = ch + rev
+    return rev
+
+s = input("Enter a string: ")
+print("Recursive:", reverse_recursive(s))
+print("Iterative:", reverse_iterative(s))
