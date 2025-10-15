c164fd61b324d3e0507e9639c02a374e6fedc447
Gokul Gaikwad <gokulgaikwad416@gmail.com>
2025-10-15 05:10:09 +0000

    completed j.py to o.py

--- o.py
+++ o.py
@@ -4,3 +4,10 @@
 
 # 💡 TIP:
 #  Use `def greet(name, msg="Hello"):` to set a default value.
+
+def greet(name, msg="Hello"):
+    print(f"{msg}, {name}!")
+
+greet("Alice")
+greet("Bob", "Hi")
+greet(name="Charlie", msg="Good morning")
