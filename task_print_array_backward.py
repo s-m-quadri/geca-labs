82689ef088e1a3e59f5c7e96f799b3b388c7be7d
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 03:13:01 +0000

    Organize submission for BT23F05F017 (merge into sub-lab-daa-01)

--- task_print_array_backward.py
+++ task_print_array_backward.py
@@ -14,29 +14,3 @@
 # - For recursion, start from last index: len(arr) - 1
 # - Reduce index by 1 each call
 # - For iteration, use a loop from end to start
-def print_reverse_recursive(arr, index=None):
-    if index is None:
-        index = len(arr) - 1
-
-    if index < 0:
-        return
-
-    print(arr[index], end=" ")
-    print_reverse_recursive(arr, index - 1)
-
-# Iterative version
-def print_reverse_iterative(arr):
-    for i in range(len(arr) - 1, -1, -1):
-        print(arr[i], end=" ")
-
-# Test both functions
-test_array = [1, 2, 3, 4, 5]
-print("Array:", test_array)
-
-print("Recursive reverse:", end=" ")
-print_reverse_recursive(test_array)
-print()
-
-print("Iterative reverse:", end=" ")
-print_reverse_iterative(test_array)
-print()
