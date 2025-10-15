8eda3da0ce3b43f65b0a352de10b4657f3ac8500
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 05:10:40 +0000

    Organize submission for BT23F05F030 (merge into sub-lab-daa-05)

--- labs-design-analysis-algorithms/lab-05/BT23F05F030/task_5.1.py
+++ labs-design-analysis-algorithms/lab-05/BT23F05F030/task_5.1.py
@@ -19,3 +19,9 @@ V = 4
 # parent = [-1, -1, -1, -1]
 # mstSet = [False, False, False, False]
 """
+def initialize_prim(V):
+    key = [float('inf')] * V      
+    parent = [-1] * V             
+    mstSet = [False] * V        
+
+    return key, parent, mstSet
