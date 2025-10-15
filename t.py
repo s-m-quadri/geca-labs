968659eccc5a925a893ec4e6f208d1a27ea39f97
Mayur303 <mgccomputer2311@gmail.com>
2025-10-15 13:30:37 +0530

    Update t.py

--- t.py
+++ t.py
@@ -1,5 +1,4 @@
-# Create a file named `output.txt`, write 3 lines into it, then read and print the content.
-#  - Use file modes `'w'` and `'r'`.
-
-# 💡 TIP:
-# Use `with open(...) as f:` to auto-close files.
+with open("output.txt", "w") as f:
+    f.write("Line 1\nLine 2\nLine 3\n")
+with open("output.txt", "r") as f:
+    print(f.read())
