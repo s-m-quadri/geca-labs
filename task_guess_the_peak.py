d48063fb1d5fa351f637dd6107dcc717d6e81970
bot-s-m-quadri <access.smq@gmail.com>
2025-10-15 04:59:12 +0000

    Organize submission for BT23F05F023 (merge into sub-lab-daa-03)

--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,19 +9,3 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
-def find_peak_in_mountain_array(arr):
-    low, high = 0, len(arr) - 1
-
-    while low < high:
-        mid = (low + high) // 2
-        if arr[mid] < arr[mid + 1]:
-            low = mid + 1  # Peak is to the right
-        else:
-            high = mid  # Peak is at mid or to the left
-
-    return arr[low]
-
-
-# Test cases
-print(find_peak_in_mountain_array([1, 3, 7, 12, 9, 5, 2]))  # Output: 12
-print(find_peak_in_mountain_array([0, 2, 4, 6, 3, 1]))      # Output: 6
