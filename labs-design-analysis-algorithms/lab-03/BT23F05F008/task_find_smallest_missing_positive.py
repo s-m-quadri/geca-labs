e4ed4be76877fe1b71647d531069068c426692d5
bot-s-m-quadri <access.smq@gmail.com>
2025-10-16 01:52:16 +0000

    Organize submission for BT23F05F008 (merge into sub-lab-daa-03)

--- labs-design-analysis-algorithms/lab-03/BT23F05F008/task_find_smallest_missing_positive.py
+++ labs-design-analysis-algorithms/lab-03/BT23F05F008/task_find_smallest_missing_positive.py
@@ -9,3 +9,12 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
+def smallest_missing_positive(arr):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == mid + 1:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low + 1
