8cc1fda67c301a546d218ff494edbb4f4f898e06
dipalichaukhe <dipalichaukhe2513@gmail.com>
2025-10-16 05:24:25 +0000

    Completed all tasks

--- k.py
+++ k.py
@@ -4,3 +4,9 @@
 
 # 💡 TIP:
 # Tuples are like lists, but immutable (they cannot be changed).
+fruits = ("apple", "banana", "cherry")
+
+# fruits[1] = "mango"  # This will cause a TypeError
+
+for item in fruits:
+    print(item)
