bedf1f6c5e4715ccc6f890e60ca360a09e5e0619
bot-s-m-quadri <access.smq@gmail.com>
2025-10-17 09:51:33 +0000

    Organize submission for BT23F05F011 (merge into sub-lab-daa-03)

--- task_find_smallest_missing_positive.py
+++ task_find_smallest_missing_positive.py
@@ -9,24 +9,3 @@
 #
 # Input: [2, 3, 4, 5]
 # Output: 1
-def smallest_missing(arr):
-    left, right = 0, len(arr) - 1
-
-    # Edge case: missing number is 1
-    if arr[0] != 1:
-        return 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        # If the value at mid equals its position + 1, missing number is on the right
-        if arr[mid] == mid + 1:
-            left = mid + 1
-        else:
-            # Gap found, missing number could be mid + 1 or on the left
-            right = mid - 1
-
-    # After the loop, left points to the smallest missing number
-    return left + 1
-
-# Test cases
-print(smallest_missing([1, 2, 3, 5, 6]))  # Output: 4_
--- task_guess_the_peak.py
+++ task_guess_the_peak.py
@@ -9,22 +9,3 @@
 #
 # Input: [0, 2, 4, 6, 3, 1]
 # Output: 6
-def find_peak(arr):
-    left, right = 0, len(arr) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-        # If mid is less than mid+1, peak is to the right
-        if arr[mid] < arr[mid + 1]:
-            left = mid + 1
-        else:
-            # If mid is greater than mid+1, peak is at mid or to the left
-            right = mid
-
-    # When left == right, we found the peak
-    return arr[left]
-
-# Test cases
-print(find_peak([1, 3, 7, 12, 9, 5, 2]))  # Output: 12
-print(find_peak([0, 2, 4, 6, 3, 1]))      # Output: 6
-
