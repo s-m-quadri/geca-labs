14171de13558022327ecbc6e99f3ca27b4b36493
jayesh9606 <jayeshdonge29@gmail.com>
2026-02-05 06:56:07 +0000

    donee

--- b.py
+++ b.py
@@ -1,11 +1,4 @@
-# Task: Create variables for a student record:
-#  - student_id (integer, e.g., 101)
-#  - student_name (string, e.g., "Alice")
-#  - marks (float, e.g., 85.5)
-#  - department (string, e.g., "CSE")
-# Print all variables in a formatted way.
-
-# 💡 TIP:
-# Use f-strings for formatting: f"ID: {student_id}"
-
-
+a=10
+b=20
+c=a+b
+print(c)
