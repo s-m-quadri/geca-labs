53289cd93de02268d31f99c8334f776df5360eec
Aadarsh_Charpe_03 <aadarshcharpe03@gmail.com>
2026-02-09 09:37:07 +0000

    Done

--- b.py
+++ b.py
@@ -7,5 +7,14 @@
 
 # 💡 TIP:
 # Use f-strings for formatting: f"ID: {student_id}"
+student_id  = BT24F05F010
+student_name = "Aadarsh"
+marks = 100.0
+department = "CSE"
+print(student_id)
+print(student_name)
+print(marks)
+print(department)
+
 
 
