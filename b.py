d80c692a2b7b97ff64b4dbda6df507ae3a30f0be
Yadvi choudhari <yadvichoudhari11@gmail.com>
2026-02-09 09:37:30 +0000

    b.py

--- b.py
+++ b.py
@@ -8,4 +8,11 @@
 # 💡 TIP:
 # Use f-strings for formatting: f"ID: {student_id}"
 
-
+student_id=101
+student_name="alice"
+marks=85.5
+department="CSE"
+print(f"student_id={student_id}")
+print(f"student_name={student_name}")
+print(f"marks={student_marks}")
+print(f"department={department}")
