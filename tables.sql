88f2a549b02660e3bf449dd77807f893d0e7daea
Vedika Gawande <vedikagawande91@gmail.com>
2026-02-10 11:27:34 +0000

    lab 02 completed

--- tables.sql
+++ tables.sql
@@ -1,17 +1,14 @@
--- Lab 0: Table Creation
--- Task: Create a students table with proper structure
+
 
 USE student_db;
 
--- TODO: Create students table with the following columns:
--- - id (integer, primary key)
--- - name (string, max 50 characters, not null)
--- - age (integer)
--- - department (string, max 30 characters)
 
--- CREATE TABLE students (
---     ...
--- );
+CREATE TABLE students (
+    id int primary key,
+    name VARCHAR(50),
+    age int,
+    department varchar(50)
+);
 
 -- Verify table creation
--- DESCRIBE students;
+DESCRIBE students;
