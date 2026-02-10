748bd530fd750a795a938623c903213be33baf5f
saylimachave29 <saylimachave59@gmail.com>
2026-02-10 14:06:26 +0000

    f.py completed

--- f.py
+++ f.py
@@ -5,9 +5,18 @@
 #  - Then read and print the file contents
 
 # 💡 TIP:
-# Write: with open("file.txt", "w") as f:
-#           f.write("text\n")
-# Read: with open("file.txt", "r") as f:
-#          print(f.read())
+# Write student data to a file
+
+with open("students.txt", "w") as f:
+    f.write("aaa,19,Information Technology\n")
+    f.write("bbb,21,Electronics\n")
+    f.write("ccc,20,Computer Science\n")
+
+# Read and print the file contents
+
+with open("students.txt", "r") as f:
+    print("Student Records:\n")
+    print(f.read())   
+    print(f.read())
 
 
