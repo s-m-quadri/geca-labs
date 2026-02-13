b6181da16bf11abc00646614e20092b45fe000fa
amruta-rothe <rotheamruta1@gmail.com>
2026-02-13 05:51:01 +0000

    4th commit

--- d.py
+++ d.py
@@ -7,5 +7,8 @@
 # Dictionary: student = {"id": 101, "name": "Alice", ...}
 # Access: student["name"]
 # Loop: for key, value in student.items():
+student = {"id": 53, "name": "Amruta", "dept": "CSE"}
+print(student["name"])
 
-
+for key, value in student.items():
+    print(key,":",value)
