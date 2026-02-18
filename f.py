61c931f7670520c33d70fb7cb1d9a449fde9109b
SagarJ2126 <ssj02122006@gmail.com>
2026-02-18 07:07:01 +0000

    f.py completed

--- f.py
+++ f.py
@@ -9,5 +9,15 @@
 #           f.write("text\n")
 # Read: with open("file.txt", "r") as f:
 #          print(f.read())
+students = [
+    "Name:Alice,Age:20,Department:Computer Science",
+    "Name:Bob,Age:22,Department:Mechanical Engineering",
+    "Name:Charlie,Age:19,Department:Electrical Engineering"
+]
+with open("students.txt", "w") as f:
+    for student in students:
+        f.write(student + "\n")
+with open("students.txt", "r") as f:
+    print(f.read())
 
 
