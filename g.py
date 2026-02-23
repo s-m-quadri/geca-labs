2eb4d6aa98ceef725bc1ea5faa4adad6b4d11ac7
Divyaingle-dev <bingle768@gmail.com>
2026-02-23 07:53:39 +0000

    completed g.py

--- g.py
+++ g.py
@@ -9,5 +9,10 @@
 #     print(f"ID: {value}")
 # except ValueError:
 #     print("Invalid input! Please enter a number.")
+try:
+    student_id = int(input("Enter student ID: "))
+    print(f"Valid ID: {student_id}")
+except ValueError:
+    print("Invalid input! Please enter a number.")
 
 
