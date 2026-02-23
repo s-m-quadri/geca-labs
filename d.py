609c93f782d7071d4d624b47b2817dd5d91bfed0
mhaskemansi19-lgtm <mhaskemansi19@gmail.com>
2026-02-23 20:05:41 +0000

    completed d.py

--- d.py
+++ d.py
@@ -7,5 +7,13 @@
 # Dictionary: student = {"id": 101, "name": "Alice", ...}
 # Access: student["name"]
 # Loop: for key, value in student.items():
+dict_1={
+    "id":40,
+    "name":"prachi",
+    "age":19
+}
+for key,value in dict_1.items():
+    print(f"{key}:{value}")
+
 
 
