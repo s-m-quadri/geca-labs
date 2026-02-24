4d689f2c327dddcc7a9be8d87571d4566e0c5ed1
jankikulkarni10-droid <jankikulkarni10@gmail.com>
2026-02-24 09:25:01 +0000

    complete lab

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -3,5 +3,13 @@
 
 -- Switch to school_db
 USE school_db;
+create table students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE  
+);
+
+
 
 -- TODO: Write your CREATE TABLE command here
