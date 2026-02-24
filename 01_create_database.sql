c97410f41ccbf057327549e569eef0d481644c97
SagarJ2126 <ssj02122006@gmail.com>
2026-02-24 09:37:38 +0000

    01 of lab 1 completed

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -1,4 +1,8 @@
 -- Task 1: Create Database
 -- Create a database named 'school_db'
-
+CREATE DATABASE school_db;
 -- TODO: Write your SQL command here
+-- Task 2: Use the Database
+-- Switch to the 'school_db' database to start working with it
+USE school_db;
+SHOW DATABASES;
