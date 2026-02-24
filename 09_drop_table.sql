5078a8a50d2dd075cc991f0cd27267208e1ee809
SagarJ2126 <ssj02122006@gmail.com>
2026-02-24 09:39:25 +0000

    09 of lab 1 completed

--- 09_drop_table.sql
+++ 09_drop_table.sql
@@ -2,6 +2,8 @@
 -- Drop (delete) the students table
 
 USE school_db;
-
+SHOW TABLES;
+DROP TABLE students;
+SHOW TABLES;
 -- TODO: Write your DROP TABLE command here
 -- Warning: This will permanently delete the table!
