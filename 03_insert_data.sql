8a3bb69334b92630010b36cd144af9a597482663
shraddhaujgare6-commits <shraddhaujgare6@gmail.com>
2026-02-24 11:05:32 +0000

    create database

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -6,4 +6,5 @@ USE school_db;
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
 INSERT INTO student VALUES(1,"PARTH",22,"parth@gmail.com");
-INSERT 
+INSERT INTO student VALUES(2,"RAM",21,"ram@gamil.com");
+INSERT INTO student VALUES(3,"RAJESH",25,"rajesh@gamil.com");
