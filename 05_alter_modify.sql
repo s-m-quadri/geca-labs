af3215b72eea2eca3371059e1eab3394026fbe2a
jayesh9606 <jayeshdonge29@gmail.com>
2026-02-24 11:25:52 +0000

    done

--- 05_alter_modify.sql
+++ 05_alter_modify.sql
@@ -2,5 +2,12 @@
 -- Change 'age' column to be TINYINT with NOT NULL constraint
 
 USE school_db;
-
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(40),
+    age TINYINT NOT NULL,
+    email VARCHAR(100),
+    phone VARCHAR(15)
+);
+DESCRIBE students;
 -- TODO: Write your ALTER TABLE MODIFY COLUMN command here
--- 09_drop_table.sql
+++ 09_drop_table.sql
@@ -2,6 +2,24 @@
 -- Drop (delete) the students table
 
 USE school_db;
+-- Show all databases
+SHOW DATABASES;
+ 
+-- Switch to your database
+USE school_db;
+ 
+-- Show all tables
+SHOW TABLES;
+ 
+-- Show table structure
+DESCRIBE students;
+ 
+-- Count rows
+SELECT COUNT(*) as total_rows FROM students;
+ 
+-- Show all data
+SELECT * FROM students;
 
 -- TODO: Write your DROP TABLE command here
 -- Warning: This will permanently delete the table!
+
