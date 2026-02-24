db6edfb4795993a64df1adc3067568ed31e1a2e7
Anushka-55 <155242840+Anushka-55@users.noreply.github.com>
2026-02-24 10:58:57 +0000

    f.py completed

--- f.py
+++ f.py
@@ -3,7 +3,10 @@
 #  - Write at least 3 student records (one per line)
 #  - Format: Name,Age,Department
 #  - Then read and print the file contents
-
+with open("students.txt", "w") as f:
+    f.write("kirti,20,CSE\n")
+    f.write("poorva,21,ECE\n")
+    f.write("renu,19,MECH\n")
 # 💡 TIP:
 # Write: with open("file.txt", "w") as f:
 #           f.write("text\n")
