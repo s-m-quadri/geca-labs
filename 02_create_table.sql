02f0300f8eca3679f56050e536727fbd9c1ad619
CHETANo3 <chetantupe0303@gmail.com>
2026-02-24 11:16:20 +0000

    completed the first lab 's task

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -5,3 +5,12 @@
 USE school_db;
 
 -- TODO: Write your CREATE TABLE command here
+
+
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    age INT,
+    email VARCHAR(50)
+);
+DESCRIBE students;
