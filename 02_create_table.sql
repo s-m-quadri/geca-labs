a91398076b082c9ba0dcdf7960291f56f0a7fb3a
shraddhaujgare6-commits <shraddhaujgare6@gmail.com>
2026-02-24 10:52:45 +0000

    create database

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -5,3 +5,9 @@
 USE school_db;
 
 -- TODO: Write your CREATE TABLE command here
+CREATE table student(
+    id INT PRIMARY KEY ;
+    name VARCHAR(100) NOT NULL ;
+    age INT NOT NULL ;
+    email VARCHAR(20) NOT NULL ;
+);
