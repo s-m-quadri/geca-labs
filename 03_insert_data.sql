3fd9422ddf7ea018d2597301f9eb72c761a2beca
Sayli Machave <saylimachave59@gmail.com>
2026-02-24 11:16:02 +0000

    1 completed

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,3 +5,5 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+INSERT INTO student_info(rollno,name,marks) VALUES(1,"AAA",20);
+SELECT * FROM student_info;
