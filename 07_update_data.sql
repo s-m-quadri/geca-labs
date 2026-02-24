e6ee1d9d5673f47b71132f4a2d93b6c364147280
Pratiksha <gavhadsudam26@gmail.com>
2026-02-24 11:29:16 +0000

    update data

--- 07_update_data.sql
+++ 07_update_data.sql
@@ -5,3 +5,8 @@ USE school_db;
 
 -- TODO: Write your UPDATE command here
 -- Remember to use WHERE clause!
+-- select * from students1;
+-- Update students1 set age=19 where student_id=2;
+select * from students1;
+update students1 set grade='A+' where student_id=2;
+select * from students1;
