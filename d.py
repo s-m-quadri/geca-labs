50cb3684cd7100a1ec9706d6e0108e98e2f11c98
anjali462006 <anjaliwakalkar6@gmail.com>
2026-02-24 15:48:36 +0000

    d.py completed

--- d.py
+++ d.py
@@ -8,4 +8,11 @@
 # Access: student["name"]
 # Loop: for key, value in student.items():
 
+student ={"id":101, "name":"Alice","age":20,"department":"CSE"}
+for key, value in student.items():
+    print(f"{key}:{value}")
+    print(student["name"])
+    
+
+
 
