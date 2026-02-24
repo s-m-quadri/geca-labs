b6bbef583b51c8ee779be9c12ec59311f70251ba
kawalerucha3-debug <kawalerucha3@gmail.com>
2026-02-24 09:17:24 +0000

    01 completed

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,3 +5,7 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+USE school_db;
+INSERT INTO students (name,age,email) VALUES ("ALICE",20,"abc123@gmail.com");
+INSERT INTO students (name,age,email) VALUES ("AN",23,"an345@gmail.com");
+INSERT INTO students (name,age,email) VALUES("CAT",20,"cat678@gmail.com");sudo 
