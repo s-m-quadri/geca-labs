e604ab40d5a07dea177bcc6d76239dd5614228f2
amruta-rothe <rotheamruta1@gmail.com>
2026-02-27 09:18:42 +0000

    completed

--- 10_check_status.sql
+++ 10_check_status.sql
@@ -1,16 +1,11 @@
 -- Task 10: Check Status
 -- View current database status
 
--- Show all databases
+
 SHOW DATABASES;
-
--- Use your database
 USE school_db;
-
--- Show all tables
 SHOW TABLES;
 
--- If students table exists, show its structure
 DESCRIBE students;
 
 -- If students table exists, count rows
