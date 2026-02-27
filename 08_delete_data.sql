882a414522916dda926b77a3fbac5e58a80b2c67
Adarsh Singh <a1d0arsh6@gmail.com>
2026-02-27 06:41:01 +0000

    LAB - 01 - DONE - 27/02/2026

--- 08_delete_data.sql
+++ 08_delete_data.sql
@@ -5,3 +5,10 @@ USE school_db;
 
 -- TODO: Write your DELETE command here
 -- Remember to use WHERE clause!
+
+-- DELETE FROM table_name WHERE condition;
+
+DELETE FROM students WHERE id = 57 ;
+
+
+SELECT * FROM students;
