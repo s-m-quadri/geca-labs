2c2fbec5beae73a8fbd9468c08572cc5e37766ee
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-02-27 06:50:16 +0000

    lab 01

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -5,3 +5,12 @@
 USE school_db;
 
 -- TODO: Write your CREATE TABLE command here
+
+    CREATE TABLE students (
+        id INT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        age INT,
+        email VARCHAR(100) UNIQUE
+    );
+    USE school_db;
+    DESCRIBE students;
