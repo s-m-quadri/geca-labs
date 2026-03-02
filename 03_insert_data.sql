371d882d695939fc6dd1aacb30f49f79ff051a12
Yadvi choudhari <yadvichoudhari11@gmail.com>
2026-03-02 09:56:37 +0000

    completed 5 of 10

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,3 +5,6 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+INSERT INTO students (name, age, email) VALUES ('Alice Johnson', 20, 'alice.johnson@example.com');
+INSERT INTO students (name, age, email) VALUES ('Bob Smith', 22, 'bob.smith@example.com');
+INSERT INTO students (name, age, email) VALUES ('Charlie Brown', 19, 'charlie.brown@example.com');
