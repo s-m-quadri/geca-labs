1d977fc06b1540492735403ee857a6c58b2100fd
deshmukhsuhaniatulrao-tech <deshmukhsuhaniatulrao@gmail.com>
2026-03-02 09:57:24 +0000

    suhukadesh

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -3,5 +3,13 @@
 
 USE school_db;
 
+INSERT INTO students VALUES ('Suhani', 19, '7th');
+
+INSERT INTO students VALUES ('Dilip', 59, '7th');
+
+
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+
+
+ 
