8eb705b2b920579d627020c5e498eb5d6340b811
shreeyadosi <shreeyadosi@gmail.com>
2026-03-02 09:56:25 +0000

    01 lab ddl completed

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -2,3 +2,5 @@
 -- Create a database named 'school_db'
 
 -- TODO: Write your SQL command here
+CREATE DATABASE IF NOT EXISTS school_db;
+SHOW DATABASES; -- This will list all databases, including the one you just created!
