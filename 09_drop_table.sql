3a11fdaa3d6ec530e005e494eda29934d0360fa0
shamaljadhav0202-lgtm <shamaljadhav0202@gmail.com>
2026-03-03 18:37:33 +0000

    done

--- 09_drop_table.sql
+++ 09_drop_table.sql
@@ -5,3 +5,6 @@ USE school_db;
 
 -- TODO: Write your DROP TABLE command here
 -- Warning: This will permanently delete the table!
+DROP TABLE students;
+-- To verify the table has been dropped, you can try to select from it (this should give an error)
+SELECT * FROM students; -- This should result in an error since the table no longer exists
