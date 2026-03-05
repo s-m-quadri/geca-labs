2d5a7a689bf596343f26c7e98d6499aa9c1d6722
tanvi1713 <tanvipardeshi1710@gmail.com>
2026-03-05 18:59:05 +0000

    completed lab 1

--- 06_alter_rename.sql
+++ 06_alter_rename.sql
@@ -5,3 +5,5 @@ USE school_db;
 
 -- TODO: Write your ALTER TABLE RENAME COLUMN command here
 -- MySQL syntax: ALTER TABLE table_name RENAME COLUMN old_name TO new_name;
+ALTER TABLE students CHANGE name student_name VARCHAR(255);
+DESCRIBE students;
