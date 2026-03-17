63587e8516ecdbfd35a3ebf57b7ee680e6e04a09
Pratiksha <gavhadsudam26@gmail.com>
2026-03-17 11:45:21 +0000

    file 14 done

--- 14_having.sql
+++ 14_having.sql
@@ -1,9 +1,12 @@
 -- Task 14: Having Clause
 -- Show grades that have more than 1 student
 
-USE school_db;
+USE school_db1;
 
 -- TODO: SELECT grade, COUNT(*) as count
 -- FROM students
 -- GROUP BY grade
 -- HAVING count > 1;
+select grade,COUNT(*) AS COUNT
+from students group by grade
+having count>=2;
