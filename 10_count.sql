5d957a09dd0de56f549cd68acf1c9e6481aba6af
Pratiksha <gavhadsudam26@gmail.com>
2026-03-17 11:38:44 +0000

    done

--- 10_count.sql
+++ 10_count.sql
@@ -1,6 +1,7 @@
 -- Task 10: Count Students
 -- Count total number of students
 
-USE school_db;
+USE school_db1;
 
 -- TODO: SELECT COUNT(*) as total_students FROM students;
+select count(*) as total_Row from students;
