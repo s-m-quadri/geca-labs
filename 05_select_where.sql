e917b4f8d5a518603e79e6ae95d2b014558f0fae
anishakate9394-crypto <anishakate9394@gmail.com>
2026-03-17 09:26:37 +0000

    05_select_where.sql

--- 05_select_where.sql
+++ 05_select_where.sql
@@ -4,3 +4,11 @@
 USE school_db;
 
 -- TODO: SELECT students WHERE grade = '10th'
+-- Students in 10th grade
+SELECT * FROM students WHERE grade = '10th';
+ 
+-- Students older than 15
+SELECT * FROM students WHERE age > 15;
+ 
+-- Students age 15 or 16
+SELECT * FROM students WHERE age IN (15, 16);
