cb0d0fe435879d5588c88c23f1048bc2c98606ed
Soham Joshi <sohamjoshi3011@gmail.com>
2026-03-17 09:46:46 +0000

    Lab Completed

--- 05_select_where.sql
+++ 05_select_where.sql
@@ -4,3 +4,7 @@
 USE school_db;
 
 -- TODO: SELECT students WHERE grade = '10th'
+SELECT name,grade FROM students;
+SELECT name,age FROM students;
+
+
--- 11_avg.sql
+++ 11_avg.sql
@@ -3,4 +3,4 @@
 
 USE school_db;
 
--- TODO: SELECT AVG(age) as average_age FROM students;
+SELECT AVG(age) as average_age FROM students;
