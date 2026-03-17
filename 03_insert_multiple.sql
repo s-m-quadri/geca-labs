ebd51de2d42524743047c237f3c5f5e88e4eb9cf
rohanpawar-23 <rohansp2006@gmail.com>
2026-03-17 11:29:00 +0000

    PRN : BT24F05F070

--- 03_insert_multiple.sql
+++ 03_insert_multiple.sql
@@ -8,3 +8,10 @@ USE school_db;
 -- ('Bob', 16, '11th'),
 -- ('Charlie', 15, '10th'),
 -- ('Diana', 17, '12th');
+
+INSERT INTO students (name, age, grade) VALUES
+('RohAn', 16, '11th'),
+('Mayur', 15, '10th'),
+('Mangesh', 17, '12th');
+
+SELECT * FROM students;
