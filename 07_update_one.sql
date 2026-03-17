dea4572dc0c45f77a81af1454a8b5841bd1b7dfc
anishakate9394-crypto <anishakate9394@gmail.com>
2026-03-17 09:30:14 +0000

    07_update_one.sql

--- 07_update_one.sql
+++ 07_update_one.sql
@@ -5,3 +5,18 @@ USE school_db;
 
 -- TODO: UPDATE students SET age = 16 WHERE id = 1;
 -- Remember to use WHERE!
+-- Update one column
+UPDATE table_name
+SET column1 = new_value
+WHERE condition;
+ 
+-- Update multiple columns
+UPDATE table_name
+SET column1 = value1,
+    column2 = value2
+WHERE condition;
+ 
+-- Update with calculation
+UPDATE table_name
+SET column = column + 10
+WHERE condition;
