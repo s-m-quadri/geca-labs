36ab3b11cb0ab80c16479eabeb29b131b800d154
SagarJ2126 <ssj02122006@gmail.com>
2026-03-18 02:04:08 +0000

    delete_where.sql completed

--- 09_delete_where.sql
+++ 09_delete_where.sql
@@ -2,7 +2,9 @@
 -- Remove students younger than 15
 
 USE school_db;
-
+SELECT * FROM students WHERE age < 15;
+DELETE FROM students WHERE age < 15;
+SELECT * FROM students;
 -- TODO: First, see who will be deleted
 -- SELECT * FROM students WHERE age < 15;
 
