c3346c1e95904f49bc262d2ef2637440ee43404c
SagarJ2126 <ssj02122006@gmail.com>
2026-03-18 02:06:11 +0000

    having.sql completed

--- 14_having.sql
+++ 14_having.sql
@@ -2,7 +2,7 @@
 -- Show grades that have more than 1 student
 
 USE school_db;
-
+SELECT grade, COUNT(*) as count FROM students GROUP BY grade HAVING count > 1;
 -- TODO: SELECT grade, COUNT(*) as count
 -- FROM students
 -- GROUP BY grade
