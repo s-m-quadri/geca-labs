0365e6a13377a8d5daa29b5cc1742af27daa986e
Pratiksha <gavhadsudam26@gmail.com>
2026-03-24 11:39:48 +0000

    done

--- 12_count_distinct.sql
+++ 12_count_distinct.sql
@@ -1,6 +1,7 @@
 -- Task 12: Count — COUNT(DISTINCT)
 -- How many distinct departments appear?
 
-USE school_db;
+USE school_db1;
 
 -- TODO: SELECT COUNT(DISTINCT dept) AS distinct_depts FROM employees;
+select count(distinct dept_name) AS distinct_department from Employees;
