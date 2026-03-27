3b9d3262e9ce36802dfa5335f573f8414504772b
ANNI-DBMS <anushkasatpute25@gmail.com>
2026-03-27 17:18:14 +0000

    01completed

--- 01_setup.sql
+++ 01_setup.sql
@@ -10,3 +10,11 @@
 -- name VARCHAR(50)
 -- age INT
 -- grade VARCHAR(10)
+CREATE DATABASE school_db;
+USE school_db;
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT,
+    grade VARCHAR(10)
+);
