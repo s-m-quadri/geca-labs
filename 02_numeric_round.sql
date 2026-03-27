9c172ec247017d2db33bfd852cda8c2a391436d3
amruta-rothe <rotheamruta1@gmail.com>
2026-03-27 05:51:49 +0000

    02_numeric_round file done

--- 02_numeric_round.sql
+++ 02_numeric_round.sql
@@ -3,7 +3,11 @@
 
 USE school_db;
 
--- TODO: SELECT full_name, salary,
---   ROUND(salary, 0) AS salary_whole,
---   ROUND(salary, 2) AS salary_two_dec
--- FROM employees;
+-- TODO: SELECT full_name, salary, ROUND(salary, 0) AS salary_whole, ROUND(salary, 2) AS salary_two_dec
+-- FROM employees;  
+SELECT 
+    full_name,
+    salary,
+    ROUND(salary, 0) AS salary_whole,
+    ROUND(salary, 2) AS salary_two_dec
+FROM employees;
