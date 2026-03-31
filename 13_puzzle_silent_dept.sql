6f2db65ef7f4ec399dcfc946650206ae613c8cbc
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 18:37:50 +0000

    13th done

--- 13_puzzle_silent_dept.sql
+++ 13_puzzle_silent_dept.sql
@@ -3,4 +3,9 @@
 
 USE join_lab;
 
+SELECT d.dept_name
+FROM departments d
+LEFT JOIN projects p ON p.dept_id = d.dept_id
+WHERE p.proj_id IS NULL;
+
 -- TODO: Use an outer join or NOT EXISTS pattern; return dept_name only
