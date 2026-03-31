8f9f91b22c811cf32ef32771044d2decf110d913
rohanpawar-23 <rohansp2006@gmail.com>
2026-03-31 14:09:33 +0000

    BT24F05F070

--- 11_puzzle_twin_projects.sql
+++ 11_puzzle_twin_projects.sql
@@ -7,3 +7,12 @@
 USE join_lab;
 
 -- TODO: Write one SELECT that returns exactly one name (the solver).
+
+SELECT s.name
+FROM staff s
+JOIN project_staff ps ON s.staff_id = ps.staff_id
+JOIN projects p ON ps.proj_id = p.proj_id
+WHERE p.title IN ('Riddle-UI', 'Riddle-API')
+GROUP BY s.staff_id
+HAVING COUNT(DISTINCT p.title) = 2;
+
