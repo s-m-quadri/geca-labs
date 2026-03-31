9d7d29b0d7d6ff3e22a580feb1472fd81b504a58
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 18:59:03 +0000

    4 done

--- 04_alter_add_column.sql
+++ 04_alter_add_column.sql
@@ -2,5 +2,7 @@
 -- Add a 'grade' column (VARCHAR(10)) to students table
 
 USE school_db;
-
+ALTER TABLE students
+ADD COLUMN grade VARCHAR(10);
+DESCRIBE students;
 -- TODO: Write your ALTER TABLE ADD COLUMN command here
