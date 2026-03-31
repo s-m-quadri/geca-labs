d23da05bb4dcb245841d018140a2f2d09260b922
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 18:59:32 +0000

    7 done

--- 07_update_data.sql
+++ 07_update_data.sql
@@ -2,6 +2,9 @@
 -- Update the grade for a specific student
 
 USE school_db;
-
+UPDATE students
+SET grade = 'A'
+WHERE id = 1;
+SELECT * FROM students;
 -- TODO: Write your UPDATE command here
 -- Remember to use WHERE clause!
