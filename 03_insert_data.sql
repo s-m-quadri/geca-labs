205f3ac6a0e72efbf3ad7d9d6183bba8d9e1de8b
Krutarth <138255111+kruxarth@users.noreply.github.com>
2026-03-31 12:55:00 +0000

    assignment one complete

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,3 +5,16 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+
+INSERT INTO students (id, name, age, email)
+VALUES (1, 'Alice', 13, 'alice@school.com');
+
+INSERT INTO students (id, name, age, email)
+VALUES (2, 'Bob', 17, 'bob@school.com');
+
+INSERT INTO students (id, name, age, email)
+VALUES (3, 'Charlie', 14, 'charlie@school.com');
+
+SELECT * FROM students
+
+
