075207e9786b17a97f8c672feba9fe36f018b97a
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 18:59:43 +0000

    8 done

--- 08_delete_data.sql
+++ 08_delete_data.sql
@@ -2,6 +2,8 @@
 -- Delete a student from the table
 
 USE school_db;
-
+DELETE FROM students
+WHERE id = 2;
+SELECT * FROM students;
 -- TODO: Write your DELETE command here
 -- Remember to use WHERE clause!
