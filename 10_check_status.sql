b1b3be25f9336953460a79838d1fd58889b77ac5
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 19:00:01 +0000

    10 done

--- 10_check_status.sql
+++ 10_check_status.sql
@@ -3,18 +3,13 @@
 
 -- Show all databases
 SHOW DATABASES;
-
 -- Use your database
 USE school_db;
-
 -- Show all tables
 SHOW TABLES;
-
 -- If students table exists, show its structure
 DESCRIBE students;
-
 -- If students table exists, count rows
 SELECT COUNT(*) as total_students FROM students;
-
 -- View all data
 SELECT * FROM students;
