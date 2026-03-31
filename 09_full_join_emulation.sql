6b573222be23f5431d0635fe8aaf238c664a93eb
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 18:36:57 +0000

    9th done

--- 09_full_join_emulation.sql
+++ 09_full_join_emulation.sql
@@ -4,4 +4,22 @@
 
 USE join_lab;
 
+SELECT
+  p.title AS project_title,
+  s.name AS staff_name,
+  ps.hours
+FROM projects p
+LEFT JOIN project_staff ps ON p.proj_id = ps.proj_id
+LEFT JOIN staff s ON s.staff_id = ps.staff_id
+
+UNION
+
+SELECT
+  p.title AS project_title,
+  s.name AS staff_name,
+  ps.hours
+FROM staff s
+LEFT JOIN project_staff ps ON s.staff_id = ps.staff_id
+LEFT JOIN projects p ON p.proj_id = ps.proj_id
+WHERE ps.proj_id IS NULL;
 -- TODO: Write a query your instructor can run; add a short comment on your strategy
