b5eae955e656f928d8ebf2ce5f4ec140244674f8
SagarJ2126 <ssj02122006@gmail.com>
2026-03-31 18:58:48 +0000

    2 done

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -3,5 +3,10 @@
 
 -- Switch to school_db
 USE school_db;
-
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    age INT
+);
+DESCRIBE students;
 -- TODO: Write your CREATE TABLE command here
