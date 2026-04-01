b8c4a4a2a79cba7a2f97f1897c338aa0d2119ddf
Sameer Dumne <83795419+sameerdumne@users.noreply.github.com>
2026-04-01 06:36:06 +0000

    BT24F05F055 completed lab 1

--- 09_drop_table.sql
+++ 09_drop_table.sql
@@ -5,3 +5,9 @@ USE school_db;
 
 -- TODO: Write your DROP TABLE command here
 -- Warning: This will permanently delete the table!
+
+DROP TABLE students;                                    
+
+
+
+                    
