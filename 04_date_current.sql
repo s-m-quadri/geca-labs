a733c694bd5668cb8ef770ae71a9d421e75132e6
sadsar09 <sarthakdeshmukh18@gmail.com>
2026-04-02 06:40:55 +0000

    complete

--- 04_date_current.sql
+++ 04_date_current.sql
@@ -8,3 +8,12 @@ USE school_db;
 -- TODO: SELECT full_name, hire_date, CURDATE() AS today,
 --   (CURDATE() >= hire_date) AS hired_on_or_before_today
 -- FROM employees;
+-- 1. Get current server date & time
+SELECT NOW() AS server_time;
+-- 2. Check hiring status relative to today
+SELECT 
+    full_name, 
+    hire_date, 
+    CURDATE() AS today,
+    (CURDATE() >= hire_date) AS hired_on_or_before_today
+FROM employees;
