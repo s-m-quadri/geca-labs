4060d9d8d29245e9bf07255dd54ab39310050959
deshmukhsanika278-lgtm <deshmukhsanika278@gmail.com>
2026-04-06 09:55:09 +0000

    bt24f05f016

--- 02_user_vars_arithmetic.sql
+++ 02_user_vars_arithmetic.sql
@@ -5,3 +5,7 @@ USE proc_lab;
 
 -- TODO: SET @a := 17; SET @b := 5;
 -- TODO: SELECT @a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
+set @a :=17;
+set @b :=5;
+SELECT 
+@a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
