73634cb008739fd6dba4ee31179f6cfbedc3a939
CHETANo3 <chetantupe0303@gmail.com>
2026-04-07 10:44:47 +0000

    lab done

--- 02_user_vars_arithmetic.sql
+++ 02_user_vars_arithmetic.sql
@@ -3,5 +3,7 @@
 
 USE proc_lab;
 
--- TODO: SET @a := 17; SET @b := 5;
--- TODO: SELECT @a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
+SET @a := 17;
+SET @b := 5;
+
+SELECT @a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
--- 08_puzzle_safe_transfer.sql
+++ 08_puzzle_safe_transfer.sql
@@ -7,11 +7,31 @@
 
 USE proc_lab;
 DELIMITER //
+
 DROP PROCEDURE IF EXISTS safe_transfer//
-CREATE PROCEDURE safe_transfer(IN from_id INT, IN to_id INT, IN amount DECIMAL(12,2))
+
+CREATE PROCEDURE safe_transfer(
+    IN from_id INT, 
+    IN to_id INT, 
+    IN amount DECIMAL(12,2)
+)
 BEGIN
-  -- TODO: DECLARE donor_balance ... SELECT balance INTO ... IF ...
-  -- TODO: UPDATE accounts twice or use transactions mindset (single-threaded lab OK)
-  SET @lab5_transfer_todo := 0;
+    DECLARE donor_balance DECIMAL(12,2);
+
+    SELECT balance INTO donor_balance 
+    FROM accounts 
+    WHERE id = from_id;
+
+    IF donor_balance IS NOT NULL AND donor_balance >= amount THEN
+        UPDATE accounts 
+        SET balance = balance - amount 
+        WHERE id = from_id;
+
+        UPDATE accounts 
+        SET balance = balance + amount 
+        WHERE id = to_id;
+    END IF;
+
 END//
+
 DELIMITER ;
