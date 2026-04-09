cc5f88eaf156f18df0a9c01c3b82d29e3416d424
Vedika Gawande <vedikagawande91@gmail.com>
2026-04-09 17:21:04 +0000

    01 completed

--- 02_user_vars_arithmetic.sql
+++ 02_user_vars_arithmetic.sql
@@ -5,3 +5,6 @@ USE proc_lab;
 
 -- TODO: SET @a := 17; SET @b := 5;
 -- TODO: SELECT @a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
+
+set @a:=17,@b:=15;
+select @a+@b as sum_,@a-@b as diff,@a*@b as prod,@a/@b as quot;
