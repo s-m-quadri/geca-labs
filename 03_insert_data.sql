1cb4f174075ad4f95278fa576a1ff2d78d3b4ce4
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 18:18:16 +0000

    Organize submission for BT24F05F043 into labs-database-management-systems/lab-01/BT24F05F043

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,7 +5,3 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
-INSERT INTO students VALUES(1,"Sheetal",19,"sheetal@gmail.com");
-INSERT INTO students VALUES(2,"Tanvi",20,"tanvi@gmail.com");
-INSERT INTO students VALUES(3,"Suhani",21,"suhani@gmail.com");
-SELECT * FROM students;
--- labs-database-management-systems/lab-01/BT24F05F043/02_create_table.sql
+++ labs-database-management-systems/lab-01/BT24F05F043/02_create_table.sql
@@ -5,3 +5,10 @@
 USE school_db;
 
 -- TODO: Write your CREATE TABLE command here
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    age INT,
+    email VARCHAR(50)
+);
+DESCRIBE students;
