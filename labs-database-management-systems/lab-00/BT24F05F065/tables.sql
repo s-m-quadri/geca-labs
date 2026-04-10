dcedfbe41e31f7d7f7adc2ba811b8361cc973b3a
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 18:28:45 +0000

    Organize submission for BT24F05F065 into labs-database-management-systems/lab-00/BT24F05F065

--- labs-database-management-systems/lab-00/BT24F05F065/tables.sql
+++ labs-database-management-systems/lab-00/BT24F05F065/tables.sql
@@ -15,3 +15,18 @@ USE student_db;
 
 -- Verify table creation
 -- DESCRIBE students;
+
+
+CREATE DATABASE IF NOT EXISTS student_db;
+USE student_db;
+SELECT DATABASE();
+
+-- tables.sql solution
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT,
+    department VARCHAR(30)
+);
+
+DESCRIBE students;
