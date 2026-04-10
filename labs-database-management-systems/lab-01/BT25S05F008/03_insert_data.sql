dd8b012c1b468753d2749c2ec166e02e98c93013
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 18:06:38 +0000

    Organize submission for BT25S05F008 into labs-database-management-systems/lab-01/BT25S05F008

--- labs-database-management-systems/lab-01/BT25S05F008/03_insert_data.sql
+++ labs-database-management-systems/lab-01/BT25S05F008/03_insert_data.sql
@@ -5,3 +5,9 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+INSERT INTO students( id,name,age,email)VALUES
+(1, 'prajkta',20,'prajktavaidya45@gmail.com'),
+(2, 'vedika',20,'vedikagawande33@gmail.com'),
+(3,'kajal',20,'kajalgardi23@gmail.com');
+
+
