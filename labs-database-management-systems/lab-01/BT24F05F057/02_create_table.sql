939a9e012a7565e60537fc08135d7a9a36746829
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 18:15:54 +0000

    Organize submission for BT24F05F057 into labs-database-management-systems/lab-01/BT24F05F057

--- labs-database-management-systems/lab-01/BT24F05F057/02_create_table.sql
+++ labs-database-management-systems/lab-01/BT24F05F057/02_create_table.sql
@@ -5,3 +5,11 @@
 USE school_db;
 
 -- TODO: Write your CREATE TABLE command here
+CREATE TABLE students(
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    email VARCHAR(50)
+);
+
+DESCRIBE students; -- TO see Tables Info 
