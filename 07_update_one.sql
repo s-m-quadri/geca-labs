f3d70946d44b0dff2586477d348ec93528d38a9b
tanvi1713 <tanvipardeshi1710@gmail.com>
2026-04-10 10:15:30 +0000

    BT24F05F045

--- 07_update_one.sql
+++ 07_update_one.sql
@@ -3,5 +3,7 @@
 
 USE school_db;
 
--- TODO: UPDATE students SET age = 16 WHERE id = 1;
--- Remember to use WHERE!
+UPDATE students 
+SET age = 16
+ WHERE id = 1;
+
--- 09_delete_where.sql
+++ 09_delete_where.sql
@@ -3,11 +3,11 @@
 
 USE school_db;
 
--- TODO: First, see who will be deleted
--- SELECT * FROM students WHERE age < 15;
 
--- TODO: Then delete them
--- DELETE FROM students WHERE age < 15;
+SELECT * FROM students WHERE age < 15;
 
--- TODO: Check result
--- SELECT * FROM students;
+
+DELETE FROM students WHERE age < 15;
+
+
+SELECT * FROM students;
