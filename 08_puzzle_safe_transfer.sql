2cc718b8c3276f7cbe4552b84cf28f2f260247c0
SagarJ2126 <ssj02122006@gmail.com>
2026-04-10 18:15:10 +0000

    8 completed

--- 08_puzzle_safe_transfer.sql
+++ 08_puzzle_safe_transfer.sql
@@ -11,7 +11,15 @@ DROP PROCEDURE IF EXISTS safe_transfer//
 CREATE PROCEDURE safe_transfer(IN from_id INT, IN to_id INT, IN amount DECIMAL(12,2))
 BEGIN
   -- TODO: DECLARE donor_balance ... SELECT balance INTO ... IF ...
-  -- TODO: UPDATE accounts twice or use transactions mindset (single-threaded lab OK)
+  DECLARE donor_balance DECIMAL(12,2);
+  SELECT balance INTO donor_balance FROM accounts WHERE id = from_id;
+
+  IF donor_balance >= amount THEN
+    -- TODO: UPDATE accounts twice or use transactions mindset (single-threaded lab OK)
+    UPDATE accounts SET balance = balance - amount WHERE id = from_id;
+    UPDATE accounts SET balance = balance + amount WHERE id = to_id;
+  END IF;
+
   SET @lab5_transfer_todo := 0;
 END//
 DELIMITER ;
