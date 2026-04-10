e170f34f96686b7dc89cde3289a8fd4352c10451
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 17:45:20 +0000

    Organize submission for BT24F05F035 into labs-database-management-systems/lab-01/BT24F05F035

--- 05_alter_modify.sql
+++ 05_alter_modify.sql
@@ -4,13 +4,3 @@
 USE school_db;
 
 -- TODO: Write your ALTER TABLE MODIFY COLUMN command here
-ALTER TABLE students
-MODIFY COLUMN age TINYINT NOT NULL;
-
-INSERT INTO students (id, name, age, email)
-VALUES (1, 'Alice', 15, 'ahilesh@school.com');
- 
-INSERT INTO students (id, name, age, email)
-VALUES (2, 'Bob', 16, 'tanmay@school.com');
- 
-SELECT * FROM students;
--- labs-database-management-systems/lab-01/BT24F05F035/01_create_database.sql
+++ labs-database-management-systems/lab-01/BT24F05F035/01_create_database.sql
@@ -2,3 +2,6 @@
 -- Create a database named 'school_db'
 
 -- TODO: Write your SQL command here
+CREATE DATABASE IF NOT EXISTS school_db;
+USE school_db;
+-- SHOW DATABASES;
