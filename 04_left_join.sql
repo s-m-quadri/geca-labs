b56231559d926823bdb71bb18ee8b237b7474e7a
amruta-rothe <rotheamruta1@gmail.com>
2026-04-10 09:40:06 +0000

    All done

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -3,6 +3,14 @@
 USE join_lab;
 
 -- TODO: SELECT departments.dept_name, staff.name
--- FROM departments
--- LEFT JOIN staff ON staff.dept_id = departments.dept_id
--- ORDER BY departments.dept_id, staff.name;
+
+SELECT 
+    departments.dept_name, 
+    staff.name
+
+FROM departments
+
+LEFT JOIN staff 
+    ON staff.dept_id = departments.dept_id
+    
+ORDER BY departments.dept_id, staff.name;
