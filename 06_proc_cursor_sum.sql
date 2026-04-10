f8102c4584f2c090699eb1c7556937cb40d137d4
SagarJ2126 <ssj02122006@gmail.com>
2026-04-10 18:14:45 +0000

    6 completed

--- 06_proc_cursor_sum.sql
+++ 06_proc_cursor_sum.sql
@@ -7,11 +7,17 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS sum_balances//
 CREATE PROCEDURE sum_balances(OUT total DECIMAL(14,2))
 BEGIN
-  -- TODO: DECLARE done INT DEFAULT 0;
-  -- TODO: DECLARE b DECIMAL(12,2);
-  -- TODO: DECLARE cur CURSOR FOR SELECT balance FROM accounts;
-  -- TODO: DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-  -- TODO: SET total = 0; OPEN cur; loop FETCH; IF done LEAVE; total := total + b; END LOOP; CLOSE cur;
-  SET total = -1;
+  DECLARE done INT DEFAULT 0;
+  DECLARE b DECIMAL(12,2);
+  DECLARE cur CURSOR FOR SELECT balance FROM accounts;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+  SET total = 0;
+  OPEN cur;
+  loop_label: LOOP
+    FETCH cur INTO b;
+    IF done THEN LEAVE loop_label; END IF;
+    SET total = total + b;
+  END LOOP;
+  CLOSE cur;
 END//
 DELIMITER ;
