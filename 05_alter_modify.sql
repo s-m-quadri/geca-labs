72bad5c3b3c2d07b51f55b4e576b20eb61d14cce
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 18:30:10 +0000

    Organize submission for BT24F05F063 into labs-database-management-systems/lab-01/BT24F05F063

--- 05_alter_modify.sql
+++ 05_alter_modify.sql
@@ -4,10 +4,3 @@
 USE school_db;
 
 -- TODO: Write your ALTER TABLE MODIFY COLUMN command here
-INSERT INTO students (id, name, age, email)
-VALUES (1, 'Alice', 15, 'alice@school.com');
- 
-INSERT INTO students (id, name, age, email)
-VALUES (2, 'Bob', 16, 'bob@school.com');
- 
-SELECT * FROM students;
--- labs-database-management-systems/lab-01/BT24F05F063/07_update_data.sql
+++ labs-database-management-systems/lab-01/BT24F05F063/07_update_data.sql
@@ -5,3 +5,7 @@ USE school_db;
 
 -- TODO: Write your UPDATE command here
 -- Remember to use WHERE clause!
+ALTER TABLE students
+DROP COLUMN phone;
+ 
+DESCRIBE students;
