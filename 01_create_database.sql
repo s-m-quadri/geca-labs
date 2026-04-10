fe962c143944e5edc99821b526a7c239c2b39e96
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 18:47:33 +0000

    Organize submission for BT24F05F019 into labs-database-management-systems/lab-01/BT24F05F019

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -2,5 +2,3 @@
 -- Create a database named 'school_db'
 
 -- TODO: Write your SQL command here
-CREATE DATABASE IF NOT EXISTS school_db;
-SHOW DATABASES; -- This will list all databases, including the one you just created!
