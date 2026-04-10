31e9f269ccb5e608d450a3942d0b91dcebe21354
Sayli Machave <saylimachave59@gmail.com>
2026-04-10 14:26:05 +0000

    13 completed

--- 13_group_aggregate.sql
+++ 13_group_aggregate.sql
@@ -2,10 +2,9 @@
 -- For each dept: employee count, total salary, average salary.
 
 USE school_db;
-
--- TODO: SELECT dept,
---   COUNT(*) AS n,
---   SUM(salary) AS total_pay,
---   AVG(salary) AS avg_pay
--- FROM employees
--- GROUP BY dept;
+ SELECT dept,
+COUNT(*) AS n,
+ SUM(salary) AS total_pay,
+  AVG(salary) AS avg_pay
+FROM employees
+GROUP BY dept;
