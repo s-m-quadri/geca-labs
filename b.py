0716e160a1e9b8fe0f3c2e8e4d10aa3b908e898b
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 17:27:21 +0000

    Organize submission for BT24F05F059 into labs-database-management-systems/lab-00/BT24F05F059

--- b.py
+++ b.py
@@ -1,8 +1,11 @@
+# Task: Create variables for a student record:
+#  - student_id (integer, e.g., 101)
+#  - student_name (string, e.g., "Alice")
+#  - marks (float, e.g., 85.5)
+#  - department (string, e.g., "CSE")
+# Print all variables in a formatted way.
 
-student_id = 101
-student_name = "Maaz"
-marks = 85.5
-department = "CSE"
+# 💡 TIP:
+# Use f-strings for formatting: f"ID: {student_id}"
 
-print(f"id:{student_id}")
-print(f"")
+
---
+++ labs-database-management-systems/lab-00/BT24F05F059/b.py
@@ -0,0 +1,8 @@
+
+student_id = 101
+student_name = "Maaz"
+marks = 85.5
+department = "CSE"
+
+print(f"id:{student_id}")
+print(f"")
