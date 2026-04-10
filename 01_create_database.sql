0e0e594aeb226b66b5faf8175fbcbc0ff8ebac97
bot-s-m-quadri <access.smq@gmail.com>
2026-04-10 17:48:11 +0000

    Organize submission for BT24F05F022 into labs-database-management-systems/lab-01/BT24F05F022

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -1,8 +1,4 @@
 -- Task 1: Create Database
 -- Create a database named 'school_db'
 
--- TODO: Write your SQL command here  
-
-create database school_db;
-USE school_db;
-show databases;
+-- TODO: Write your SQL command here
