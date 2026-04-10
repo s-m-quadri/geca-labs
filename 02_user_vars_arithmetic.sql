63d2923ca0b5730f4565cd60c3444f26f1af3dee
SagarJ2126 <ssj02122006@gmail.com>
2026-04-10 18:13:31 +0000

    2 completed

--- 02_user_vars_arithmetic.sql
+++ 02_user_vars_arithmetic.sql
@@ -3,5 +3,5 @@
 
 USE proc_lab;
 
--- TODO: SET @a := 17; SET @b := 5;
--- TODO: SELECT @a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
+SET @a := 17; SET @b := 5;
+SELECT @a + @b AS sum_, @a - @b AS diff, @a * @b AS prod, @a / @b AS quot;
