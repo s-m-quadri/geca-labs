a1089e0cc71750277f8d4493b9731e9d978b3ed5
Pankaj Ramdas Karpe <pankajkarpe86@gmail.com>
2026-04-11 06:13:49 +0000

    complete lab-03 for BT24F05F069

---
+++ BT24F05F069/lab-03/14_group_having.sql
@@ -0,0 +1,11 @@
+USE school_db;
+
+SELECT dept, AVG(salary) AS avg_pay
+FROM employees
+GROUP BY dept
+HAVING AVG(salary) > 35000;
+
+SELECT dept,
+       GROUP_CONCAT(full_name ORDER BY full_name SEPARATOR ', ') AS members
+FROM employees
+GROUP BY dept;
