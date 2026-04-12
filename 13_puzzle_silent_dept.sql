705e6e00949428cd88d0e93b1655e55042d9d422
Prabhat <jhaprabhat.anand@gmail.com>
2026-04-12 15:15:23 +0000

    done lab 4 again in new  codespace

--- 13_puzzle_silent_dept.sql
+++ 13_puzzle_silent_dept.sql
@@ -4,3 +4,18 @@
 USE join_lab;
 
 -- TODO: Use an outer join or NOT EXISTS pattern; return dept_name only
+
+SELECT d.dept_name
+FROM departments d
+LEFT JOIN projects p 
+    ON d.dept_id = p.dept_id
+WHERE p.proj_id IS NULL;
+
+--- Alternative using NOT EXISTS:
+-- SELECT d.dept_name
+-- FROM departments d
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM projects p
+--     WHERE p.dept_id = d.dept_id
+-- );
