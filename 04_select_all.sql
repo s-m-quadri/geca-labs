e4840849eca272ca9bb19de1913248d831bae21b
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 19:04:44 +0000

    Organize submission for BT25S05F006 into labs-database-management-systems/lab-02/BT25S05F006

--- 04_select_all.sql
+++ 04_select_all.sql
@@ -1,14 +1,6 @@
 -- Task 4: Select All Students
 -- View all student records
 
-USE school_db1;
+USE school_db;
 
 -- TODO: Write SELECT query to show all students
-SELECT *FROM students;
-Select name from students;
-
-update students set marks=94
-where id=7;
-update students set marks=95
-where id=6;
-SELECT *FROM students;
--- 05_select_where.sql
+++ 05_select_where.sql
@@ -1,8 +1,6 @@
 -- Task 5: Select with WHERE
 -- Find students in 10th grade
 
-USE school_db1;
+USE school_db;
 
 -- TODO: SELECT students WHERE grade = '10th'
-select name from students where name="Pratiksha";
-select age,name from students where id<=3;
