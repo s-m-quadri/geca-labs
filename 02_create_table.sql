8a468235c90514bf01c42cb4ff9307453f57e494
Kapil Tekam <kapiltekam01@gmail.com>
2026-04-19 18:20:12 +0000

    done

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -5,3 +5,9 @@
 USE school_db;
 
 -- TODO: Write your CREATE TABLE command here
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    age INT,
+    email VARCHAR(100)
+);  
