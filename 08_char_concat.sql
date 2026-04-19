946931e86f6be1d0ad590913c8230915c68064ad
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 19:14:50 +0000

    Organize submission for BT24F05F027 into labs-database-management-systems/lab-03/BT24F05F027

--- 08_char_concat.sql
+++ 08_char_concat.sql
@@ -3,6 +3,6 @@
 
 USE school_db;
 
-SELECT full_name,
-CONCAT(full_name, ' | ', dept, ' | ', salary) AS label
-FROM employees;
+-- TODO: SELECT full_name,
+--   CONCAT(full_name, ' | ', dept, ' | ', salary) AS label
+-- FROM employees;
