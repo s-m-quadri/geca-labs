bd6ec38cce9b22e498e8bc558452733dd0bf7b83
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 19:38:40 +0000

    Organize submission for BT24F05F043 into labs-database-management-systems/lab-03/BT24F05F043

--- labs-database-management-systems/lab-03/BT24F05F043/08_char_concat.sql
+++ labs-database-management-systems/lab-03/BT24F05F043/08_char_concat.sql
@@ -1,8 +1,14 @@
 -- Task 8: Character — CONCAT
 -- Build a single label column: "Name | Dept | Salary"
 
-USE school_db;
+
 
 -- TODO: SELECT full_name,
 --   CONCAT(full_name, ' | ', dept, ' | ', salary) AS label
 -- FROM employees;
+USE school_db;
+
+SELECT 
+    full_name,
+    CONCAT(full_name, ' | ', dept, ' | ', salary) AS label
+FROM employees;
