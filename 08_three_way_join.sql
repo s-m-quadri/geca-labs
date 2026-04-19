9bf97baf2befd8426e89a51b6417bfe726d89988
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 18:45:07 +0000

    Organize submission for BT25S05F002 into labs-database-management-systems/lab-04/BT25S05F002

--- 08_three_way_join.sql
+++ 08_three_way_join.sql
@@ -4,14 +4,3 @@ USE join_lab;
 
 -- TODO: From project_staff, join staff and projects (and departments if you want dept_name)
 -- Columns: staff.name, departments.dept_name, projects.title, project_staff.hours
-
-select project_staff.hours,staff.name,departments.dept_name,projects.title
-from project_staff
-join staff on staff.staff_id=project_staff.staff_id
-join projects on projects.proj_id=project_staff.proj_id
-join departments on departments.dept_id=staff.dept_id;
-
-select * from project_staff;
-select * from staff;
-select * from projects;
-select * from departments;
