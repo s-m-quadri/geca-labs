4001ea3a2a379febb9ae25393fc9b74d48c8f231
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 19:34:54 +0000

    Organize submission for BT24F05F042 into labs-database-management-systems/lab-04/BT24F05F042

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -5,12 +5,4 @@ USE join_lab;
 -- TODO: SELECT departments.dept_name, staff.name
 -- FROM departments
 -- LEFT JOIN staff ON staff.dept_id = departments.dept_id
--- ORDER BY departments.dept_id, staff.name;USE join_lab;
-
-SELECT 
-    departments.dept_name, 
-    staff.name
-FROM departments
-LEFT JOIN staff 
-ON staff.dept_id = departments.dept_id
-ORDER BY departments.dept_id, staff.name;
+-- ORDER BY departments.dept_id, staff.name;
--- labs-database-management-systems/lab-04/BT24F05F042/13_puzzle_silent_dept.sql
+++ labs-database-management-systems/lab-04/BT24F05F042/13_puzzle_silent_dept.sql
@@ -4,3 +4,12 @@
 USE join_lab;
 
 -- TODO: Use an outer join or NOT EXISTS pattern; return dept_name only
+USE join_lab;
+
+SELECT dept_name
+FROM departments d
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM projects p
+    WHERE p.dept_id = d.dept_id
+);
