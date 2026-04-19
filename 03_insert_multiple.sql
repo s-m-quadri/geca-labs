77401e693594dfbe43536ec7515f8db679d77cae
Kapil Tekam <kapiltekam01@gmail.com>
2026-04-19 18:43:20 +0000

    Done

--- 03_insert_multiple.sql
+++ 03_insert_multiple.sql
@@ -8,3 +8,8 @@ USE school_db;
 -- ('Bob', 16, '11th'),
 -- ('Charlie', 15, '10th'),
 -- ('Diana', 17, '12th');
+INSERT INTO students (name, age, grade) VALUES
+('Bob', 16, '11th'),
+('Charlie', 15, '10th'),
+('Diana', 17, '12th');
+
