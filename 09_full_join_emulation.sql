3043bf0b6d5045a741cb4f0b2bec1227e3306325
Sayli Machave <saylimachave59@gmail.com>
2026-04-19 16:22:40 +0000

    9 completed

--- 09_full_join_emulation.sql
+++ 09_full_join_emulation.sql
@@ -5,3 +5,20 @@
 USE join_lab;
 
 -- TODO: Write a query your instructor can run; add a short comment on your strategy
+USE join_lab;
+
+-- Strategy: Emulate a FULL OUTER JOIN by taking the UNION of a LEFT JOIN 
+-- and a RIGHT JOIN. This captures projects with no staff, staff with no 
+-- projects, and those with assignments.
+
+SELECT projects.title, staff.name
+FROM projects
+LEFT JOIN project_staff ON projects.project_id = project_staff.project_id
+LEFT JOIN staff ON project_staff.staff_id = staff.staff_id
+
+UNION
+
+SELECT projects.title, staff.name
+FROM projects
+RIGHT JOIN project_staff ON projects.project_id = project_staff.project_id
+RIGHT JOIN staff ON project_staff.staff_id = staff.staff_id;
