ca51aa974610be60a5c9b258aa74d38930b044a4
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 19:16:38 +0000

    Organize submission for BT24F05F041 into labs-database-management-systems/lab-03/BT24F05F041

--- 01_setup.sql
+++ 01_setup.sql
@@ -3,13 +3,7 @@
 
 -- TODO: DROP DATABASE IF EXISTS school_db; then CREATE DATABASE school_db;
 
-DROP DATABASE IF EXISTS school_db;
-
-CREATE DATABASE school_db;
-
 -- TODO: USE school_db;
-
-USE school_db;
 
 -- TODO: CREATE TABLE employees with:
 --   emp_id INT PRIMARY KEY AUTO_INCREMENT
@@ -19,25 +13,7 @@ USE school_db;
 --   hire_date DATE
 --   phone VARCHAR(25)
 
-CREATE TABLE employees(
-    emp_id INT PRIMARY KEY AUTO_INCREMENT,
-    full_name VARCHAR(60) NOT NULL,
-    dept VARCHAR(40),
-    salary DECIMAL(10,2),
-    hire_date DATE,
-    phone VARCHAR(25)
-);
-
 -- TODO: INSERT at least 5 rows. Vary dept, salary, hire_date.
 -- Optional: put leading/trailing spaces in some phone values for TRIM task later.
 
-INSERT INTO employees VALUES
-(1, "Anand Kharat", "Finance", 5750.5, "2003-03-03", "12345 67890"),
-(2, "Sarthak Jha", "Marketing", 3950.6, "2024-07-02", "098 765 4321"),
-(3, "Ayaz Khan", "Marketing", 12000.0, "2022-06-01", " 567 384 0912"),
-(4, "Aman Singh", "Finance", 2250.0, "2024-03-09", "0987 123 456"),
-(5, "Anil Thakare", "Finance", 15550.5, "2020-01-06", "47928 52043");
-
 -- TODO: SELECT * FROM employees;
-
-SELECT * FROM employees;
