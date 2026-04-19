d170b0c1c3c5f43b4f90e5413f2f5aaeadaebc7e
bot-s-m-quadri <access.smq@gmail.com>
2026-04-19 19:30:32 +0000

    Organize submission for BT24F05F025 into labs-database-management-systems/lab-04/BT24F05F025

--- labs-database-management-systems/lab-04/BT24F05F025/02_cross_join.sql
+++ labs-database-management-systems/lab-04/BT24F05F025/02_cross_join.sql
@@ -2,6 +2,9 @@
 -- List every pair (staff.name, project.title). Count rows mentally: |staff| * |projects|
 
 USE join_lab;
+select count(*) from staff;
+select count(*) from projects;
+
 
 -- TODO: SELECT staff.name, projects.title
 -- FROM staff
