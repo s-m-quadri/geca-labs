23e471de16d261475619d9edabcb88c6f08fb29f
snehal2511 <sonisnehal1106@gmail.com>
2026-04-23 19:04:36 +0000

    done

--- 11_puzzle_twin_projects.sql
+++ 11_puzzle_twin_projects.sql
@@ -7,3 +7,9 @@
 USE join_lab;
 
 -- TODO: Write one SELECT that returns exactly one name (the solver).
+
+USE join_lab;
+SELECT a.name AS person_a, b.name AS person_b
+FROM staff AS a
+JOIN staff AS b
+  ON a.dept_id = b.dept_id AND a.staff_id < b.staff_id;
