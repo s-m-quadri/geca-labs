b82cfa7ddc1784ba99f3877ca4f8040789459840
akshadamane286 <BT24F05F038@geca.ac.in>
2026-04-24 16:48:36 +0000

    done

--- a.py
+++ a.py
@@ -5,4 +5,5 @@
 # 💡 TIP:
 # Use the print() function to display text
 
-
+print("Hello, Python!")
+print("Welcome to DBMS Lab")
--- e.py
+++ e.py
@@ -7,4 +7,12 @@
 # def function_name(param1, param2):
 #     print(f"Student: {param1}")
 
+def display_student(name, age, dept):
+    print(f"Name       : {name}")
+    print(f"Age        : {age}")
+    print(f"Department : {dept}")
+    print("-" * 25)
 
+display_student("Rahul", 20, "CSE")
+display_student("Sneha", 19, "IT")
+display_student("Amit", 21, "Mechanical")
