a4122938924a9fc9b073ee97de395013b938966c
SagarJ2126 <ssj02122006@gmail.com>
2026-04-24 10:34:29 +0000

    9 completed

--- 09_puzzle_silver_medal.sql
+++ 09_puzzle_silver_medal.sql
@@ -4,3 +4,9 @@
 \c view_lab
 
 -- TODO: one SELECT; avoid hard-coded price values from the seed in the outer query
+SELECT name FROM view_lab.products
+WHERE price = (
+    SELECT DISTINCT price FROM view_lab.products
+    ORDER BY price DESC
+    LIMIT 1 OFFSET 1
+);
