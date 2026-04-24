e1ed5ee74327bfe76dd2bfe15b8919c790bc1b65
Vedant Dakare <dakarevedant24@gmail.com>
2026-04-24 17:02:50 +0000

    done with lab 2

--- 01_setup.sql
+++ 01_setup.sql
@@ -10,3 +10,17 @@
 -- name VARCHAR(50)
 -- age INT
 -- grade VARCHAR(10)
+
+-- Create database
+CREATE DATABASE school_db;
+
+-- Switch to the database
+USE school_db;
+
+-- Create students table
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT,
+    grade VARCHAR(10)
+);
