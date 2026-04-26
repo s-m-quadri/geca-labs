f51f3dff8a84c420459154f03e5883a1ca33abf4
VishakhaC-1234 <vishakhachau1234@gmail.com>
2026-04-26 18:21:11 +0000

    done

--- 08_three_way_join.sql
+++ 08_three_way_join.sql
@@ -4,3 +4,17 @@ USE join_lab;
 
 -- TODO: From project_staff, join staff and projects (and departments if you want dept_name)
 -- Columns: staff.name, departments.dept_name, projects.title, project_staff.hours
+USE join_lab;
+
+SELECT 
+  staff.name,
+  departments.dept_name,
+  projects.title,
+  project_staff.hours
+FROM project_staff
+JOIN staff 
+  ON project_staff.staff_id = staff.staff_id
+JOIN projects 
+  ON project_staff.proj_id = projects.proj_id
+JOIN departments 
+  ON staff.dept_id = departments.dept_id;
