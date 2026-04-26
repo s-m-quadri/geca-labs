a5b677c76e312ce7011d8ffc676a3c267a53a4d2
BAGDE SUDHANSHU SUDHIR <sudhanshubagde1234@gmail.com>
2026-04-26 05:01:03 +0000

    Completed

--- 13_puzzle_silent_dept.sql
+++ 13_puzzle_silent_dept.sql
@@ -4,3 +4,10 @@
 USE join_lab;
 
 -- TODO: Use an outer join or NOT EXISTS pattern; return dept_name only
+
+SELECT dept_name
+FROM departments
+WHERE dept_id NOT IN (
+  SELECT DISTINCT dept_id
+  FROM projects
+);
