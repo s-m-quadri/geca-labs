8125b2d70f479f6353de374d17ae06be0e20adbd
Yadvi choudhari <yadvichoudhari11@gmail.com>
2026-04-27 08:51:56 +0000

    done

--- 08_derived_table.sql
+++ 08_derived_table.sql
@@ -1,6 +1,6 @@
 -- Task 8: Derived table in FROM
 -- From a subquery that computes per-order revenue, select orders where revenue > 30
-\c view_lab
+
 
 -- TODO: SELECT * FROM (
 --   SELECT order_id, SUM(ol.qty * p.price) AS rev
@@ -9,3 +9,16 @@
 --   GROUP BY order_id
 -- ) AS t
 -- WHERE t.rev > 30;
+USE view_lab;
+
+SELECT *
+FROM (
+    SELECT 
+        ol.order_id, 
+        SUM(ol.qty * p.price) AS rev
+    FROM order_lines ol
+    JOIN products p 
+        ON ol.prod_id = p.prod_id
+    GROUP BY ol.order_id
+) AS t
+WHERE t.rev > 30;
