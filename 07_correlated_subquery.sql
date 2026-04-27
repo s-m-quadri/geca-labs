a251e710a6a90abd907bc5a3ec7a2f8d29f0bc39
VishakhaC-1234 <vishakhachau1234@gmail.com>
2026-04-27 16:32:58 +0000

    done

--- 07_correlated_subquery.sql
+++ 07_correlated_subquery.sql
@@ -1,5 +1,18 @@
 -- Task 7: Correlated subquery -- customers who spent more than 50 total
 -- (sum of qty * price across all their order lines)
-\c view_lab
+USE view_lab;
 
 -- TODO: correlated pattern on customers + orders + order_lines + products
+USE view_lab;
+
+SELECT c.name
+FROM customers c
+WHERE (
+    SELECT SUM(ol.qty * p.price)
+    FROM orders o
+    JOIN order_lines ol 
+        ON o.order_id = ol.order_id
+    JOIN products p 
+        ON ol.prod_id = p.prod_id
+    WHERE o.cust_id = c.cust_id
+) > 50;
