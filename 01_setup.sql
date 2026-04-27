70f5355186ea43fc2bd4271a6ea495f3318bf9b2
chaitanyabakhal <chaitanyabakhal99@gmail.com>
2026-04-27 07:56:10 +0000

    1 done

--- 01_setup.sql
+++ 01_setup.sql
@@ -1,6 +1,18 @@
 -- Task 1: Setup Database and Table
 -- Create database and students table
+-- Create database
+CREATE DATABASE school_db;
 
+-- Use the database
+USE school_db;
+
+-- Create students table
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    grade VARCHAR(10)
+);
 -- TODO: Create database school_db
 
 -- TODO: Switch to school_db
