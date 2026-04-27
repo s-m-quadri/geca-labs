b70167483c38cbe2933cd3c48a2c1b19e7d073ab
Soham Joshi <sohamjoshi3011@gmail.com>
2026-04-27 17:25:22 +0530

    clean up comments
    Removed commented-out code and adjusted procedure logic to set total correctly.

--- 06_proc_cursor_sum.sql
+++ 06_proc_cursor_sum.sql
@@ -1,14 +1,10 @@
-
 USE proc_lab;
 DROP PROCEDURE IF EXISTS sum_balances;
+
+DELIMITER $$
+
 CREATE PROCEDURE sum_balances(OUT total DECIMAL(14,2))
 BEGIN
-  -- TODO: DECLARE done INT DEFAULT 0;
-  -- TODO: DECLARE b DECIMAL(12,2);
-  -- TODO: DECLARE cur CURSOR FOR SELECT balance FROM accounts;
-  -- TODO: DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-  -- TODO: SET total = 0; OPEN cur; loop FETCH; IF done LEAVE; total := total + b; END LOOP; CLOSE cur;
-  SET total = -1;
   DECLARE done INT DEFAULT 0;
   DECLARE b DECIMAL(12,2);
 
@@ -29,4 +25,6 @@ BEGIN
   END LOOP;
 
   CLOSE cur;
-END;
+END$$
+
+DELIMITER ;
