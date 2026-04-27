18c977d52ddbc18d097f16b9a54207cfe006ebd4
chaitanyabakhal <chaitanyabakhal99@gmail.com>
2026-04-27 08:54:23 +0000

    7 done

--- 07_correlated_subquery.sql
+++ 07_correlated_subquery.sql
@@ -2,4 +2,16 @@
 -- (sum of qty * price across all their order lines)
 \c view_lab
 
+SELECT c.name
+FROM customers c
+WHERE (
+    SELECT SUM(ol.qty * p.unit_price)
+    FROM orders o
+    JOIN order_lines ol 
+        ON o.order_id = ol.order_id
+    JOIN products p 
+        ON ol.prod_id = p.prod_id
+    WHERE o.cust_id = c.cust_id
+) > 50;
+
 -- TODO: correlated pattern on customers + orders + order_lines + products
