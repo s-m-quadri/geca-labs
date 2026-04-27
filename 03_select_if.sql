53d32e44c01ae1bcda33d792de987ef985e2b38a
chaitanyabakhal <chaitanyabakhal99@gmail.com>
2026-04-27 08:45:43 +0000

    3 done

--- 03_select_if.sql
+++ 03_select_if.sql
@@ -2,6 +2,14 @@
 -- Label each account as 'high' if balance >= 1000 else 'low'
 \c proc_lab
 
+SELECT 
+    holder,
+    balance,
+    CASE 
+        WHEN balance >= 1000 THEN 'high'
+        ELSE 'low'
+    END AS tier
+FROM accounts;
 -- TODO: SELECT holder, balance,
 --              CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
 --       FROM accounts;
