ab8bf5e9a5bb1d6d8f86bf364f1c07ca922c0727
bot-s-m-quadri <access.smq@gmail.com>
2026-04-27 04:44:19 +0000

    Organize submission for BT24F05F003 into labs-database-management-systems/lab-04/BT24F05F003

--- 09_full_join_emulation.sql
+++ 09_full_join_emulation.sql
@@ -5,15 +5,3 @@
 USE join_lab;
 
 -- TODO: Write a query your instructor can run; add a short comment on your strategy
-SELECT staff.name, projects.title, project_staff.hours
-FROM staff
-LEFT JOIN project_staff ON staff.staff_id = project_staff.staff_id
-LEFT JOIN projects ON project_staff.project_id = projects.project_id
-WHERE project_staff.project_id IS NOT NULL
-UNION ALL   
-SELECT staff.name, projects.title, project_staff.hours
-FROM projects
-LEFT JOIN project_staff ON projects.project_id = project_staff.project_id
-LEFT JOIN staff ON project_staff.staff_id = staff.staff_id
-WHERE project_staff.staff_id IS NULL
-ORDER BY staff.name, projects.title;
