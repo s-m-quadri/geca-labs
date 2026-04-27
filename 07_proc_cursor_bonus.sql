ff6c8e0f9a5889a8946ad112fad7d1176825b95f
chaitanyabakhal <chaitanyabakhal99@gmail.com>
2026-04-27 08:46:12 +0000

    7 done

--- 07_proc_cursor_bonus.sql
+++ 07_proc_cursor_bonus.sql
@@ -4,16 +4,24 @@
 \c proc_lab
 
 CREATE OR REPLACE PROCEDURE apply_bonuses()
-LANGUAGE plpgsql AS $$
+LANGUAGE plpgsql 
+AS $$
 DECLARE
   eid INT;
-  cur CURSOR FOR SELECT emp_id FROM payroll WHERE bonus_eligible = TRUE;
+  cur CURSOR FOR 
+      SELECT emp_id 
+      FROM payroll 
+      WHERE bonus_eligible = TRUE;
 BEGIN
   OPEN cur;
   LOOP
     FETCH cur INTO eid;
     EXIT WHEN NOT FOUND;
-    -- TODO: UPDATE payroll SET salary = salary + 100 WHERE emp_id = eid;
+
+    UPDATE payroll
+    SET salary = salary + 100
+    WHERE emp_id = eid;
+
   END LOOP;
   CLOSE cur;
 END;
