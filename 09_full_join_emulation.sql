df42bf6d1d66977da997c6d2367ecb50e72f5f0a
chaitanyabakhal <chaitanyabakhal99@gmail.com>
2026-04-27 08:17:23 +0000

    9 done

--- 09_full_join_emulation.sql
+++ 09_full_join_emulation.sql
@@ -4,4 +4,31 @@
 
 USE join_lab;
 
+-- Strategy:
+-- 1) LEFT JOIN: all projects + matching staff
+-- 2) RIGHT JOIN: all staff + matching projects
+-- 3) UNION removes duplicates → simulates FULL OUTER JOIN
+
+SELECT 
+    projects.title,
+    staff.name,
+    project_staff.hours
+FROM projects
+LEFT JOIN project_staff 
+    ON projects.project_id = project_staff.project_id
+LEFT JOIN staff 
+    ON project_staff.staff_id = staff.staff_id
+
+UNION
+
+SELECT 
+    projects.title,
+    staff.name,
+    project_staff.hours
+FROM projects
+RIGHT JOIN project_staff 
+    ON projects.project_id = project_staff.project_id
+RIGHT JOIN staff 
+    ON project_staff.staff_id = staff.staff_id;
+
 -- TODO: Write a query your instructor can run; add a short comment on your strategy
