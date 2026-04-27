e195f8642056327ad2d14b1245a525bebf3cffe8
Prabhat <jhaprabhat.anand@gmail.com>
2026-04-27 05:57:59 +0000

    Completed lab 2 once again

--- 03_insert_multiple.sql
+++ 03_insert_multiple.sql
@@ -8,3 +8,8 @@ USE school_db;
 -- ('Bob', 16, '11th'),
 -- ('Charlie', 15, '10th'),
 -- ('Diana', 17, '12th');
+INSERT INTO students (name, age, grade) VALUES
+('Soham', 20, 'SY CSE'),
+('Guru', 19, 'SY CSE'),
+('Shreya', 20, 'SY FS');
+SELECT * FROM students; 
