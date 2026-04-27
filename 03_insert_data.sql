e41bc00a6f8b63f4100b5928929bdcb40ba5b614
Prabhat <jhaprabhat.anand@gmail.com>
2026-04-27 05:12:55 +0000

    Lab 1 re done

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,3 +5,7 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+INSERT INTO students (name, age, email) VALUES ('Adarsh Singh', 22, 'adarsh.singh@example.com');
+INSERT INTO students (name, age, email) VALUES ('Krutarth Fulare', 23, 'krutarth.fulare@example.com');
+INSERT INTO students (name, age, email) VALUES ('Charlie Puth', 26, 'charlie.puth@example.com'); 
+SELECT * FROM students; 
