6d183087b62c2c08cfb4f060eb756ad5a7ac804c
rohanpawar-23 <rohansp2006@gmail.com>
2026-04-28 23:02:27 +0530

    Update 07_correlated_subquery.sql
    Commented out the database connection command for clarity.

--- 07_correlated_subquery.sql
+++ 07_correlated_subquery.sql
@@ -1,6 +1,6 @@
 -- Task 7: Correlated subquery -- customers who spent more than 50 total
 -- (sum of qty * price across all their order lines)
-\c view_lab
+--\c view_lab
 
 -- TODO: correlated pattern on customers + orders + order_lines + products
 
