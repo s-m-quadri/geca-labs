aaf299387e55acba29d8a09da2bbf3f00b3d348c
Soham Joshi <sohamjoshi3011@gmail.com>
2026-04-28 12:50:04 +0530

    Made some changes
    Updated the SQL script to call the apply_rate function directly using SELECT instead of CALL.

--- 05_call_procedures.sql
+++ 05_call_procedures.sql
@@ -1,6 +1,6 @@
-USE proc_lab;
+-- Task 5: Call apply_rate (created in task 4)
+-- PostgreSQL functions are called with SELECT, not CALL + session variable
+-- \c proc_lab
 
--- TODO: CALL apply_rate(200, 10, @out);
--- TODO: SELECT @out AS with_tax;
--- CALL apply_rate(200, 10, @out);
--- SELECT @out AS with_tax;
+-- TODO: SELECT apply_rate(200, 10) AS with_tax;
+SELECT apply_rate(200, 10) AS with_tax;
