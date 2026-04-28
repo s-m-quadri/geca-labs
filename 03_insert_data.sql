a354af4582b8571d5133cebe8a35e8e845f8d329
anantjoshi87 <antjoshi2006@gmail.com>
2026-04-28 07:22:10 +0000

    BT24F05F068

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,3 +5,7 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+
+INSERT INTO students (first_name, last_name, age) VALUES ('John', 'Doe', 20);
+INSERT INTO students (first_name, last_name, age) VALUES ('Jane', 'Smith', 22);
+INSERT INTO students (first_name, last_name, age) VALUES ('Emily', 'Johnson', 19);
