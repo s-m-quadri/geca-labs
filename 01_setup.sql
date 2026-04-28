afea5a266bc3552fc0373c25d498f8225d47b4fb
sarthak-thakare246 <sarthakthakare273@gmail.com>
2026-04-28 09:34:46 +0000

    BT24F05F061

--- 01_setup.sql
+++ 01_setup.sql
@@ -10,3 +10,12 @@
 -- name VARCHAR(50)
 -- age INT
 -- grade VARCHAR(10)
+Create database school_db;
+USE school_db;
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT,
+    grade VARCHAR(10)
+);
+
