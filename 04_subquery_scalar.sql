3063e3b5553e51f8c77b1b68c6b1692060e36cab
Sayli Machave <saylimachave59@gmail.com>
2026-04-29 12:10:40 +0000

    04 completed

--- 04_subquery_scalar.sql
+++ 04_subquery_scalar.sql
@@ -1,5 +1,5 @@
 -- Task 4: Scalar subquery -- products priced above the average product price
 \c view_lab
 
--- TODO: SELECT name, price FROM products
---       WHERE price > ( ... scalar subquery for AVG(price) ... );
+ SELECT name, price FROM products
+   WHERE price > ( ... scalar subquery for AVG(price) ... );
