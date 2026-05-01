19b590458dda6c06d33037f17275dab204817883
SanviMulay <sanvirmulay31@gmail.com>
2026-05-01 17:18:49 +0000

    done

--- 08_derived_table.sql
+++ 08_derived_table.sql
@@ -9,3 +9,13 @@
 --   GROUP BY order_id
 -- ) AS t
 -- WHERE t.rev > 30;
+SELECT *
+FROM (
+  SELECT 
+    ol.order_id,
+    SUM(ol.qty * p.price) AS rev
+  FROM order_lines ol
+  JOIN products p ON ol.prod_id = p.prod_id
+  GROUP BY ol.order_id
+) AS t
+WHERE t.rev > 30;
