e6453b33654ad6e4ee15f73ce2cffb71dfcb88a3
tanvi1713 <tanvipardeshi1710@gmail.com>
2026-05-01 14:28:37 +0000

    done

--- 09_puzzle_balance_enigma.sql
+++ 09_puzzle_balance_enigma.sql
@@ -2,7 +2,7 @@
 -- "The smallest balance whispers with the largest: their product is the code."
 -- Compute (MIN balance) * (MAX balance) from accounts and label it secret_code.
 \c proc_lab
+SELECT
+  (SELECT MIN(balance) FROM accounts) *
+  (SELECT MAX(balance) FROM accounts) AS secret_code;
 
--- TODO: SELECT
---   (SELECT MIN(balance) FROM accounts) *
---   (SELECT MAX(balance) FROM accounts) AS secret_code;
