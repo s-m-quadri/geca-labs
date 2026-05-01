fefff34ad14de09a43272ee9d024eae7762caf08
sadsar09 <sarthakdeshmukh18@gmail.com>
2026-05-01 17:23:35 +0000

    BT24F05F017 lab 06

--- 11_puzzle_split_basket.sql
+++ 11_puzzle_split_basket.sql
@@ -4,3 +4,11 @@
 \c view_lab
 
 -- TODO: HAVING with conditional sums, or EXISTS pair, or intersect of two subqueries
+
+SELECT order_id FROM v_order_lines_detail
+GROUP BY order_id
+HAVING SUM(CASE WHEN unit_price < 15 THEN 1 ELSE 0 END) > 0
+   AND SUM(CASE WHEN unit_price > 30 THEN 1 ELSE 0 END) > 0;    
+    
+
+            
