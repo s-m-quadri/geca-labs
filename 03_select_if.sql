3579dadf00b7e54e1e1af9b937b9ca190911e649
BAGDE SUDHANSHU SUDHIR <sudhanshubagde1234@gmail.com>
2026-05-02 22:11:24 +0530

    Fix SQL syntax for account tier selection

--- 03_select_if.sql
+++ 03_select_if.sql
@@ -3,9 +3,9 @@
 \c proc_lab
 
 -- TODO: SELECT holder, balance,
---              CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
---       FROM accounts;
+-- CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
+-- FROM accounts;
 
 TODO: SELECT holder, balance,
-             CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
-      FROM accounts;
+CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
+FROM accounts;
