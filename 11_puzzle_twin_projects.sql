e7ce6bbe680b17f7d4b118dfce5e154e878e4429
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 17:33:28 +0000

    Organize submission for BT24F05F048 into labs-database-management-systems/lab-04/BT24F05F048

--- 11_puzzle_twin_projects.sql
+++ 11_puzzle_twin_projects.sql
@@ -7,14 +7,3 @@
 USE join_lab;
 
 -- TODO: Write one SELECT that returns exactly one name (the solver).
-
-SELECT s.name
-FROM staff s
-JOIN project_staff ps1 ON s.staff_id = ps1.staff_id
-JOIN project_staff ps2 ON s.staff_id = ps2.staff_id
-JOIN projects p1 ON ps1.proj_id = p1.proj_id
-JOIN projects p2 ON ps2.proj_id = p2.proj_id
-WHERE p1.proj_id < p2.proj_id              
-  AND p1.dept_id = p2.dept_id              
-  AND p1.title LIKE 'Riddle%'             
-  AND p2.title LIKE 'Riddle%';
