7bebb65898301fa8deea8d0a2f01f137f09c1f6c
rohitkamdi03-png <rohitkamdi03@gmail.com>
2026-05-02 06:01:28 +0000

    2 done

--- 02_create_view.sql
+++ 02_create_view.sql
@@ -2,5 +2,17 @@
 -- Columns: order_id, prod_id, qty, product name, unit price, line_total (qty * price)
 \c view_lab
 
+CREATE OR REPLACE VIEW v_order_lines_detail AS
+SELECT 
+    ol.order_id,
+    ol.prod_id,
+    ol.qty,
+    p.product_name,
+    p.unit_price,
+    (ol.qty * p.unit_price) AS line_total
+FROM order_lines ol
+JOIN products p 
+    ON ol.prod_id = p.prod_id;
+
 -- TODO: CREATE OR REPLACE VIEW v_order_lines_detail AS
 -- SELECT ...
