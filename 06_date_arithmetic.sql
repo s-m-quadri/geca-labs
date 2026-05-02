acaf66f74131f74a048560c23d849f6306f7f2ab
amaanpro987 <choudhariamaan47@gmail.com>
2026-05-02 17:22:25 +0000

    BT24F05F012

--- 06_date_arithmetic.sql
+++ 06_date_arithmetic.sql
@@ -4,10 +4,10 @@
 
 USE school_db;
 
--- TODO: SELECT full_name, hire_date,
---   DATEDIFF(CURDATE(), hire_date) AS days_employed
--- FROM employees;
+SELECT full_name, hire_date,
+  DATEDIFF(CURDATE(), hire_date) AS days_employed
+FROM employees;
 
--- TODO: SELECT full_name, hire_date,
---   DATE_ADD(hire_date, INTERVAL 1 YEAR) AS first_anniversary
--- FROM employees;
+SELECT full_name, hire_date,
+  DATE_ADD(hire_date, INTERVAL 1 YEAR) AS first_anniversary
+FROM employees;
