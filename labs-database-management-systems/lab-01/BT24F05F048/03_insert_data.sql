55c6a2ef1c5bf4bec87b3c3209a04b9a732b8469
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 17:31:54 +0000

    Organize submission for BT24F05F048 into labs-database-management-systems/lab-01/BT24F05F048

--- labs-database-management-systems/lab-01/BT24F05F048/03_insert_data.sql
+++ labs-database-management-systems/lab-01/BT24F05F048/03_insert_data.sql
@@ -5,3 +5,9 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+INSERT INTO students (id, name, age)
+VALUES (1, 'Alice', 20),
+       (2, 'Bob', 22),
+       (3, 'Charlie', 21);
+
+DESCRIBE students;  
--- labs-database-management-systems/lab-01/BT24F05F048/08_delete_data.sql
+++ labs-database-management-systems/lab-01/BT24F05F048/08_delete_data.sql
@@ -3,5 +3,9 @@
 
 USE school_db;
 
+DELETE FROM students
+WHERE student_id = 101;
+
+
 -- TODO: Write your DELETE command here
 -- Remember to use WHERE clause!
