7a0a2404f6e7b823e47a233e94245c973731cf37
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 17:34:56 +0000

    Organize submission for BT24F05F048 into labs-database-management-systems/lab-02/BT24F05F048

--- labs-database-management-systems/lab-02/BT24F05F048/09_delete_where.sql
+++ labs-database-management-systems/lab-02/BT24F05F048/09_delete_where.sql
@@ -11,3 +11,8 @@ USE school_db;
 
 -- TODO: Check result
 -- SELECT * FROM students;
+USE school_db;
+-- Always SELECT first to confirm what will be deleted
+SELECT * FROM students WHERE age < 15;
+DELETE FROM students WHERE age < 15;
+SELECT * FROM students;
--- labs-database-management-systems/lab-02/BT24F05F048/10_count.sql
+++ labs-database-management-systems/lab-02/BT24F05F048/10_count.sql
@@ -4,3 +4,6 @@
 USE school_db;
 
 -- TODO: SELECT COUNT(*) as total_students FROM students;
+USE school_db;
+SELECT COUNT(*) AS total FROM students;
+SELECT COUNT(*) AS total_10th FROM students WHERE grade = '10th';
