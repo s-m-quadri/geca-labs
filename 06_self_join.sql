ca098ce7241ab9088647656ccf47f967858b8ba0
shreeyadosi <shreeyadosi@gmail.com>
2026-05-02 16:36:30 +0000

    BT24F05F019

--- 06_self_join.sql
+++ 06_self_join.sql
@@ -6,3 +6,6 @@ USE join_lab;
 -- TODO: SELECT a.name AS person_a, b.name AS person_b, a.dept_id
 -- FROM staff a
 -- JOIN staff b ON ...
+select a.name as person_a, b.name as person_b, a.dept_id
+from staff a
+join staff b on a.dept_id = b.dept_id and a.staff_id < b.staff_id;
--- 11_puzzle_twin_projects.sql
+++ 11_puzzle_twin_projects.sql
@@ -7,3 +7,11 @@
 USE join_lab;
 
 -- TODO: Write one SELECT that returns exactly one name (the solver).
+select staff.name
+from project_staff
+join staff on project_staff.staff_id = staff.staff_id
+join projects on project_staff.project_id = projects.project_id
+join departments on projects.dept_id = departments.dept_id
+where projects.title like 'Twin%' -- same name prefix
+group by staff.name
+having count(distinct projects.project_id) = 2; -- booked hours on both projects    
