a509f296ea8879e40da452d9f10fe24e48f4ea10
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 17:35:25 +0000

    Organize submission for BT24F05F048 into labs-database-management-systems/lab-00/BT24F05F048

--- labs-database-management-systems/lab-00/BT24F05F048/f.py
+++ labs-database-management-systems/lab-00/BT24F05F048/f.py
@@ -10,4 +10,16 @@
 # Read: with open("file.txt", "r") as f:
 #          print(f.read())
 
+student_records = [
+    "Alice,20,CSE",
+    "Bob,21,ECE",
+    "Charlie,19,ME"
+]
+
+with open("students.txt", "w") as f:
+    for record in student_records:
+        f.write(record + "\n")
+
+with open("students.txt", "r") as f:
+    print(f.read())
 
