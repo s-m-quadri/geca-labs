9f2e324ef8ef2578b837b6c60e4dbf5f48e907b3
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 16:03:49 +0000

    Organize submission for BT24F05F069 into labs-database-management-systems/lab-02/BT24F05F069

--- labs-database-management-systems/lab-02/BT24F05F069/03_insert_multiple.sql
+++ labs-database-management-systems/lab-02/BT24F05F069/03_insert_multiple.sql
@@ -8,3 +8,7 @@ USE school_db;
 -- ('Bob', 16, '11th'),
 -- ('Charlie', 15, '10th'),
 -- ('Diana', 17, '12th');
+INSERT INTO students (name, age, grade) VALUES
+('Pankaj', 21, '12th'),
+('Meeran', 19, '11th'),
+('Anant', 22, '12th');
