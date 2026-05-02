1a487229dc336f36718240dd4f6ff0d7e9065dbf
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 16:38:11 +0000

    Organize submission for BT24F05F008 into labs-database-management-systems/lab-04/BT24F05F008

--- 09_full_join_emulation.sql
+++ 09_full_join_emulation.sql
@@ -5,29 +5,3 @@
 USE join_lab;
 
 -- TODO: Write a query your instructor can run; add a short comment on your strategy
--- FULL OUTER JOIN emulation:
--- 1) projects LEFT JOIN project_staff + staff → keeps all projects
--- 2) staff LEFT JOIN project_staff + projects → keeps all staff
--- UNION removes duplicates, giving full outer behavior
-
-SELECT 
-  projects.title,
-  staff.name,
-  project_staff.hours
-FROM projects
-LEFT JOIN project_staff 
-  ON projects.proj_id = project_staff.proj_id
-LEFT JOIN staff 
-  ON project_staff.staff_id = staff.staff_id
-
-UNION
-
-SELECT 
-  projects.title,
-  staff.name,
-  project_staff.hours
-FROM staff
-LEFT JOIN project_staff 
-  ON staff.staff_id = project_staff.staff_id
-LEFT JOIN projects 
-  ON project_staff.proj_id = projects.proj_id;
