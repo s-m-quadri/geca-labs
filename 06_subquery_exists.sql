7c19b248f57023acf09b55fd85db5b05630e4a59
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 16:52:19 +0000

    Organize submission for BT24F05F070 into labs-database-management-systems/lab-06/BT24F05F070

--- 06_subquery_exists.sql
+++ 06_subquery_exists.sql
@@ -1,8 +1,7 @@
 -- Task 6: EXISTS -- customers who have at least one order
---\c view_lab
+\c view_lab
 
--- TODO:
-SELECT c.name FROM customers c
-WHERE EXISTS (
-  SELECT 1 FROM orders o WHERE o.cust_id = c.cust_id
-);
+-- TODO: SELECT c.name FROM customers c
+--       WHERE EXISTS (
+--         SELECT 1 FROM orders o WHERE o.cust_id = c.cust_id
+--       );
