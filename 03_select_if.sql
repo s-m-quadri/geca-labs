fca4807067e78db184c024fca4e8baabe6ab6f15
raghava26 <raghav.abhyankar0326@gmail.com>
2026-05-02 12:41:04 +0000

    LAB 05 COMPLETED

--- 03_select_if.sql
+++ 03_select_if.sql
@@ -5,3 +5,7 @@
 -- TODO: SELECT holder, balance,
 --              CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
 --       FROM accounts;
+SELECT holder, balance,
+             CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
+      FROM accounts;
+        
