caa29279c26de34a566f62dff7012e982a7ced05
Divyaingle-dev <bingle768@gmail.com>
2026-05-02 00:39:47 +0000

    completed 02

--- 02_create_view.sql
+++ 02_create_view.sql
@@ -3,5 +3,13 @@
 
 USE view_lab;
 
--- TODO: CREATE OR REPLACE VIEW v_order_lines_detail AS
--- SELECT ...
+CREATE OR REPLACE VIEW v_order_lines_detail AS
+SELECT
+  ol.order_id,
+  ol.prod_id,
+  ol.qty,
+  p.name AS product_name,
+  p.price AS unit_price,
+  ol.qty * p.price AS line_total
+FROM order_lines AS ol
+JOIN products AS p ON ol.prod_id = p.prod_id;
