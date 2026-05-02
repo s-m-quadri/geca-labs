093549dfd978de6416b217e174cf126ba6261a2b
rohitkamdi03-png <rohitkamdi03@gmail.com>
2026-05-02 05:38:57 +0000

    4 done

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -2,6 +2,14 @@
 
 USE join_lab;
 
+SELECT 
+    departments.dept_name,
+    staff.name
+FROM departments
+LEFT JOIN staff 
+    ON staff.dept_id = departments.dept_id
+ORDER BY departments.dept_id, staff.name;
+
 -- TODO: SELECT departments.dept_name, staff.name
 -- FROM departments
 -- LEFT JOIN staff ON staff.dept_id = departments.dept_id
