5414d50fb90611bbc17f394af32fe176b34c7758
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 17:30:39 +0000

    Organize submission for BT24F05F048 into labs-database-management-systems/lab-06/BT24F05F048

--- 10_puzzle_wallflower.sql
+++ 10_puzzle_wallflower.sql
@@ -1,8 +1,5 @@
 -- Puzzle B (riddle)
 -- "Who never pressed checkout? List customer names with zero orders."
+\c view_lab
 
-SELECT name 
-FROM customers c
-WHERE NOT EXISTS (
-  SELECT 1 FROM orders o WHERE o.cust_id = c.cust_id
-);
+-- TODO: anti-join or NOT EXISTS
