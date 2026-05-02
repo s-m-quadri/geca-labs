c3ea5eaa35aee5a4b0c9c95563f77b82be6c8940
akshadamane286 <BT24F05F038@geca.ac.in>
2026-05-02 13:38:28 +0000

    DONE

--- 06_subquery_exists.sql
+++ 06_subquery_exists.sql
@@ -1,7 +1,17 @@
--- Task 6: EXISTS -- customers who have at least one order
-\c view_lab
+-- Task 6: EXISTS -- customers who have at least one orderUSE
+USE view_lab
 
 -- TODO: SELECT c.name FROM customers c
 --       WHERE EXISTS (
 --         SELECT 1 FROM orders o WHERE o.cust_id = c.cust_id
 --       );
+USE view_lab;
+
+SELECT
+    c.name
+FROM customers c
+WHERE EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.cust_id = c.cust_id
+);
