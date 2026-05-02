28e29352c1602f1f7e1ebb2fa342decda537d73a
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 16:55:35 +0000

    Organize submission for BT24F05F010 into labs-database-management-systems/lab-05/BT24F05F010

--- labs-database-management-systems/lab-05/BT24F05F010/02_user_vars_arithmetic.sql
+++ labs-database-management-systems/lab-05/BT24F05F010/02_user_vars_arithmetic.sql
@@ -3,3 +3,4 @@
 \c proc_lab
 
 -- TODO: SELECT 17 + 5 AS sum_, 17 - 5 AS diff, 17 * 5 AS prod, 17.0 / 5 AS quot;
+select 17 + 5 as sum_, 17 - 5 as diff, 17 * 5 as prod, 17.0 / 5 as quot;                                       
