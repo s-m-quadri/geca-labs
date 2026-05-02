339cf65632ffc5b5dbcee0d1fe5767d22a557c98
rohitkamdi03-png <rohitkamdi03@gmail.com>
2026-05-02 05:52:45 +0000

    5 done

--- 05_call_procedures.sql
+++ 05_call_procedures.sql
@@ -2,4 +2,6 @@
 -- PostgreSQL functions are called with SELECT, not CALL + session variable
 \c proc_lab
 
+SELECT apply_rate(200, 10) AS with_tax;
+
 -- TODO: SELECT apply_rate(200, 10) AS with_tax;
