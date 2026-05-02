186361f901bb299981f154a659bc428a74637e90
BAGDE SUDHANSHU SUDHIR <sudhanshubagde1234@gmail.com>
2026-05-02 22:13:10 +0530

    Update 09_puzzle_balance_enigma.sql

--- 09_puzzle_balance_enigma.sql
+++ 09_puzzle_balance_enigma.sql
@@ -3,6 +3,6 @@
 -- Compute (MIN balance) * (MAX balance) from accounts and label it secret_code.
 \c proc_lab
 
-TODO: SELECT
-  (SELECT MIN(balance) FROM accounts) *
-  (SELECT MAX(balance) FROM accounts) AS secret_code;
+SELECT
+(SELECT MIN(balance) FROM accounts) *
+(SELECT MAX(balance) FROM accounts) AS secret_code;
