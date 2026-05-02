6433fefb849b46dda4a354a71883eefb674b2d0e
sarthak-thakare246 <sarthakthakare273@gmail.com>
2026-05-02 08:35:57 +0000

    BT24F05F061 LAB 05

--- 03_select_if.sql
+++ 03_select_if.sql
@@ -4,3 +4,4 @@
 SELECT holder, balance,
             CASE WHEN balance >= 1000 THEN 'high' ELSE 'low' END AS tier
  FROM accounts;
+ 
--- 09_puzzle_balance_enigma.sql
+++ 09_puzzle_balance_enigma.sql
@@ -6,3 +6,4 @@
 SELECT
   (SELECT MIN(balance) FROM accounts) *
   (SELECT MAX(balance) FROM accounts) AS secret_code;
+  
