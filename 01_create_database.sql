ebd44f5fee4c05aa10e19131e55847f2815dea13
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 16:41:40 +0000

    Organize submission for BT24F05F068 into labs-database-management-systems/lab-01/BT24F05F068

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -2,10 +2,3 @@
 -- Create a database named 'school_db'
 
 -- TODO: Write your SQL command here
-
--- Task 2: Create Tables
-Create a table named 'students' with the following columns:
-- id (INT, PRIMARY KEY, AUTO_INCREMENT)
-- first_name (VARCHAR(50))
-- last_name (VARCHAR(50))
-- age (INT)
