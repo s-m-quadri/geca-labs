0d52cf55359589aeb2b8dff915304d543e895858
bot-s-m-quadri <access.smq@gmail.com>
2026-05-02 17:01:13 +0000

    Organize submission for BT24F05F005 into labs-database-management-systems/lab-01/BT24F05F005

--- 07_update_data.sql
+++ 07_update_data.sql
@@ -5,6 +5,3 @@ USE school_db;
 
 -- TODO: Write your UPDATE command here
 -- Remember to use WHERE clause!
-update students set student_grade = 'A' where student_name = 'Alice Johnson';
-update students set student_grade = 'B' where student_name = 'Bob Smith';
-update students set student_grade = 'A-' where student_name = 'Charlie Brown';
