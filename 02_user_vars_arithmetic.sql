1eef34ae2ce9937fc7c59a84751d2f15f0911ef2
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 11:05:08 +0000

    Organize submission for BT24F05F029 into labs-database-management-systems/lab-05/BT24F05F029

--- 02_user_vars_arithmetic.sql
+++ 02_user_vars_arithmetic.sql
@@ -2,10 +2,4 @@
 -- PostgreSQL has no session variables (@a := ...); just SELECT the expressions.
 \c proc_lab
 
-SELECT 
-    17 + 5 AS sum_,
-    17 - 5 AS diff,
-    17 * 5 AS prod,
-    17.0 / 5 AS quot;
-
 -- TODO: SELECT 17 + 5 AS sum_, 17 - 5 AS diff, 17 * 5 AS prod, 17.0 / 5 AS quot;
--- labs-database-management-systems/lab-05/BT24F05F029/07_proc_cursor_bonus.sql
+++ labs-database-management-systems/lab-05/BT24F05F029/07_proc_cursor_bonus.sql
@@ -10,11 +10,17 @@ DECLARE
   cur CURSOR FOR SELECT emp_id FROM payroll WHERE bonus_eligible = TRUE;
 BEGIN
   OPEN cur;
+
   LOOP
     FETCH cur INTO eid;
     EXIT WHEN NOT FOUND;
-    -- TODO: UPDATE payroll SET salary = salary + 100 WHERE emp_id = eid;
+
+    UPDATE payroll 
+    SET salary = salary + 100 
+    WHERE emp_id = eid;
+
   END LOOP;
+
   CLOSE cur;
 END;
 $$;
