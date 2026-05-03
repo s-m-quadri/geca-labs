a134a2ddf5cf11ba9d95b9d633b92cf3c4f59fcc
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 13:09:31 +0000

    Organize submission for BT24F05F033 into labs-database-management-systems/lab-03/BT24F05F033

--- 04_date_current.sql
+++ 04_date_current.sql
@@ -3,8 +3,8 @@
 
 USE school_db;
 
- SELECT NOW() AS server_time;
+-- TODO: SELECT NOW() AS server_time;
 
- SELECT full_name, hire_date, CURDATE() AS today,
-   (CURDATE() >= hire_date) AS hired_on_or_before_today
- FROM employees;
+-- TODO: SELECT full_name, hire_date, CURDATE() AS today,
+--   (CURDATE() >= hire_date) AS hired_on_or_before_today
+-- FROM employees;
