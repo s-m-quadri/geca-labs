5c0776f2ba390a7f6393d99c5b8230eca24ce572
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 10:44:12 +0000

    Organize submission for BT24F05F032 into labs-database-management-systems/lab-04/BT24F05F032

--- labs-database-management-systems/lab-04/BT24F05F032/02_cross_join.sql
+++ labs-database-management-systems/lab-04/BT24F05F032/02_cross_join.sql
@@ -3,6 +3,9 @@
 
 USE join_lab;
 
--- TODO: SELECT staff.name, projects.title
+-- TODO: SELECT staff.name, projects.title    
 -- FROM staff
 -- CROSS JOIN projects;
+ SELECT staff.name, projects.title    
+ FROM staff
+ CROSS JOIN projects;                                                          
