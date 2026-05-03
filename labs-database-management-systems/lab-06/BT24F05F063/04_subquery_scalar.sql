e1bd26b228e4c0af12fffc81af9814e45b13e09d
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 11:04:41 +0000

    Organize submission for BT24F05F063 into labs-database-management-systems/lab-06/BT24F05F063

--- labs-database-management-systems/lab-06/BT24F05F063/04_subquery_scalar.sql
+++ labs-database-management-systems/lab-06/BT24F05F063/04_subquery_scalar.sql
@@ -3,3 +3,5 @@
 
 -- TODO: SELECT name, price FROM products
 --       WHERE price > ( ... scalar subquery for AVG(price) ... );
+SELECT prod_name, unit_price FROM products
+WHERE unit_price > (SELECT AVG(unit_price) FROM products);  
