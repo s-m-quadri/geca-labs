ad10793da775fbbf51877716c83bdb765c2683b9
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 12:37:45 +0000

    Organize submission for BT24F05F017 into labs-database-management-systems/lab-01/BT24F05F017

--- labs-database-management-systems/lab-01/BT24F05F017/03_insert_data.sql
+++ labs-database-management-systems/lab-01/BT24F05F017/03_insert_data.sql
@@ -5,3 +5,9 @@ USE school_db;
 
 -- TODO: Write your INSERT commands here
 -- Example: INSERT INTO students VALUES (...);
+USE school_db;
+
+INSERT INTO students VALUES
+(1, 'Rahul', 18, 'A'),
+(2, 'Priya', 19, 'B'),
+(3, 'Amit', 17, 'A');
--- labs-database-management-systems/lab-01/BT24F05F017/07_update_data.sql
+++ labs-database-management-systems/lab-01/BT24F05F017/07_update_data.sql
@@ -5,3 +5,5 @@ USE school_db;
 
 -- TODO: Write your UPDATE command here
 -- Remember to use WHERE clause!
+ALTER TABLE students
+RENAME COLUMN name TO student_name;
