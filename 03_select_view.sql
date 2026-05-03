c4cd926307a69f42ba079444a70c330f59acec47
YashPatil9990 <yashrajpatil9990@gmail.com>
2026-05-03 22:36:35 +0530

    COMPLETED

--- 03_select_view.sql
+++ 03_select_view.sql
@@ -5,3 +5,6 @@
 -- TODO: SELECT order_id, SUM(line_total) AS order_total
 --       FROM v_order_lines_detail
 --       GROUP BY order_id;
+SELECT order_id, SUM(line_total) AS order_total
+FROM v_order_lines_detail
+GROUP BY order_id;
