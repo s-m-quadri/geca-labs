596868ef3bc0427f9d1f6abb82e0102feda092c2
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 10:24:27 +0000

    Organize submission for BT24F05F068 into labs-database-management-systems/lab-02/BT24F05F068

--- 01_setup.sql
+++ 01_setup.sql
@@ -1,13 +1,12 @@
 -- Task 1: Setup Database and Table
 -- Create database and students table
 
-CREATE DATABASE school_db;
+-- TODO: Create database school_db
 
-USE school_db;
+-- TODO: Switch to school_db
 
-CREATE TABLE students (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50),
-  age INT,
-  grade VARCHAR(10)
-);
+-- TODO: Create students table with:
+-- id INT PRIMARY KEY AUTO_INCREMENT
+-- name VARCHAR(50)
+-- age INT
+-- grade VARCHAR(10)
