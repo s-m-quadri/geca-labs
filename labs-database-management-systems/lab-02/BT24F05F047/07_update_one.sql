f5893a8c2177a6290143257d34ba112517644b93
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 17:35:23 +0000

    Organize submission for BT24F05F047 into labs-database-management-systems/lab-02/BT24F05F047

--- labs-database-management-systems/lab-02/BT24F05F047/07_update_one.sql
+++ labs-database-management-systems/lab-02/BT24F05F047/07_update_one.sql
@@ -5,3 +5,6 @@ USE school_db;
 
 -- TODO: UPDATE students SET age = 16 WHERE id = 1;
 -- Remember to use WHERE!
+SELECT * FROM students WHERE id = 1;
+UPDATE students SET age = 16 WHERE id = 1;
+SELECT * FROM students WHERE id = 1;        
