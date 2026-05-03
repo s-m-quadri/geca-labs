84660dd8ced9b428fa7022752347761b9a5720d4
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 09:52:34 +0000

    Organize submission for BT24F05F056 into labs-database-management-systems/lab-06/BT24F05F056

--- 07_correlated_subquery.sql
+++ 07_correlated_subquery.sql
@@ -3,13 +3,3 @@
 \c view_lab
 
 -- TODO: correlated pattern on customers + orders + order_lines + products
-SELECT c.cust_id,
-    c.name
-FROM customers c
-WHERE COALESCE((
-    SELECT SUM(ol.qty * p.price)
-    FROM orders o
-    JOIN order_lines ol ON o.order_id = ol.order_id
-    JOIN products p ON ol.prod_id = p.prod_id
-    WHERE o.cust_id = c.cust_id
-), 0) > 50;
