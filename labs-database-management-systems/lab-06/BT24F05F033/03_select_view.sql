e955bb6eff3dbc4c09e90de4fa67a0351eb4574b
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 10:48:20 +0000

    Organize submission for BT24F05F033 into labs-database-management-systems/lab-06/BT24F05F033

---
+++ labs-database-management-systems/lab-06/BT24F05F033/03_select_view.sql
@@ -0,0 +1,7 @@
+-- Task 3: Query the view (create it in 02 first)
+-- Sum line_total per order_id
+\c view_lab
+
+SELECT order_id, SUM(line_total) AS order_total
+FROM v_order_lines_detail
+GROUP BY order_id;
