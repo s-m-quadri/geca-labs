c28a8e3986fbe5c21738f86ab08df7f524296be4
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 10:52:55 +0000

    Organize submission for BT24F05F042 into labs-database-management-systems/lab-05/BT24F05F042

--- 08_puzzle_safe_transfer.sql
+++ 08_puzzle_safe_transfer.sql
@@ -7,35 +7,15 @@
 
 CREATE OR REPLACE PROCEDURE safe_transfer(
   from_id INT,
-  to_id INT,
-  amount DECIMAL(12,2)
-)
-LANGUAGE plpgsql AS $$
+  to_id   INT,
+  amount  DECIMAL(12,2)
+) LANGUAGE plpgsql AS $$
 DECLARE
   donor_bal DECIMAL(12,2);
 BEGIN
-  -- get donor balance
-  SELECT balance INTO donor_bal 
-  FROM accounts 
-  WHERE id = from_id;
-
-  -- check if enough balance
-  IF donor_bal < amount THEN
-    RETURN;
-  END IF;
-
-  -- subtract from donor
-  UPDATE accounts 
-  SET balance = balance - amount 
-  WHERE id = from_id;
-
-  -- add to receiver
-  UPDATE accounts 
-  SET balance = balance + amount 
-  WHERE id = to_id;
-
+  -- TODO: SELECT balance INTO donor_bal FROM accounts WHERE id = from_id;
+  -- TODO: IF donor_bal < amount THEN RETURN; END IF;
+  -- TODO: UPDATE accounts SET balance = balance - amount WHERE id = from_id;
+  -- TODO: UPDATE accounts SET balance = balance + amount WHERE id = to_id;
 END;
 $$;
-SELECT * FROM accounts;
-CALL safe_transfer(1, 2, 100);
-SELECT * FROM accounts;
