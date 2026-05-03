29472c15e750a9090f08e4825e8afc6e2978ef3f
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 10:59:19 +0000

    Organize submission for BT24F05F012 into labs-database-management-systems/lab-03/BT24F05F012

--- labs-database-management-systems/lab-03/BT24F05F012/07_char_case.sql
+++ labs-database-management-systems/lab-03/BT24F05F012/07_char_case.sql
@@ -3,7 +3,7 @@
 
 USE school_db;
 
--- TODO: SELECT full_name,
---   UPPER(dept) AS dept_upper,
---   LOWER(full_name) AS name_lower
--- FROM employees;
+SELECT full_name,
+  UPPER(dept) AS dept_upper,
+  LOWER(full_name) AS name_lower
+FROM employees;
--- labs-database-management-systems/lab-03/BT24F05F012/12_count_distinct.sql
+++ labs-database-management-systems/lab-03/BT24F05F012/12_count_distinct.sql
@@ -3,4 +3,4 @@
 
 USE school_db;
 
--- TODO: SELECT COUNT(DISTINCT dept) AS distinct_depts FROM employees;
+SELECT COUNT(DISTINCT dept) AS distinct_depts FROM employees;
