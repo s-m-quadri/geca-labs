b322418cedbc2c1dd4573583766e6e1f57ca8162
bot-s-m-quadri <access.smq@gmail.com>
2026-05-03 10:51:50 +0000

    Organize submission for BT25S05F008 into labs-database-management-systems/lab-06/BT25S05F008

--- labs-database-management-systems/lab-06/BT25S05F008/03_select_view.sql
+++ labs-database-management-systems/lab-06/BT25S05F008/03_select_view.sql
@@ -5,3 +5,6 @@
 -- TODO: SELECT order_id, SUM(line_total) AS order_total
 --       FROM v_order_lines_detail
 --       GROUP BY order_id;
+select order_id ,sum(line_total) as order_total
+from v_order_lines_detail
+group by order_id;
