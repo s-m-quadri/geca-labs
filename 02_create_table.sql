7e485505038bbe843c8ce1d1da050a38ffb0e293
anishakate9394-crypto <anishakate9394@gmail.com>
2026-05-04 11:46:20 +0530

    Create STUDENT table in school_db

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -6,3 +6,11 @@ USE school_db;
 
 -- TODO: Write your CREATE TABLE command
 CREATE TABLE STUDENT
+USE school_db;
+
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100) UNIQUE
+);
