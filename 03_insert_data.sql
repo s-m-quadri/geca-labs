2f122b86149670241d8d7e423f253a0f910a0088
shraddhaujgare6-commits <shraddhaujgare6@gmail.com>
2026-05-05 19:21:59 +0000

    resolved conflicts

---
+++ 03_insert_data.sql
@@ -0,0 +1,11 @@
+-- Task 3: Insert Sample Data
+-- Insert 3 student records into the students table
+
+USE school_db;
+
+-- TODO: Write your INSERT commands here
+INSERT INTO students (name, age, email, grade)
+VALUES 
+('Rahul Sharma', 20, 'rahul.sharma@example.com', 'A'),
+('Sneha Patil', 22, 'sneha.patil@example.com', 'B+'),
+('Amit Deshmukh', 19, 'amit.deshmukh@example.com', 'A-');
