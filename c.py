f493c16bb0b18dc5ca4280587bd1699f8986f546
Riya Sali <gecaa54@gmail.com>
2026-05-05 13:46:10 +0000

    BT24F05F054 lab 00

--- c.py
+++ c.py
@@ -9,3 +9,11 @@
 # Count: len(students)
 
 
+students = ["Mayur", "talvinder", "amman", "mangesh"]
+
+n=1
+for i in (students):
+    print(f"{n}. {i}")
+    n+=1
+
+print("total no. of students: ",len(students))
--- e.py
+++ e.py
@@ -7,4 +7,10 @@
 # def function_name(param1, param2):
 #     print(f"Student: {param1}")
 
+def student_name(id, name, age, dept):
+    print(f"student detail, \nid\t: {id} \nname\t: {name} \nage\t: {age} \ndept\t: {dept}")
+    print("\n")
 
+student_name(1, "Mayur", 20, "CSE")
+student_name(2, "Rohan", 19, "CSE")
+student_name(3, "Pawan", 21, "CSE")
