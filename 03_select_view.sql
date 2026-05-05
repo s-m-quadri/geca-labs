f6f191a831a537b0017acd8082d10e3f3bf47f57
Mayuri Futane <futanemayuri0@gmail.com>
2026-05-05 08:37:02 +0530

    Add query to sum line_total per order_id
    Query the view to get the total revenue for each order, ordered by order_id.

--- 03_select_view.sql
+++ 03_select_view.sql
@@ -1,17 +1,10 @@
--- Task 3: Query the view (create it in 02 first)
 -- Sum line_total per order_id
 \c view_lab
 
 -- TODO: SELECT order_id, SUM(line_total) AS order_total
 --       FROM v_order_lines_detail
 --       GROUP BY order_id;
--- Connect to the database
-\c view_lab
-
--- Query the view to get the total revenue for each order
-SELECT 
-    order_id, 
-    SUM(line_total) AS order_total
+SELECT order_id, SUM(line_total) AS order_total
 FROM v_order_lines_detail
 GROUP BY order_id
 ORDER BY order_id;
