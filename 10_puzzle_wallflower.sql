e4354ebbe0e73d6cc675ee0ebbb625dfc21d7eed
Mayuri Futane <futanemayuri0@gmail.com>
2026-05-05 08:40:38 +0530

    Update 10_puzzle_wallflower.sql

--- 10_puzzle_wallflower.sql
+++ 10_puzzle_wallflower.sql
@@ -1,16 +1,11 @@
--- Puzzle B (riddle)
 -- "Who never pressed checkout? List customer names with zero orders."
 \c view_lab
 
 -- TODO: anti-join or NOT EXISTS
--- Connect to the database
-\c view_lab
-
--- Find customers with zero orders using NOT EXISTS
-SELECT name 
+SELECT c.name
 FROM customers c
 WHERE NOT EXISTS (
-    SELECT 1 
-    FROM orders o 
-    WHERE o.cust_id = c.cust_id
+  SELECT 1
+  FROM orders o
+  WHERE o.cust_id = c.cust_id
 );
