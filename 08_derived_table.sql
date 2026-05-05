bfd680a1c4ba1e3b597a40352875cf2813594df0
Mayuri Futane <futanemayuri0@gmail.com>
2026-05-05 08:39:34 +0530

    Refactor derived table query for revenue filtering

--- 08_derived_table.sql
+++ 08_derived_table.sql
@@ -1,4 +1,3 @@
--- Task 8: Derived table in FROM
 -- From a subquery that computes per-order revenue, select orders where revenue > 30
 \c view_lab
 
@@ -9,16 +8,11 @@
 --   GROUP BY order_id
 -- ) AS t
 -- WHERE t.rev > 30;
--- Connect to the database
-\c view_lab
-
--- Use a derived table to filter orders with revenue greater than 30
-SELECT * FROM (
-    SELECT 
-        ol.order_id, 
-        SUM(ol.qty * p.price) AS rev
-    FROM order_lines ol
-    JOIN products p ON ol.prod_id = p.prod_id
-    GROUP BY order_id
+SELECT *
+FROM (
+  SELECT ol.order_id, SUM(ol.qty * p.price) AS rev
+  FROM order_lines ol
+  JOIN products p ON ol.prod_id = p.prod_id
+  GROUP BY ol.order_id
 ) AS t
 WHERE t.rev > 30;
