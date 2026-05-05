cc5bd3b37d34917a2957734e2a5f4e2605b52f07
bot-s-m-quadri <access.smq@gmail.com>
2026-05-05 08:09:56 +0000

    Organize submission for BT25S05F007 into labs-database-management-systems/lab-06/BT25S05F007

---
+++ labs-database-management-systems/lab-06/BT25S05F007/06_subquery_exists.sql
@@ -0,0 +1,7 @@
+SELECT c.name 
+FROM customers c
+WHERE EXISTS (
+    SELECT 1 
+    FROM orders o 
+    WHERE o.cust_id = c.cust_id
+);
---
+++ labs-database-management-systems/lab-06/BT25S05F007/10_puzzle_wallflower.sql
@@ -0,0 +1,7 @@
+SELECT c.name
+FROM customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.cust_id = c.cust_id
+);
