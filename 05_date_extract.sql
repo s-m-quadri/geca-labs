52a299b0cd6c97f2ee7bc89d373485c47d76be03
Anushka-55 <155242840+Anushka-55@users.noreply.github.com>
2026-05-06 11:17:40 +0000

    lab 3 completed

--- 05_date_extract.sql
+++ 05_date_extract.sql
@@ -3,8 +3,8 @@
 
 USE school_db;
 
--- TODO: SELECT full_name, hire_date,
---   YEAR(hire_date) AS y,
---   MONTH(hire_date) AS m,
---   DAY(hire_date) AS d
--- FROM employees;
+SELECT full_name, hire_date,
+  YEAR(hire_date) AS y,
+  MONTH(hire_date) AS m,
+  DAY(hire_date) AS d
+FROM employees;
