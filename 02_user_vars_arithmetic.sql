bdb8d67c063a1b07461d37ae89b90f96945be8d0
Om Sonawane <sonawaneom0018@gmail.com>
2026-05-06 19:27:04 +0530

    Update 02_user_vars_arithmetic.sql

--- 02_user_vars_arithmetic.sql
+++ 02_user_vars_arithmetic.sql
@@ -1,4 +1,4 @@
-BT25S05F005-- Task 2: Arithmetic in a SELECT (+ - * /)
+-- Task 2: Arithmetic in a SELECT (+ - * /)
 -- PostgreSQL has no session variables (@a := ...); just SELECT the expressions.
 --\c proc_lab
 
