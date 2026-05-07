0da0f89644929b23065172619085e3196821099c
bot-s-m-quadri <access.smq@gmail.com>
2026-05-07 08:10:37 +0000

    Organize submission for BT24F05F054 into labs-database-management-systems/lab-02/BT24F05F054

--- labs-database-management-systems/lab-02/BT24F05F054/01_setup.sql
+++ labs-database-management-systems/lab-02/BT24F05F054/01_setup.sql
@@ -10,3 +10,13 @@
 -- name VARCHAR(50)
 -- age INT
 -- grade VARCHAR(10)
+
+CREATE DATABASE IF NOT EXISTS school_db;
+USE school_db;
+
+CREATE TABLE IF NOT EXISTS students(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50),
+age INT,
+grade VARCHAR(10)
+);
