6f5d862b1c6321bbfcf53f669ee3e55eb0a79255
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 19:13:48 +0000

    2 done

--- 02_cross_join.sql
+++ 02_cross_join.sql
@@ -1,8 +1,4 @@
--- Task 2: Cartesian product (cross join)
--- List every pair (staff.name, project.title). Count rows mentally: |staff| * |projects|
-
 USE join_lab;
-
--- TODO: SELECT staff.name, projects.title
--- FROM staff
--- CROSS JOIN projects;
+SELECT s.name AS staff_name, p.title AS project_title
+FROM staff AS s
+CROSS JOIN projects AS p;
