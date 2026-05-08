8f900390c15b9651c92c54663df15e9c37e4102f
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 10:56:19 +0000

    task 2 done

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -1,10 +1,6 @@
--- Task 2: Create Students Table
--- Create table 'students' with: id, name, age, email
 
--- Switch to school_db
 USE school_db;
 
--- TODO: Write your CREATE TABLE command here
 
     CREATE TABLE students (
         id INT PRIMARY KEY,
