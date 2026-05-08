84b483a0b5680bfdad95868e6ec14a62fabd9d3f
sharadkahale007 <kahalesharad007@gmail.com>
2026-05-08 11:17:01 +0000

    5 done

--- 05_alter_modify.sql
+++ 05_alter_modify.sql
@@ -2,5 +2,7 @@
 -- Change 'age' column to be TINYINT with NOT NULL constraint
 
 USE school_db;
-
+USE school_db;
+ALTER TABLE students MODIFY COLUMN age TINYINT NOT NULL;
+DESCRIBE students;
 -- TODO: Write your ALTER TABLE MODIFY COLUMN command here
