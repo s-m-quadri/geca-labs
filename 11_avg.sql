fef9aff7881c9bf9ed923b416b008d6d23f47513
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 11:18:15 +0000

    task 11 done

--- 11_avg.sql
+++ 11_avg.sql
@@ -1,6 +1,3 @@
--- Task 11: Average Age
--- Calculate average age of all students
-
 USE school_db;
-
--- TODO: SELECT AVG(age) as average_age FROM students;
+SELECT AVG(age) AS avg_age FROM students;
+SELECT grade, AVG(age) AS avg_age FROM students GROUP BY grade;
