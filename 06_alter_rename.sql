2682701caaea9d81e4bd3a474329fae747bd6e10
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 10:57:13 +0000

    task 6 done

--- 06_alter_rename.sql
+++
@@ -1,7 +0,0 @@
--- Task 6: Rename Column
--- Rename 'name' column to 'student_name'
-
-USE school_db;
-
--- TODO: Write your ALTER TABLE RENAME COLUMN command here
--- MySQL syntax: ALTER TABLE table_name RENAME COLUMN old_name TO new_name;
