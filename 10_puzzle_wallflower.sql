233c1516102ff89b6d6ca34a690938b27fef0f30
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 19:46:22 +0000

    10 done

--- 10_puzzle_wallflower.sql
+++ 10_puzzle_wallflower.sql
@@ -1,5 +1,12 @@
--- Puzzle B (riddle)
--- "Who never pressed checkout? List customer names with zero orders."
 \c view_lab
-
--- TODO: anti-join or NOT EXISTS
+-- Option A: NOT EXISTS
+SELECT name FROM customers AS c
+WHERE NOT EXISTS (
+  SELECT 1 FROM orders AS o WHERE o.cust_id = c.cust_id
+);
+ 
+-- Option B: LEFT JOIN anti-join
+SELECT c.name
+FROM customers AS c
+LEFT JOIN orders AS o ON o.cust_id = c.cust_id
+WHERE o.order_id IS NULL;
