f3398cb1b98d471ae18c77badbf13cb93d00131a
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 11:32:14 +0000

    task 3 done

--- 03_numeric_abs_mod.sql
+++ 03_numeric_abs_mod.sql
@@ -1,10 +1,3 @@
--- Task 3: Numeric — ABS and MOD
--- ABS: distance of each salary from 50000
--- MOD: emp_id modulo 3 (remainder)
-
 USE school_db;
-
--- TODO: SELECT emp_id, full_name, salary,
---   ABS(salary - 50000) AS dist_from_50k,
---   MOD(emp_id, 3) AS id_mod_3
--- FROM employees;
+SELECT full_name, ABS(salary - 50000) AS diff_from_50k, MOD(emp_id, 3) AS id_mod3
+FROM employees;
