c08afcd6d5d37168ba2d032851f80e76bc55b049
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 19:15:30 +0000

    13 done

--- 13_puzzle_silent_dept.sql
+++ 13_puzzle_silent_dept.sql
@@ -1,6 +1,5 @@
--- Puzzle C (riddle)
--- "One department never sponsored a project. Which `dept_name` is the wallflower?"
-
 USE join_lab;
-
--- TODO: Use an outer join or NOT EXISTS pattern; return dept_name only
+SELECT d.dept_name
+FROM departments AS d
+LEFT JOIN projects AS p ON p.dept_id = d.dept_id
+WHERE p.proj_id IS NULL;
