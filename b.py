a51d7446b918367042de38cbd0d7ebff6d9e6928
sharadkahale007 <kahalesharad007@gmail.com>
2026-05-08 11:02:14 +0000

    b done

--- b.py
+++ b.py
@@ -4,7 +4,11 @@
 #  - marks (float, e.g., 85.5)
 #  - department (string, e.g., "CSE")
 # Print all variables in a formatted way.
-
+student_id = 101
+student_name = "Alice"
+marks = 85.5
+department = "CSE"
+print(f"ID: {student_id}, Name: {student_name}, Marks: {marks}, Dept: {department}")
 # 💡 TIP:
 # Use f-strings for formatting: f"ID: {student_id}"
 
