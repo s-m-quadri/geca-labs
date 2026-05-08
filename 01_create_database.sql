58d865d1c3259ed03c244b74fa7724bb418d3699
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 10:56:07 +0000

    task 1 done

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -1,8 +1,2 @@
--- Task 1: Create Database
--- Create a database named 'school_db'
-
--- TODO: Write your SQL command here
-
- CREATE DATABASE school_db;
- USE school_db;
- SHOW DATABASES;
+CREATE DATABASE school_db;
+SHOW DATABASES;
