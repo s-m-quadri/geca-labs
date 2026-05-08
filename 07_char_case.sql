4f67d67765d24c5a8f2dbfa9146b5dec4a546d89
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 11:33:21 +0000

    task 7 done

--- 07_char_case.sql
+++ 07_char_case.sql
@@ -1,9 +1,2 @@
--- Task 7: Character — UPPER and LOWER
--- Display department in uppercase and full_name in lowercase.
-
 USE school_db;
-
--- TODO: SELECT full_name,
---   UPPER(dept) AS dept_upper,
---   LOWER(full_name) AS name_lower
--- FROM employees;
+SELECT UPPER(dept) AS dept_upper, LOWER(full_name) AS name_lower FROM employees;
