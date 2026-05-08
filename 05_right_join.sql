21be69cb3e377884d3e9ec36726161b74630852e
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 19:14:19 +0000

    5 done

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,7 +1,4 @@
--- Task 5: Right outer join (MySQL supports RIGHT JOIN)
--- All staff rows, with department name when present
-
 USE join_lab;
-
--- TODO: Rewrite the left-join pattern using RIGHT JOIN
--- (departments on the left, staff on the right) so every staff appears once.
+SELECT s.name AS staff_name, d.dept_name
+FROM departments AS d
+RIGHT JOIN staff AS s ON d.dept_id = s.dept_id;
