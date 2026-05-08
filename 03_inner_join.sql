83444894ce56a1a1110f7b9684daaf06db7165ea
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-08 19:14:00 +0000

    3 done

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,7 +1,4 @@
--- Task 3: Inner join — staff with their department name
-
 USE join_lab;
-
--- TODO: SELECT staff.name, departments.dept_name
--- FROM staff
--- INNER JOIN departments ON staff.dept_id = departments.dept_id;
+SELECT s.name, d.dept_name
+FROM staff AS s
+INNER JOIN departments AS d ON s.dept_id = d.dept_id;
