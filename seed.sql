52437545f8228946262456f1c5e04babd5bc3156
syedmaaz965740-glitch <syedmaaz965740@gmail.com>
2026-05-09 11:07:36 +0000

    seed done

---
+++ seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO Students VALUES
+(1,'Aman','BCA','9876543210'),
+(2,'Rahul','BSc IT','9876501234');
+
+INSERT INTO Books VALUES
+(101,'Java Programming','James Gosling',5),
+(102,'DBMS Concepts','Korth',4);
+
+INSERT INTO IssueBooks VALUES
+(1,1,101,'2026-05-09','2026-05-20');
