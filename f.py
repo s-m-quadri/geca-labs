e7dad5409a4431d223f342382ceacff78d6177fd
bot-s-m-quadri <access.smq@gmail.com>
2026-05-17 15:54:15 +0000

    Organize submission for BT23F05F040 into labs-database-management-systems/lab-00/BT23F05F040

--- f.py
+++ f.py
@@ -10,19 +10,4 @@
 # Read: with open("file.txt", "r") as f:
 #          print(f.read())
 
-with open("student.txt", "w") as f:
-    f.write("")
 
-def give_input(name, age, dept):
-    with open("student.txt", "a") as f:
-        f.write(f"Student detail,\n")
-        f.write(f"1.Name\t:{name}\n")
-        f.write(f"2.Age\t:{age}\n")
-        f.write(f"Dept.\t:{dept}\n\n")
-
-give_input("Mayur Wakchaure",20,"CSE")
-give_input("Pawan Pawar",21,"CSE")
-give_input("Rohan Pawar",19,"CSE")
-
-with open("student.txt", "r") as f:
-    print(f.read())
